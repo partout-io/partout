b19141546b46f563a68e21b9b4722dc3e3d7b87c
Davide <keeshux@gmail.com>
2025-02-25 14:15:40 +0100

    Move meta to manifest top

--- Package.swift
+++ Package.swift
@@ -3,6 +3,10 @@
 
 import PackageDescription
 
+let filename = "PassepartoutKit.xcframework.zip"
+let version = "0.99.13"
+let checksum = "2a79a9864d24dac1e8b054fd28a5d774a2847077cbce982f4cfbee9f57d76a40"
+
 let package = Package(
     name: "PassepartoutKit-Binary",
     platforms: [
@@ -19,8 +23,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Target",
-            url: "https://github.com/passepartoutvpn/passepartoutkit/releases/download/0.99.13/PassepartoutKit.xcframework.zip",
-            checksum: "2a79a9864d24dac1e8b054fd28a5d774a2847077cbce982f4cfbee9f57d76a40"
+            url: "https://github.com/passepartoutvpn/passepartoutkit/releases/download/\(version)/\(filename)",
+            checksum: checksum
         ),
         .testTarget(
             name: "TargetTests",
