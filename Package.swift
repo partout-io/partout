246ccda2adc67df50dcd62bab52de1f0bd60bc1b
Davide De Rosa <keeshux@gmail.com>
2025-04-07 11:04:10 +0000

    0.99.50

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@
 import PackageDescription
 
 let filename = "PassepartoutKit.xcframework.zip"
-let version = "0.99.23"
-let checksum = "919ed203c3ad81d0fb9bd12d82b72af229969eaf6f02bc79fde98f12172ece2a"
+let version = "0.99.50"
+let checksum = "081b92e64af02d2c33c8bb65b4460d44be39b2f394f981e9cab1eb867e5b8792"
 
 let package = Package(
     name: "PassepartoutKit-Binary",
