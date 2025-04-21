26018ac90b160ecada761d85804d4c8f15e9ae54
Davide <keeshux@gmail.com>
2025-04-21 11:07:53 +0200

    Use SSH URL in CI for private repos

--- scripts/ci-generate.sh
+++ scripts/ci-generate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 src_path="PartoutCore"
-src_url="https://github.com/passepartoutvpn/partout-core"
+src_url="git@github.com:passepartoutvpn/partout-core.git"
 src_sha1=`grep 'let sha1' Package.swift | sed -E "s/^let sha1 = \"([A-Fa-f0-9]{40})\"$/\\1/"`
 git clone --no-checkout --filter=blob:none --depth=1 $src_url $src_path
 ( cd $src_path && git fetch origin $src_sha1 && git checkout $src_sha1 )
