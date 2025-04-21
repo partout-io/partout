partout-io/partout

commit 7bec449063def37d49b106fdc3ceae001263b38a

Pull VPN modules from Core (#41)
Fixes #38

Package.swift
@@ -19,10 +19,10 @@ environment = .remoteBinary
 // environment = .localSource
 
 // for action-release-binary-package
-let sha1 = "c087530f1086cb6793bb3097d7ce42d4f5ba6857"
+let sha1 = "2d5fb2ef1cf07e557cfe541638469b2263323694"
 let binaryFilename = "PartoutCore.xcframework.zip"
-let version = "0.99.92"
-let checksum = "36bf4192d7465156038c197914374f6e68b523e49bb50a544f7968dfadcfcfe6"
+let version = "0.99.100"
+let checksum = "955651e7692023cffaafc52ea04f6513e8f6b40e70dff5a79b80bf3a6586230b"
 
 // MARK: - Products
 
@@ -80,7 +80,9 @@ package.targets.append(contentsOf: [
         name: "PartoutAPI",
         dependencies: [
             .product(name: "GenericJSON", package: "generic-json-swift"),
-            "PartoutCoreWrapper"
+            "PartoutCoreWrapper",
+            "_PartoutOpenVPN",
+            "_PartoutWireGuard"
         ]
     ),
     .testTarget(
@@ -187,10 +189,14 @@ package.targets.append(contentsOf: [
         dependencies: ["openssl-apple"]
     ),
     .target(
+        name: "_PartoutOpenVPN",
+        dependencies: ["PartoutCoreWrapper"]
+    ),
+    .target(
         name: "_PartoutOpenVPNOpenSSL_ObjC",
         dependencies: [
             "_PartoutCryptoOpenSSL_ObjC",
-            "PartoutCoreWrapper"
+            "_PartoutOpenVPN"
         ],
         exclude: [
             "lib/COPYING",
@@ -215,6 +221,10 @@ package.targets.append(contentsOf: [
         dependencies: ["_PartoutCryptoOpenSSL"]
     ),
     .testTarget(
+        name: "_PartoutOpenVPNTests",
+        dependencies: ["_PartoutOpenVPN"]
+    ),
+    .testTarget(
         name: "_PartoutOpenVPNOpenSSLTests",
         dependencies: ["_PartoutOpenVPNOpenSSL"],
         resources: [
@@ -231,14 +241,22 @@ package.targets.append(contentsOf: [
         dependencies: ["_PartoutWireGuardGo"]
     ),
     .target(
+        name: "_PartoutWireGuard",
+        dependencies: ["PartoutCoreWrapper"]
+    ),
+    .target(
         name: "_PartoutWireGuardGo",
         dependencies: [
-            "PartoutCoreWrapper",
+            "_PartoutWireGuard",
             .product(name: "WireGuardKit", package: "wireguard-apple")
         ],
         resources: [
             .process("Resources")
         ]
+    ),
+    .testTarget(
+        name: "_PartoutWireGuardTests",
+        dependencies: ["_PartoutWireGuard"]
     ),
     .testTarget(
         name: "_PartoutWireGuardGoTests",

commit e7c3310d1d548ca6b90a62afabb9dd16a2225124

Export OpenVPN and WireGuard modules with umbrella (#42)
Broken in #41

Package.swift
@@ -70,6 +70,8 @@ package.targets.append(contentsOf: [
         name: "Partout",
         dependencies: [
             "PartoutAPI",
+            "_PartoutOpenVPN",
+            "_PartoutWireGuard",
             .target(name: "PartoutNE", condition: .when(platforms: applePlatforms)),
             .target(name: "_PartoutPlatformAndroid", condition: .when(platforms: [.android])),
             .target(name: "_PartoutPlatformApple", condition: .when(platforms: applePlatforms)),
@@ -196,7 +198,7 @@ package.targets.append(contentsOf: [
         name: "_PartoutOpenVPNOpenSSL_ObjC",
         dependencies: [
             "_PartoutCryptoOpenSSL_ObjC",
-            "_PartoutOpenVPN"
+            "PartoutCoreWrapper"
         ],
         exclude: [
             "lib/COPYING",
@@ -213,6 +215,7 @@ package.targets.append(contentsOf: [
         name: "_PartoutOpenVPNOpenSSL",
         dependencies: [
             "_PartoutCryptoOpenSSL",
+            "_PartoutOpenVPN",
             "_PartoutOpenVPNOpenSSL_ObjC"
         ]
     ),

Sources/Partout/Partout.swift
@@ -33,6 +33,11 @@ import Foundation
 @_exported import PartoutNE
 #endif
 
+// MARK: - Modules
+
+@_exported import _PartoutOpenVPN
+@_exported import _PartoutWireGuard
+
 // MARK: - Platform extensions
 
 #if canImport(_PartoutPlatformAndroid)