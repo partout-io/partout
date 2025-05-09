2d40d16bb6557b926b63bc107222411b659e4f3b
Davide <keeshux@gmail.com>
2025-05-09 15:08:48 +0200

    And fix tests...

--- Tests/OpenVPN/OpenVPNOpenSSL/StandardOpenVPNParserTests.swift
+++ Tests/OpenVPN/OpenVPNOpenSSL/StandardOpenVPNParserTests.swift
@@ -186,13 +186,13 @@ final class StandardOpenVPNParserTests: XCTestCase {
         XCTAssertEqual(pushReply.options.ipv4?.subnet, Subnet(rawValue: "172.31.2.6/24"))
         XCTAssertEqual(pushReply.options.ipv4?.includedRoutes, [
             Route(defaultWithGateway: Address(rawValue: "172.31.2.1")),
-            Route(Subnet(rawValue: "172.31.2.0/24"), Address(rawValue: "172.31.2.6"))
+            Route(Subnet(rawValue: "172.31.2.0/24"), nil)
         ])
 
         XCTAssertEqual(pushReply.options.ipv6?.subnet, Subnet(rawValue: "1234::12/10"))
         XCTAssertEqual(pushReply.options.ipv6?.includedRoutes, [
             Route(defaultWithGateway: Address(rawValue: "1234::1")),
-            Route(Subnet(rawValue: "1200::/10"), Address(rawValue: "1234::12"))
+            Route(Subnet(rawValue: "1200::/10"), nil)
         ])
     }
 }
