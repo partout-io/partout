partout-io/partout

commit a12086ba988f15f441d83da3f056c10d91efc249

OpenVPN: Fix missing network routes to the VPN (#49)
`ifconfig` with topology `subnet` and `ifconfig-ipv6` define the
properties of the VPN network:

- The local VPN address
- The network mask or prefix (`ifconfig-ipv6`) of the VPN

For example, `ifconfig 192.168.5.12 255.255.255.0` means that
192.168.5.12 is our IPv4 address in the 192.168.5.0/24 network. A route
to such a network is missing.

The format of `ifconfig-ipv6`, instead, is similar to `ifconfig` with
topology `net30`, where the second argument is the remote VPN endpoint.
However, the IPv6 address comes with a prefix that implies the IPv6
network of the VPN. Similarly, no such route is added.

Sources/OpenVPN/OpenVPNOpenSSL/StandardOpenVPNParser+Builder.swift
@@ -649,7 +649,7 @@ extension StandardOpenVPNParser.Builder {
             }
 
             let address4: String
-            let addressMask4: String
+            let vpnMask4: String?
 
             let topology = Topology(rawValue: optTopology ?? "") ?? .net30
             switch topology {
@@ -660,19 +660,27 @@ extension StandardOpenVPNParser.Builder {
                     throw StandardOpenVPNParserError.malformed(option: "route-gateway takes 1 argument")
                 }
                 address4 = ifconfig4Arguments[0]
-                addressMask4 = ifconfig4Arguments[1]
+                vpnMask4 = ifconfig4Arguments[1]
                 defaultGateway4 = gateway4Arguments[0]
 
             default:
                 address4 = ifconfig4Arguments[0]
-                addressMask4 = "255.255.255.255"
+                vpnMask4 = nil
                 defaultGateway4 = ifconfig4Arguments[1]
             }
 
-            builder.ipv4 = IPSettings(subnet: try Subnet(address4, addressMask4))
-            if let defaultGateway4, let defaultGw = Address(rawValue: defaultGateway4) {
-                builder.ipv4 = builder.ipv4?.including(routes: [Route(defaultWithGateway: defaultGw)])
+            let vpnAddress4 = try Subnet(address4, 32)
+            var includedRoutes: [Route] = []
+            if let defaultGateway4, let defaultGw4Address = Address(rawValue: defaultGateway4) {
+                includedRoutes.append(Route(defaultWithGateway: defaultGw4Address))
             }
+            if let vpnMask4, let vpnNetwork4 = vpnAddress4.address.network(with: vpnMask4) {
+                let vpnDestination4 = try Subnet(vpnNetwork4.rawValue, vpnMask4)
+                includedRoutes.append(Route(vpnDestination4, vpnAddress4.address))
+            }
+
+            builder.ipv4 = IPSettings(subnet: vpnAddress4)
+                .including(routes: includedRoutes)
         } else {
             defaultGateway4 = nil
         }
@@ -692,17 +700,25 @@ extension StandardOpenVPNParser.Builder {
             guard address6Components.count == 2 else {
                 throw StandardOpenVPNParserError.malformed(option: "ifconfig-ipv6 address must have a /prefix")
             }
-            guard let addressPrefix6 = Int(address6Components[1]) else {
+            guard let vpnPrefix6 = Int(address6Components[1]) else {
                 throw StandardOpenVPNParserError.malformed(option: "ifconfig-ipv6 address prefix must be a 8-bit number")
             }
 
             let address6 = address6Components[0]
             defaultGateway6 = ifconfig6Arguments[1]
 
-            builder.ipv6 = IPSettings(subnet: try Subnet(address6, addressPrefix6))
-            if let defaultGateway6, let defaultGw = Address(rawValue: defaultGateway6) {
-                builder.ipv6 = builder.ipv6?.including(routes: [Route(defaultWithGateway: defaultGw)])
+            let vpnAddress6 = try Subnet(address6, 128)
+            var includedRoutes: [Route] = []
+            if let defaultGateway6, let defaultGw6Address = Address(rawValue: defaultGateway6) {
+                includedRoutes.append(Route(defaultWithGateway: defaultGw6Address))
             }
+            if let vpnNetwork6 = vpnAddress6.address.network(with: vpnPrefix6) {
+                let vpnDestination6 = try Subnet(vpnNetwork6.rawValue, vpnPrefix6)
+                includedRoutes.append(Route(vpnDestination6, vpnAddress6.address))
+            }
+
+            builder.ipv6 = IPSettings(subnet: vpnAddress6)
+                .including(routes: includedRoutes)
         } else {
             defaultGateway6 = nil
         }

Tests/OpenVPN/OpenVPNOpenSSL/PushReplyTests.swift
@@ -67,7 +67,7 @@ final class PushReplyTests: XCTestCase {
         reply.debug()
 
         XCTAssertEqual(reply.options.ipv4?.subnet?.address.rawValue, "10.8.0.2")
-        XCTAssertEqual(reply.options.ipv4?.subnet?.ipv4Mask, "255.255.255.0")
+        XCTAssertEqual(reply.options.ipv4?.subnet?.ipv4Mask, "255.255.255.255")
         XCTAssertEqual(reply.options.ipv4?.defaultGateway?.rawValue, "10.8.0.1")
         XCTAssertEqual(reply.options.dnsServers, ["8.8.8.8", "4.4.4.4"])
     }
@@ -99,10 +99,10 @@ final class PushReplyTests: XCTestCase {
         reply.debug()
 
         XCTAssertEqual(reply.options.ipv4?.subnet?.address.rawValue, "10.8.0.2")
-        XCTAssertEqual(reply.options.ipv4?.subnet?.ipv4Mask, "255.255.255.0")
+        XCTAssertEqual(reply.options.ipv4?.subnet?.ipv4Mask, "255.255.255.255")
         XCTAssertEqual(reply.options.ipv4?.defaultGateway?.rawValue, "10.8.0.1")
         XCTAssertEqual(reply.options.ipv6?.subnet?.address.rawValue, "fe80::601:30ff:feb7:ec01")
-        XCTAssertEqual(reply.options.ipv6?.subnet?.prefixLength, 64)
+        XCTAssertEqual(reply.options.ipv6?.subnet?.prefixLength, 128)
         XCTAssertEqual(reply.options.ipv6?.defaultGateway?.rawValue, "fe80::601:30ff:feb7:dc02")
         XCTAssertEqual(reply.options.dnsServers, ["2001:4860:4860::8888", "2001:4860:4860::8844"])
     }

Tests/OpenVPN/OpenVPNOpenSSL/StandardOpenVPNParserTests.swift
@@ -175,6 +175,26 @@ final class StandardOpenVPNParserTests: XCTestCase {
             _ = try? parser.parsed(fromLines: lines)
         }
     }
+
+    // MARK: Routes
+
+    func test_givenIfconfig_whenParse_thenIncludesNetworkRoutes() throws {
+        let line = "PUSH_REPLY,route-gateway 172.31.2.1,route-gateway 172.31.2.1,topology subnet,ping 10,ping-restart 120,route 192.168.41.0 255.255.255.0,route 192.168.50.0 255.255.255.0,route 192.168.42.0 255.255.255.0,route 192.168.43.0 255.255.255.0,route 192.168.44.0 255.255.255.0,route 192.168.45.0 255.255.255.0,route 192.168.70.0 255.255.255.0,route 192.168.31.0 255.255.255.0,dhcp-option DNS 192.168.41.2,dhcp-option DNS 192.168.44.11,ifconfig 172.31.2.6 255.255.255.0,ifconfig-ipv6 1234::12/10 1234::1,"
+
+        let pushReply = try XCTUnwrap(parser.pushReply(with: line))
+
+        XCTAssertEqual(pushReply.options.ipv4?.subnet, Subnet(rawValue: "172.31.2.6/32"))
+        XCTAssertEqual(pushReply.options.ipv4?.includedRoutes, [
+            Route(defaultWithGateway: Address(rawValue: "172.31.2.1")),
+            Route(Subnet(rawValue: "172.31.2.0/24"), Address(rawValue: "172.31.2.6"))
+        ])
+
+        XCTAssertEqual(pushReply.options.ipv6?.subnet, Subnet(rawValue: "1234::12/128"))
+        XCTAssertEqual(pushReply.options.ipv6?.includedRoutes, [
+            Route(defaultWithGateway: Address(rawValue: "1234::1")),
+            Route(Subnet(rawValue: "1200::/10"), Address(rawValue: "1234::12"))
+        ])
+    }
 }
 
 // MARK: - Helpers

.version
@@ -1,1 +1,1 @@
-0.99.100
+0.99.101

Package.swift
@@ -51,10 +51,10 @@ environment = .remoteBinary
 let areas: Set<Area> = Set(Area.allCases)
 
 // action-release-binary-package (PartoutCore)
-let sha1 = "201b69600b2c524bad95d7d2e4417d8eb6e0ee76"
+let sha1 = "d5f152a54d82cb7307f37e388a8b7fff06bb3a60"
 let binaryFilename = "PartoutCore.xcframework.zip"
-let version = "0.99.100"
-let checksum = "955651e7692023cffaafc52ea04f6513e8f6b40e70dff5a79b80bf3a6586230b"
+let version = "0.99.101"
+let checksum = "e7c8568d3cd35a9b7b85592b67bbfb52fd5b701739fff7150f256e1b96adfe81"
 
 let applePlatforms: [Platform] = [.iOS, .macOS, .tvOS]
 let nonApplePlatforms: [Platform] = [.android, .linux, .windows]

commit 5135705a4f32c3e38a29d572581785c86a16f547

OpenVPN: Fix regression in ifconfig parsing (#60)
Do not use /32 and /128 for the interface IP, revert to the provided
prefix/netmask. Then omit the gateway, which is probably the real
culprit. Including the route in the tunnel is enough. Route gateway
behavior in NE is, honestly, still unclear to me.

Regression in #49

Sources/OpenVPN/OpenVPNOpenSSL/StandardOpenVPNParser+Builder.swift
@@ -658,7 +658,7 @@ extension StandardOpenVPNParser.Builder {
             }
 
             let address4: String
-            let vpnMask4: String?
+            let vpnMask4: String
 
             let topology = Topology(rawValue: optTopology ?? "") ?? .net30
             switch topology {
@@ -672,20 +672,23 @@ extension StandardOpenVPNParser.Builder {
                 vpnMask4 = ifconfig4Arguments[1]
                 defaultGateway4 = gateway4Arguments[0]
 
-            default:
+            case .net30:
                 address4 = ifconfig4Arguments[0]
-                vpnMask4 = nil
+                vpnMask4 = "255.255.255.252"
                 defaultGateway4 = ifconfig4Arguments[1]
+
+            case .p2p:
+                throw StandardOpenVPNParserError.unsupportedConfiguration(option: "topology p2p")
             }
 
-            let vpnAddress4 = try Subnet(address4, 32)
+            let vpnAddress4 = try Subnet(address4, vpnMask4)
             var includedRoutes: [Route] = []
             if let defaultGateway4, let defaultGw4Address = Address(rawValue: defaultGateway4) {
                 includedRoutes.append(Route(defaultWithGateway: defaultGw4Address))
             }
-            if let vpnMask4, let vpnNetwork4 = vpnAddress4.address.network(with: vpnMask4) {
+            if let vpnNetwork4 = vpnAddress4.address.network(with: vpnMask4) {
                 let vpnDestination4 = try Subnet(vpnNetwork4.rawValue, vpnMask4)
-                includedRoutes.append(Route(vpnDestination4, vpnAddress4.address))
+                includedRoutes.append(Route(vpnDestination4, nil))
             }
 
             builder.ipv4 = IPSettings(subnet: vpnAddress4)
@@ -716,14 +719,14 @@ extension StandardOpenVPNParser.Builder {
             let address6 = address6Components[0]
             defaultGateway6 = ifconfig6Arguments[1]
 
-            let vpnAddress6 = try Subnet(address6, 128)
+            let vpnAddress6 = try Subnet(address6, vpnPrefix6)
             var includedRoutes: [Route] = []
             if let defaultGateway6, let defaultGw6Address = Address(rawValue: defaultGateway6) {
                 includedRoutes.append(Route(defaultWithGateway: defaultGw6Address))
             }
             if let vpnNetwork6 = vpnAddress6.address.network(with: vpnPrefix6) {
                 let vpnDestination6 = try Subnet(vpnNetwork6.rawValue, vpnPrefix6)
-                includedRoutes.append(Route(vpnDestination6, vpnAddress6.address))
+                includedRoutes.append(Route(vpnDestination6, nil))
             }
 
             builder.ipv6 = IPSettings(subnet: vpnAddress6)
@@ -796,6 +799,7 @@ private extension StandardOpenVPNParser.Builder {
     enum Topology: String {
         case net30
 
+        @available(*, deprecated, message: "Removed in 2.5.0")
         case p2p
 
         case subnet

Tests/OpenVPN/OpenVPNOpenSSL/PushReplyTests.swift
@@ -53,7 +53,7 @@ final class PushReplyTests: XCTestCase {
         reply.debug()
 
         XCTAssertEqual(reply.options.ipv4?.subnet?.address.rawValue, "10.5.10.6")
-        XCTAssertEqual(reply.options.ipv4?.subnet?.ipv4Mask, "255.255.255.255")
+        XCTAssertEqual(reply.options.ipv4?.subnet?.ipv4Mask, "255.255.255.252")
         XCTAssertEqual(reply.options.ipv4?.defaultGateway?.rawValue, "10.5.10.5")
         XCTAssertEqual(reply.options.dnsServers, ["209.222.18.222", "209.222.18.218"])
     }
@@ -67,7 +67,7 @@ final class PushReplyTests: XCTestCase {
         reply.debug()
 
         XCTAssertEqual(reply.options.ipv4?.subnet?.address.rawValue, "10.8.0.2")
-        XCTAssertEqual(reply.options.ipv4?.subnet?.ipv4Mask, "255.255.255.255")
+        XCTAssertEqual(reply.options.ipv4?.subnet?.ipv4Mask, "255.255.255.0")
         XCTAssertEqual(reply.options.ipv4?.defaultGateway?.rawValue, "10.8.0.1")
         XCTAssertEqual(reply.options.dnsServers, ["8.8.8.8", "4.4.4.4"])
     }
@@ -99,10 +99,10 @@ final class PushReplyTests: XCTestCase {
         reply.debug()
 
         XCTAssertEqual(reply.options.ipv4?.subnet?.address.rawValue, "10.8.0.2")
-        XCTAssertEqual(reply.options.ipv4?.subnet?.ipv4Mask, "255.255.255.255")
+        XCTAssertEqual(reply.options.ipv4?.subnet?.ipv4Mask, "255.255.255.0")
         XCTAssertEqual(reply.options.ipv4?.defaultGateway?.rawValue, "10.8.0.1")
         XCTAssertEqual(reply.options.ipv6?.subnet?.address.rawValue, "fe80::601:30ff:feb7:ec01")
-        XCTAssertEqual(reply.options.ipv6?.subnet?.prefixLength, 128)
+        XCTAssertEqual(reply.options.ipv6?.subnet?.prefixLength, 64)
         XCTAssertEqual(reply.options.ipv6?.defaultGateway?.rawValue, "fe80::601:30ff:feb7:dc02")
         XCTAssertEqual(reply.options.dnsServers, ["2001:4860:4860::8888", "2001:4860:4860::8844"])
     }

Tests/OpenVPN/OpenVPNOpenSSL/StandardOpenVPNParserTests.swift
@@ -183,16 +183,16 @@ final class StandardOpenVPNParserTests: XCTestCase {
 
         let pushReply = try XCTUnwrap(parser.pushReply(with: line))
 
-        XCTAssertEqual(pushReply.options.ipv4?.subnet, Subnet(rawValue: "172.31.2.6/32"))
+        XCTAssertEqual(pushReply.options.ipv4?.subnet, Subnet(rawValue: "172.31.2.6/24"))
         XCTAssertEqual(pushReply.options.ipv4?.includedRoutes, [
             Route(defaultWithGateway: Address(rawValue: "172.31.2.1")),
-            Route(Subnet(rawValue: "172.31.2.0/24"), Address(rawValue: "172.31.2.6"))
+            Route(Subnet(rawValue: "172.31.2.0/24"), nil)
         ])
 
-        XCTAssertEqual(pushReply.options.ipv6?.subnet, Subnet(rawValue: "1234::12/128"))
+        XCTAssertEqual(pushReply.options.ipv6?.subnet, Subnet(rawValue: "1234::12/10"))
         XCTAssertEqual(pushReply.options.ipv6?.includedRoutes, [
             Route(defaultWithGateway: Address(rawValue: "1234::1")),
-            Route(Subnet(rawValue: "1200::/10"), Address(rawValue: "1234::12"))
+            Route(Subnet(rawValue: "1200::/10"), nil)
         ])
     }
 }