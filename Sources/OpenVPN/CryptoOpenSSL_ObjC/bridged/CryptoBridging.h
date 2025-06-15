fb0ea0dc544f06e35c469243a4fc3aea0d314f6f
Davide <keeshux@gmail.com>
2025-06-15 19:42:24 +0200

    Add ObjC wrappers around C code

---
+++ Sources/OpenVPN/CryptoOpenSSL_ObjC/bridged/CryptoBridging.h
@@ -0,0 +1,46 @@
+//
+//  CryptoBridging.h
+//  Partout
+//
+//  Created by Davide De Rosa on 6/15/25.
+//  Copyright (c) 2025 Davide De Rosa. All rights reserved.
+//
+//  https://github.com/passepartoutvpn
+//
+//  This file is part of Partout.
+//
+//  Partout is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Partout is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Partout.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#import "CryptoMacros.h"
+#import "ZeroingData.h"
+#import "zeroing_data.h"
+
+@interface ZeroingData (C)
+
+- (zeroing_data_t *)ptr;
+
+@end
+
+static inline crypto_flags_t crypto_flags_from(const CryptoFlags *flags) {
+    crypto_flags_t cf = { 0 };
+    if (flags) {
+        cf.ad = flags->ad;
+        cf.ad_len = flags->adLength;
+        cf.iv = flags->iv;
+        cf.iv_len = flags->ivLength;
+        cf.for_testing = flags->forTesting;
+    }
+    return cf;
+}
