1a159adbd0b27a0c039f78c33494980bbc2af64f
Davide <keeshux@gmail.com>
2025-06-23 15:31:07 +0200

    Restore moved files

--- Sources/OpenVPN/OpenVPNOpenSSL/Internal/DataChannel.swift
+++ Sources/OpenVPN/OpenVPNOpenSSL/Internal/DataChannel.swift
@@ -25,37 +25,6 @@
 
 import Foundation
 import PartoutCore
-
-#if OPENVPN_WRAPPED
-
-final class DataChannel {
-    private let ctx: PartoutLoggerContext
-
-    let key: UInt8
-
-    private let dataPath: DataPathProtocol
-
-    init(_ ctx: PartoutLoggerContext, key: UInt8, dataPath: DataPathProtocol) {
-        self.ctx = ctx
-        self.key = key
-        self.dataPath = dataPath
-    }
-
-    func encrypt(packets: [Data]) throws -> [Data]? {
-        try dataPath.encrypt(packets, key: key)
-    }
-
-    func decrypt(packets: [Data]) throws -> [Data]? {
-        let result = try dataPath.decrypt(packets)
-        if result.keepAlive {
-            pp_log(ctx, .openvpn, .debug, "Data: Received ping, do nothing")
-        }
-        return result.packets
-    }
-}
-
-#else
-
 internal import _PartoutOpenVPNOpenSSL_ObjC
 
 final class DataChannel {
@@ -84,5 +53,3 @@ final class DataChannel {
         return decrypted
     }
 }
-
-#endif
