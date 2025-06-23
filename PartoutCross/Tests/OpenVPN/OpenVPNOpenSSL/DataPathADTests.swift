8029bfb4ce2c27b1e5631f79f48ecbce95d4cf67
Davide <keeshux@gmail.com>
2025-06-24 00:25:01 +0200

    Clean up test imports

--- PartoutCross/Tests/OpenVPN/OpenVPNOpenSSL/DataPathADTests.swift
+++ PartoutCross/Tests/OpenVPN/OpenVPNOpenSSL/DataPathADTests.swift
@@ -25,7 +25,6 @@
 
 import _PartoutOpenVPN
 @testable import _PartoutOpenVPNOpenSSL_Cross
-import PartoutCore
 import XCTest
 
 // for non-Swift symbols like DataPacket*
--- PartoutCross/Tests/OpenVPN/OpenVPNOpenSSL/DataPathHMACTests.swift
+++ PartoutCross/Tests/OpenVPN/OpenVPNOpenSSL/DataPathHMACTests.swift
@@ -25,7 +25,6 @@
 
 import _PartoutOpenVPN
 @testable import _PartoutOpenVPNOpenSSL_Cross
-import PartoutCore
 import XCTest
 
 final class DataPathHMACTests: XCTestCase, DataPathTestsProtocol {
