a5aa0de9d5a573b64e0a9b975e0ca4315d5439f3
Davide <keeshux@gmail.com>
2025-06-25 12:44:59 +0200

    Add tests about first stage processing
    Reversibility tests can pass even if processed == data.

--- Tests/OpenVPN/OpenVPNOpenSSL/XORProcessorTests.swift
+++ Tests/OpenVPN/OpenVPNOpenSSL/XORProcessorTests.swift
@@ -33,6 +33,84 @@ final class XORProcessorTests: XCTestCase {
 
     private let mask = SecureData("f76dab30")!
 
+    func test_givenProcessor_whenMask_thenIsExpected() {
+        let sut = XORProcessor(method: .xormask(mask: mask))
+        let data = prng.data(length: 10)
+        let maskData = mask.zData
+        let processed = sut.processPacket(data, outbound: false)
+        print(data.toHex())
+        print(processed.toHex())
+        for (i, byte) in processed.enumerated() {
+            XCTAssertEqual(byte, data[i] ^ maskData.bytes[i % maskData.length])
+        }
+    }
+
+    func test_givenProcessor_whenPtrPos_thenIsExpected() {
+        let sut = XORProcessor(method: .xorptrpos)
+        let data = prng.data(length: 10)
+        let processed = sut.processPacket(data, outbound: false)
+        print(data.toHex())
+        print(processed.toHex())
+        for (i, byte) in processed.enumerated() {
+            XCTAssertEqual(byte, data[i] ^ UInt8((i + 1) & 0xff))
+        }
+    }
+
+    func test_givenProcessor_whenReverse_thenIsExpected() {
+        let sut = XORProcessor(method: .reverse)
+        var data = prng.data(length: 10)
+        var processed = sut.processPacket(data, outbound: false)
+        print(data.toHex())
+        print(processed.toHex())
+
+        XCTAssertEqual(processed[0], data[0])
+        data.removeFirst()
+        processed.removeFirst()
+        print(data.toHex())
+        print(processed.toHex())
+        assert(data.count == 9)
+        assert(processed.count == 9)
+        XCTAssertEqual(processed, Data(data.reversed()))
+
+//        // this crashes as if it returned pre-removeFirst() offsets, bug in Data?
+//        for (i, byte) in processed.enumerated() {
+//            XCTAssertEqual(byte, data[data.count - i - 1])
+//        }
+//
+//        // this crashes for the same reason
+//        for (i, byte) in processed.reversed().enumerated() {
+//            XCTAssertEqual(byte, data[i])
+//        }
+    }
+
+    func test_givenProcessor_whenObfuscateOutbound_thenIsExpected() {
+        let sut = XORProcessor(method: .obfuscate(mask: mask))
+        let data = Data(hex: "832ae7598dfa0378bc19")
+        let processed = sut.processPacket(data, outbound: true)
+        let expected = Data(hex: "e52680106098bc658b15")
+
+        // original = "832ae7598dfa0378bc19"
+        // ptrpos   = "8228e45d88fc0470b513"
+        // reverse  = "8213b57004fc885de428"
+        // ptrpos   = "8311b67401fa8f55ed22"
+        // mask     = "e52680106098bc658b15"
+
+        print(data.toHex())
+        print(processed.toHex())
+        XCTAssertEqual(processed, expected)
+    }
+
+    func test_givenProcessor_whenObfuscateInbound_thenIsExpected() {
+        let sut = XORProcessor(method: .obfuscate(mask: mask))
+        let data = Data(hex: "e52680106098bc658b15")
+        let processed = sut.processPacket(data, outbound: false)
+        let expected = Data(hex: "832ae7598dfa0378bc19")
+
+        print(data.toHex())
+        print(processed.toHex())
+        XCTAssertEqual(processed, expected)
+    }
+
     func test_givenProcessor_whenMaskthenIsReversible() {
         let sut = XORProcessor(method: .xormask(mask: mask))
         sut.assertReversible(prng.data(length: 1000))
