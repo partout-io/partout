524d872eea0dace23e91bb749a3560c0df8f23da
Davide <keeshux@gmail.com>
2025-07-04 23:31:12 +0200

    Migrate core tests

--- Tests/OpenVPN/Core/ConfigurationBuilderTests.swift
+++ Tests/OpenVPN/Core/ConfigurationBuilderTests.swift
@@ -25,26 +25,28 @@
 
 import _PartoutOpenVPNCore
 import Foundation
-import XCTest
+import Testing
 
-final class ConfigurationBuilderTests: XCTestCase {
-    func test_givenBuilder_whenClient_thenHasFallbackValues() throws {
+struct ConfigurationBuilderTests {
+    @Test
+    func givenBuilder_whenClient_thenHasFallbackValues() throws {
         var sut = OpenVPN.Configuration.Builder()
         sut.ca = .init(pem: "")
         sut.remotes = [.init(rawValue: "1.2.3.4:UDP:1000")!]
         let cfg = try sut.tryBuild(isClient: true)
-        XCTAssertEqual(cfg.cipher, .aes128cbc)
-        XCTAssertNil(cfg.digest)
-        XCTAssertNil(cfg.compressionFraming)
-        XCTAssertNil(cfg.compressionAlgorithm)
+        #expect(cfg.cipher == .aes128cbc)
+        #expect(cfg.digest == nil)
+        #expect(cfg.compressionFraming == nil)
+        #expect(cfg.compressionAlgorithm == nil)
     }
 
-    func test_givenBuilder_whenNonClient_thenHasNoFallbackValues() throws {
+    @Test
+    func givenBuilder_whenNonClient_thenHasNoFallbackValues() throws {
         let sut = OpenVPN.Configuration.Builder()
         let cfg = try sut.tryBuild(isClient: false)
-        XCTAssertNil(cfg.cipher)
-        XCTAssertNil(cfg.digest)
-        XCTAssertNil(cfg.compressionFraming)
-        XCTAssertNil(cfg.compressionAlgorithm)
+        #expect(cfg.cipher == nil)
+        #expect(cfg.digest == nil)
+        #expect(cfg.compressionFraming == nil)
+        #expect(cfg.compressionAlgorithm == nil)
     }
 }
