fef4f8cfac94c3e8233cb8e567ed4979868b8f3f
Davide <keeshux@gmail.com>
2025-07-09 16:05:11 +0200

    Fix license headers

--- Tests/WireGuard/WireGuardGo/MappersTests.swift
+++ Tests/WireGuard/WireGuardGo/MappersTests.swift
@@ -1,26 +1,26 @@
 //
 //  MappersTests.swift
-//  Passepartout
+//  Partout
 //
 //  Created by Davide De Rosa on 2/19/25.
 //  Copyright (c) 2025 Davide De Rosa. All rights reserved.
 //
 //  https://github.com/passepartoutvpn
 //
-//  This file is part of Passepartout.
+//  This file is part of Partout.
 //
-//  Passepartout is free software: you can redistribute it and/or modify
+//  Partout is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
 //  the Free Software Foundation, either version 3 of the License, or
 //  (at your option) any later version.
 //
-//  Passepartout is distributed in the hope that it will be useful,
+//  Partout is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU General Public License for more details.
 //
 //  You should have received a copy of the GNU General Public License
-//  along with Passepartout.  If not, see <http://www.gnu.org/licenses/>.
+//  along with Partout.  If not, see <http://www.gnu.org/licenses/>.
 //
 
 import _PartoutWireGuardCore
