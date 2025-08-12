partout-io/partout

commit 6da45e59247387e9345aba72d56d5874dc14fd0c

Normalize naming conventions for C ABI (#164)
Follow some naming conventions to highlight the C signatures of the
library by a prefix based on the access:

- Public = `partout_`, ABI must be stable here (e.g.
`partout_version()`)
- Package = `pp_`, functions used across all targets, but only
internally (e.g. `pp_zd_`, `pp_prng_`)
- Private = `<context>_`, target-specific, like OpenVPN

Case:

- `openvpn_` in types and functions
- `OpenVPN` in enums and constants/defines
- `OPENVPN_` in macros

Deeper renamings:

- `zeroing_data` → `pp_zd`
- `crypto_<alg>_ctx` → `pp_crypto_<alg>` (privately used in .c files,
`pp_crypto_ctx` as generic pointer)
- `tls_channel` → `pp_tls`
- `ctrl_pkt` → `openvpn_ctrl`
- Drop the trailing `_t` from all custom types

Targets:

- Move `ABI` targets to root, with Swift for impl and C for public
headers
- Move `API` targets to root
- Move `Crypto` targets to root, out of `Vendors`
- Move generic C utils from `Crypto` to `Vendors/Portable` (zd, prng,
asserts etc.)

Lastly, bring GenericJSON in to have zero dependencies except vendored
crypto/connectivity libraries.

Sources/Crypto/TLSOpenSSL_C/tls.c
@@ -1,0 +1,412 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Davide De Rosa
+ *
+ * SPDX-License-Identifier: GPL-3.0
+ */
+
+#include <openssl/bio.h>
+#include <openssl/ssl.h>
+#include <openssl/x509v3.h>
+#include <openssl/err.h>
+#include <stdio.h>
+#include "portable/common.h"
+#include "tls/tls.h"
+#include "tls/macros.h"
+
+//static const char *const TLSBoxClientEKU = "TLS Web Client Authentication";
+static const char *const TLSBoxServerEKU = "TLS Web Server Authentication";
+
+static int PPTLSExDataIdx = -1;
+
+struct pp_tls {
+    const pp_tls_options *_Nonnull opt;
+    SSL_CTX *_Nonnull ssl_ctx;
+    size_t buf_len;
+    uint8_t *_Nonnull buf_cipher;
+    uint8_t *_Nonnull buf_plain;
+
+    SSL *_Nonnull ssl;
+    BIO *_Nonnull bio_plain;
+    BIO *_Nonnull bio_cipher_in;
+    BIO *_Nonnull bio_cipher_out;
+    bool is_connected;
+};
+
+static
+BIO *create_BIO_from_PEM(const char *_Nonnull pem) {
+    return BIO_new_mem_buf(pem, (int)strlen(pem));
+}
+
+static
+int pp_tls_verify_peer(int ok, X509_STORE_CTX *_Nonnull ctx) {
+    if (!ok) {
+        SSL *ssl = X509_STORE_CTX_get_ex_data(ctx, SSL_get_ex_data_X509_STORE_CTX_idx());
+        pp_tls_ctx tls = SSL_get_ex_data(ssl, PPTLSExDataIdx);
+        tls->opt->on_verify_failure();
+    }
+    return ok;
+}
+
+// MARK: -
+
+pp_tls_ctx pp_tls_create(const pp_tls_options *opt, pp_tls_error_code *error) {
+    SSL_CTX *ssl_ctx = SSL_CTX_new(TLS_client_method());
+    X509 *cert = NULL;
+    BIO *cert_bio = NULL;
+    EVP_PKEY *pkey = NULL;
+    BIO *pkey_bio = NULL;
+
+    SSL_CTX_set_options(ssl_ctx, SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3 | SSL_OP_NO_COMPRESSION);
+    SSL_CTX_set_verify(ssl_ctx, SSL_VERIFY_PEER, pp_tls_verify_peer);
+    SSL_CTX_set_security_level(ssl_ctx, opt->sec_level);
+
+    if (opt->ca_path) {
+        if (!SSL_CTX_load_verify_locations(ssl_ctx, opt->ca_path, NULL)) {
+            PP_CRYPTO_SET_ERROR(PPTLSErrorCAUse)
+            goto failure;
+        }
+    }
+    if (opt->cert_pem) {
+        cert_bio = create_BIO_from_PEM(opt->cert_pem);
+        if (!cert_bio) {
+            PP_CRYPTO_SET_ERROR(PPTLSErrorClientCertificateRead)
+            goto failure;
+        }
+        cert = PEM_read_bio_X509(cert_bio, NULL, NULL, NULL);
+        if (!cert) {
+            PP_CRYPTO_SET_ERROR(PPTLSErrorClientCertificateRead)
+            goto failure;
+        }
+        if (!SSL_CTX_use_certificate(ssl_ctx, cert)) {
+            PP_CRYPTO_SET_ERROR(PPTLSErrorClientCertificateUse)
+            goto failure;
+        }
+        X509_free(cert);
+        BIO_free(cert_bio);
+
+        if (opt->key_pem) {
+            pkey_bio = create_BIO_from_PEM(opt->key_pem);
+            if (!pkey_bio) {
+                PP_CRYPTO_SET_ERROR(PPTLSErrorClientKeyRead)
+                goto failure;
+            }
+            pkey = PEM_read_bio_PrivateKey(pkey_bio, NULL, NULL, NULL);
+            if (!pkey) {
+                PP_CRYPTO_SET_ERROR(PPTLSErrorClientKeyRead)
+                goto failure;
+            }
+            if (!SSL_CTX_use_PrivateKey(ssl_ctx, pkey)) {
+                PP_CRYPTO_SET_ERROR(PPTLSErrorClientKeyUse)
+                goto failure;
+            }
+            EVP_PKEY_free(pkey);
+            BIO_free(pkey_bio);
+        }
+    }
+
+    // no longer fails
+
+    pp_tls_ctx tls = pp_alloc_crypto(sizeof(pp_tls));
+    tls->opt = opt;
+    tls->ssl_ctx = ssl_ctx;
+    tls->buf_len = tls->opt->buf_len;
+    tls->buf_cipher = pp_alloc_crypto(tls->buf_len);
+    tls->buf_plain = pp_alloc_crypto(tls->buf_len);
+    return tls;
+
+failure:
+    ERR_print_errors_fp(stdout);
+    SSL_CTX_free(ssl_ctx);
+    if (cert) X509_free(cert);
+    if (cert_bio) BIO_free(cert_bio);
+    if (pkey) EVP_PKEY_free(pkey);
+    if (pkey_bio) BIO_free(pkey_bio);
+    return NULL;
+}
+
+void pp_tls_free(pp_tls_ctx tls) {
+    if (!tls) return;
+
+    // DO NOT FREE these due to use in BIO_set_ssl() macro
+//    if (self.bioCipherTextIn) {
+//        BIO_free(self.bioCipherTextIn);
+//    }
+//    if (self.bioCipherTextOut) {
+//        BIO_free(self.bioCipherTextOut);
+//    }
+    if (tls->bio_plain) {
+        BIO_free_all(tls->bio_plain);
+    }
+    if (tls->ssl) {
+        SSL_free(tls->ssl);
+    }
+
+    pp_zero(tls->buf_cipher, tls->opt->buf_len);
+    pp_zero(tls->buf_plain, tls->opt->buf_len);
+    free(tls->buf_cipher);
+    free(tls->buf_plain);
+    pp_tls_options_free((pp_tls_options *)tls->opt);
+    SSL_CTX_free(tls->ssl_ctx);
+}
+
+bool pp_tls_start(pp_tls_ctx _Nonnull tls) {
+    if (tls->bio_plain) {
+        BIO_free_all(tls->bio_plain);
+        tls->bio_plain = NULL;
+        tls->bio_cipher_in = NULL;
+        tls->bio_cipher_out = NULL;
+    }
+    if (tls->ssl) {
+        SSL_free(tls->ssl);
+        tls->ssl = NULL;
+    }
+    pp_zero(tls->buf_cipher, tls->opt->buf_len);
+    pp_zero(tls->buf_plain, tls->opt->buf_len);
+    tls->is_connected = false;
+
+    tls->ssl = SSL_new(tls->ssl_ctx);
+    tls->bio_plain = BIO_new(BIO_f_ssl());
+    tls->bio_cipher_in = BIO_new(BIO_s_mem());
+    tls->bio_cipher_out = BIO_new(BIO_s_mem());
+
+    SSL_set_connect_state(tls->ssl);
+    SSL_set_bio(tls->ssl, tls->bio_cipher_in, tls->bio_cipher_out);
+    BIO_set_ssl(tls->bio_plain, tls->ssl, BIO_NOCLOSE);
+
+    // attach custom object
+    SSL_set_ex_data(tls->ssl, PPTLSExDataIdx, tls);
+
+    return SSL_do_handshake(tls->ssl);
+}
+
+bool pp_tls_is_connected(pp_tls_ctx _Nonnull tls) {
+    return tls->is_connected;
+}
+
+// MARK: - I/O
+
+bool pp_tls_verify_ssl_eku(SSL *_Nonnull ssl);
+bool pp_tls_verify_ssl_san_host(SSL *_Nonnull ssl, const char *_Nonnull hostname);
+
+pp_zd *_Nullable pp_tls_pull_cipher(pp_tls_ctx _Nonnull tls,
+                                                  pp_tls_error_code *_Nullable error) {
+    if (error) {
+        *error = PPTLSErrorNone;
+    }
+    if (!tls->is_connected && !SSL_is_init_finished(tls->ssl)) {
+        SSL_do_handshake(tls->ssl);
+    }
+    const int ret = BIO_read(tls->bio_cipher_out, tls->buf_cipher, (int)tls->opt->buf_len);
+    if (!tls->is_connected && SSL_is_init_finished(tls->ssl)) {
+        tls->is_connected = true;
+        if (tls->opt->eku && !pp_tls_verify_ssl_eku(tls->ssl)) {
+            if (error) {
+                *error = PPTLSErrorServerEKU;
+            }
+            return NULL;
+        }
+        if (tls->opt->san_host) {
+            pp_assert(tls->opt->hostname);
+            if (!pp_tls_verify_ssl_san_host(tls->ssl, tls->opt->hostname)) {
+                if (error) {
+                    *error = PPTLSErrorServerHost;
+                }
+                return NULL;
+            }
+        }
+    }
+    if ((ret < 0) && !BIO_should_retry(tls->bio_cipher_out)) {
+        if (error) {
+            *error = PPTLSErrorHandshake;
+        }
+        return NULL;
+    }
+    if (ret <= 0) {
+        return NULL;
+    }
+    return pp_zd_create_from_data(tls->buf_cipher, ret);
+}
+
+pp_zd *_Nullable pp_tls_pull_plain(pp_tls_ctx _Nonnull tls,
+                                                 pp_tls_error_code *_Nullable error) {
+    const int ret = BIO_read(tls->bio_plain, tls->buf_plain, (int)tls->opt->buf_len);
+    if (error) {
+        *error = PPTLSErrorNone;
+    }
+    if ((ret < 0) && !BIO_should_retry(tls->bio_plain)) {
+        if (error) {
+            *error = PPTLSErrorHandshake;
+        }
+        return NULL;
+    }
+    if (ret <= 0) {
+        return NULL;
+    }
+    return pp_zd_create_from_data(tls->buf_plain, ret);
+}
+
+bool pp_tls_put_cipher(pp_tls_ctx _Nonnull tls,
+                            const uint8_t *_Nonnull src, size_t src_len,
+                            pp_tls_error_code *_Nullable error) {
+    if (error) {
+        *error = PPTLSErrorNone;
+    }
+    const int ret = BIO_write(tls->bio_cipher_in, src, (int)src_len);
+    if (ret != (int)src_len) {
+        if (error) {
+            *error = PPTLSErrorHandshake;
+        }
+        return false;
+    }
+    return true;
+}
+
+bool pp_tls_put_plain(pp_tls_ctx _Nonnull tls,
+                           const uint8_t *_Nonnull src, size_t src_len,
+                           pp_tls_error_code *_Nullable error) {
+    if (error) {
+        *error = PPTLSErrorNone;
+    }
+    const int ret = BIO_write(tls->bio_plain, src, (int)src_len);
+    if (ret != (int)src_len) {
+        if (error) {
+            *error = PPTLSErrorHandshake;
+        }
+        return false;
+    }
+    return true;
+}
+
+// MARK: - MD5
+
+char *pp_tls_ca_md5(const pp_tls_ctx tls) {
+    const EVP_MD *alg = EVP_get_digestbyname("MD5");
+    uint8_t md[16];
+    unsigned int len;
+
+    FILE *pem = fopen(tls->opt->ca_path, "r");
+    if (!pem) {
+        goto failure;
+    }
+    X509 *cert = PEM_read_X509(pem, NULL, NULL, NULL);
+    if (!cert) {
+        goto failure;
+    }
+    X509_digest(cert, alg, md, &len);
+    X509_free(cert);
+    fclose(pem);
+    pp_assert(len == sizeof(md));//, @"Unexpected MD5 size (%d != %lu)", len, sizeof(md));
+
+    char *hex = pp_alloc_crypto(2 * sizeof(md) + 1);
+    char *ptr = hex;
+    for (size_t i = 0; i < sizeof(md); ++i) {
+        ptr += snprintf(ptr, 2, "%02x", md[i]);
+    }
+    *ptr = '\0';
+    return hex;
+
+failure:
+    if (pem) fclose(pem);
+    return NULL;
+}
+
+// MARK: - Verifications
+
+bool pp_tls_verify_ssl_eku(SSL *_Nonnull ssl) {
+    X509 *cert = NULL;
+    EXTENDED_KEY_USAGE *eku = NULL;
+
+    cert = SSL_get1_peer_certificate(ssl);
+    if (!cert) {
+        goto failure;
+    }
+
+    // don't be afraid of saving some time:
+    //
+    // https://stackoverflow.com/questions/37047379/how-extract-all-oids-from-certificate-with-openssl
+    //
+    const int ext_index = X509_get_ext_by_NID(cert, NID_ext_key_usage, -1);
+    if (ext_index < 0) {
+        goto failure;
+    }
+    X509_EXTENSION *ext = X509_get_ext(cert, ext_index);
+    if (!ext) {
+        goto failure;
+    }
+    eku = X509V3_EXT_d2i(ext);
+    if (!eku) {
+        goto failure;
+    }
+
+    const int num = (int)sk_ASN1_OBJECT_num(eku);
+    char buffer[100];
+    bool is_valid = false;
+    for (int i = 0; i < num; ++i) {
+        OBJ_obj2txt(buffer, sizeof(buffer), sk_ASN1_OBJECT_value(eku, i), 1); // get OID
+        const char *oid = OBJ_nid2ln(OBJ_obj2nid(sk_ASN1_OBJECT_value(eku, i)));
+        if (oid && !strcmp(oid, TLSBoxServerEKU)) {
+            is_valid = true;
+            break;
+        }
+    }
+    EXTENDED_KEY_USAGE_free(eku);
+    X509_free(cert);
+    return is_valid;
+
+failure:
+    if (eku) EXTENDED_KEY_USAGE_free(eku);
+    if (cert) X509_free(cert);
+    return false;
+}
+
+bool pp_tls_verify_ssl_san_host(SSL *_Nonnull ssl, const char *_Nonnull hostname) {
+    X509 *cert = NULL;
+    GENERAL_NAMES *names = NULL;
+
+    cert = SSL_get1_peer_certificate(ssl);
+    if (!cert) {
+        goto failure;
+    }
+    names = X509_get_ext_d2i(cert, NID_subject_alt_name, 0, 0);
+    if (!names) {
+        goto failure;
+    }
+    const int count = (int)sk_GENERAL_NAME_num(names);
+    if (!count) {
+        goto failure;
+    }
+
+    bool is_valid = false;
+    for (int i = 0; i < count; ++i) {
+        GENERAL_NAME* entry = sk_GENERAL_NAME_value(names, i);
+        if (!entry || entry->type != GEN_DNS) {
+            continue;
+        }
+        unsigned char *ns_name = NULL;
+        const int len1 = ASN1_STRING_to_UTF8(&ns_name, entry->d.dNSName);
+        if (!ns_name) {
+            continue;
+        }
+        const int len2 = (int)strlen((const char *)ns_name);
+        if (len1 != len2) {
+            OPENSSL_free(ns_name);
+            ns_name = NULL;
+            continue;
+        }
+        if (ns_name && len1 && len2 && (len1 == len2) && strcmp((const char *)ns_name, hostname) == 0) {
+            OPENSSL_free(ns_name);
+            ns_name = NULL;
+            is_valid = true;
+            break;
+        }
+    }
+
+    GENERAL_NAMES_free(names);
+    X509_free(cert);
+    return is_valid;
+
+failure:
+    if (names) GENERAL_NAMES_free(names);
+    if (cert) X509_free(cert);
+    return false;
+}

Sources/OpenVPN/Cross_C/include/openvpn/dp_mode.h
@@ -18,46 +18,46 @@
 typedef struct {
     void *_Nonnull mode;
     uint32_t packet_id;
-    zeroing_data_t *_Nonnull dst;
+    pp_zd *_Nonnull dst;
     const uint8_t *_Nonnull src;
     size_t src_len;
-} dp_mode_assemble_ctx;
+} openvpn_dp_mode_assemble_ctx;
 
 // encrypt -> SEND
 typedef struct {
     uint8_t key;
     uint32_t packet_id;
-    zeroing_data_t *_Nonnull dst;
+    pp_zd *_Nonnull dst;
     const uint8_t *_Nonnull src;
     size_t src_len;
-    dp_error_t *_Nullable error;
-} dp_mode_encrypt_ctx;
+    openvpn_dp_error *_Nullable error;
+} openvpn_dp_mode_encrypt_ctx;
 
-typedef size_t (*dp_mode_assemble_fn)(void *_Nonnull mode);
-typedef size_t (*dp_mode_encrypt_fn)(void *_Nonnull mode);
+typedef size_t (*openvpn_dp_mode_assemble_fn)(void *_Nonnull mode);
+typedef size_t (*openvpn_dp_mode_encrypt_fn)(void *_Nonnull mode);
 
 // MARK: - Inbound
 
 // RECEIVE -> decrypt
 typedef struct {
-    zeroing_data_t *_Nonnull dst;
+    pp_zd *_Nonnull dst;
     uint32_t *_Nonnull dst_packet_id;
     const uint8_t *_Nonnull src;
     size_t src_len;
-    dp_error_t *_Nullable error;
-} dp_mode_decrypt_ctx;
+    openvpn_dp_error *_Nullable error;
+} openvpn_dp_mode_decrypt_ctx;
 
 // decrypt -> parse
 typedef struct {
-    zeroing_data_t *_Nonnull dst;
+    pp_zd *_Nonnull dst;
     uint8_t *_Nonnull dst_header;
     uint8_t *_Nonnull src; // allow parse in place
     size_t src_len;
-    dp_error_t *_Nullable error;
-} dp_mode_parse_ctx;
+    openvpn_dp_error *_Nullable error;
+} openvpn_dp_mode_parse_ctx;
 
-typedef size_t (*dp_mode_decrypt_fn)(void *_Nonnull mode);
-typedef size_t (*dp_mode_parse_fn)(void *_Nonnull mode);
+typedef size_t (*openvpn_dp_mode_decrypt_fn)(void *_Nonnull mode);
+typedef size_t (*openvpn_dp_mode_parse_fn)(void *_Nonnull mode);
 
 // MARK: - Mode
 
@@ -74,12 +74,12 @@ typedef size_t (*dp_mode_parse_fn)(void *_Nonnull mode);
     - Parses packet from payload
 
  The way packets are encrypted and decrypted is delegated to
- the crypto_*_t types in the CryptoOpenSSL target. On the other
+ the pp_crypto_*_t types in the CryptoOpenSSL target. On the other
  hand, the way payloads are assembled and parsed depends on
  two factors:
 
  - The encryption mode (AD or HMAC, where AD = associated data)
- - The compression framing (see compression_framing_t)
+ - The compression framing (see openvpn_compression_framing)
 
  Only AEAD (AD) and CBC (HMAC) algorithms are supported for
  data transfer at this time.
@@ -88,68 +88,68 @@ typedef size_t (*dp_mode_parse_fn)(void *_Nonnull mode);
 #include "openvpn/packet.h"
 
 typedef struct {
-    dp_framing_assemble_fn _Nullable framing_assemble;
-    dp_mode_assemble_fn _Nonnull assemble;
-    crypto_encrypt_fn _Nonnull raw_encrypt;
-    dp_mode_encrypt_fn _Nonnull encrypt;
-} dp_mode_encrypter_t;
+    openvpn_dp_framing_assemble_fn _Nullable framing_assemble;
+    openvpn_dp_mode_assemble_fn _Nonnull assemble;
+    pp_crypto_encrypt_fn _Nonnull raw_encrypt;
+    openvpn_dp_mode_encrypt_fn _Nonnull encrypt;
+} openvpn_dp_mode_encrypter;
 
 typedef struct {
-    dp_framing_parse_fn _Nullable framing_parse;
-    dp_mode_parse_fn _Nonnull parse;
-    crypto_decrypt_fn _Nonnull raw_decrypt;
-    dp_mode_decrypt_fn _Nonnull decrypt;
-} dp_mode_decrypter_t;
+    openvpn_dp_framing_parse_fn _Nullable framing_parse;
+    openvpn_dp_mode_parse_fn _Nonnull parse;
+    pp_crypto_decrypt_fn _Nonnull raw_decrypt;
+    openvpn_dp_mode_decrypt_fn _Nonnull decrypt;
+} openvpn_dp_mode_decrypter;
 
 typedef struct {
-    compression_framing_t comp_f;
+    openvpn_compression_framing comp_f;
     uint32_t peer_id;
     uint16_t mss_val;
-} dp_mode_options_t;
+} openvpn_dp_mode_options;
 
 typedef struct {
     void *_Nonnull crypto;
-    crypto_free_fn _Nonnull crypto_free;
-    dp_mode_encrypter_t enc;
-    dp_mode_decrypter_t dec;
-    dp_mode_options_t opt;
+    pp_crypto_free_fn _Nonnull pp_crypto_free;
+    openvpn_dp_mode_encrypter enc;
+    openvpn_dp_mode_decrypter dec;
+    openvpn_dp_mode_options opt;
 
-    dp_mode_assemble_ctx assemble_ctx;
-    dp_mode_encrypt_ctx enc_ctx;
-    dp_mode_decrypt_ctx dec_ctx;
-    dp_mode_parse_ctx parse_ctx;
-} dp_mode_t;
+    openvpn_dp_mode_assemble_ctx assemble_ctx;
+    openvpn_dp_mode_encrypt_ctx enc_ctx;
+    openvpn_dp_mode_decrypt_ctx dec_ctx;
+    openvpn_dp_mode_parse_ctx parse_ctx;
+} openvpn_dp_mode;
 
 // "crypto" is owned and released on free
 
-dp_mode_t *_Nonnull dp_mode_create_opt(crypto_ctx _Nonnull crypto,
-                                       crypto_free_fn _Nonnull crypto_free,
-                                       const dp_mode_encrypter_t *_Nonnull enc,
-                                       const dp_mode_decrypter_t *_Nonnull dec,
-                                       const dp_mode_options_t *_Nullable opt);
+openvpn_dp_mode *_Nonnull openvpn_dp_mode_create_opt(pp_crypto_ctx _Nonnull crypto,
+                                       pp_crypto_free_fn _Nonnull pp_crypto_free,
+                                       const openvpn_dp_mode_encrypter *_Nonnull enc,
+                                       const openvpn_dp_mode_decrypter *_Nonnull dec,
+                                       const openvpn_dp_mode_options *_Nullable opt);
 
 static inline
-dp_mode_t *_Nonnull dp_mode_create(crypto_ctx _Nonnull crypto,
-                                   crypto_free_fn _Nonnull crypto_free,
-                                   const dp_mode_encrypter_t *_Nonnull enc,
-                                   const dp_mode_decrypter_t *_Nonnull dec) {
-    return dp_mode_create_opt(crypto, crypto_free, enc, dec, NULL);
+openvpn_dp_mode *_Nonnull openvpn_dp_mode_create(pp_crypto_ctx _Nonnull crypto,
+                                   pp_crypto_free_fn _Nonnull pp_crypto_free,
+                                   const openvpn_dp_mode_encrypter *_Nonnull enc,
+                                   const openvpn_dp_mode_decrypter *_Nonnull dec) {
+    return openvpn_dp_mode_create_opt(crypto, pp_crypto_free, enc, dec, NULL);
 }
 
-void dp_mode_free(dp_mode_t * _Nonnull);
+void openvpn_dp_mode_free(openvpn_dp_mode * _Nonnull);
 
 static inline
-uint32_t dp_mode_peer_id(dp_mode_t *_Nonnull mode) {
+uint32_t openvpn_dp_mode_peer_id(openvpn_dp_mode *_Nonnull mode) {
     return mode->opt.peer_id;
 }
 
 static inline
-void dp_mode_set_peer_id(dp_mode_t *_Nonnull mode, uint32_t peer_id) {
-    mode->opt.peer_id = peer_id_masked(peer_id);
+void openvpn_dp_mode_set_peer_id(openvpn_dp_mode *_Nonnull mode, uint32_t peer_id) {
+    mode->opt.peer_id = openvpn_peer_id_masked(peer_id);
 }
 
 static inline
-compression_framing_t dp_mode_framing(const dp_mode_t *_Nonnull mode) {
+openvpn_compression_framing openvpn_dp_mode_framing(const openvpn_dp_mode *_Nonnull mode) {
     return mode->opt.comp_f;
 }
 
@@ -160,110 +160,110 @@ compression_framing_t dp_mode_framing(const dp_mode_t *_Nonnull mode) {
 // HMAC = assemble_capacity(len) + sizeof(uint32_t)
 //
 static inline
-size_t dp_mode_assemble_capacity(const dp_mode_t *_Nonnull mode, size_t len) {
+size_t openvpn_dp_mode_assemble_capacity(const openvpn_dp_mode *_Nonnull mode, size_t len) {
     (void)mode;
-    return dp_framing_assemble_capacity(len) + sizeof(uint32_t);
+    return openvpn_dp_framing_assemble_capacity(len) + sizeof(uint32_t);
 }
 
 //
-// AD = PacketOpcodeLength + PacketPeerIdLength + meta.encryption_capacity(len)
-// HMAC = PacketOpcodeLength + meta.encryption_capacity(len)
+// AD = OpenVPNPacketOpcodeLength + PacketPeerIdLength + meta.encryption_capacity(len)
+// HMAC = OpenVPNPacketOpcodeLength + meta.encryption_capacity(len)
 //
 static inline
-size_t dp_mode_encrypt_capacity(const dp_mode_t *_Nonnull mode, size_t len) {
-    const crypto_ctx ctx = mode->crypto;
-    const size_t max_prefix_len = PacketOpcodeLength + PacketPeerIdLength;
-    const size_t enc_len = crypto_encryption_capacity(ctx, len);
+size_t openvpn_dp_mode_encrypt_capacity(const openvpn_dp_mode *_Nonnull mode, size_t len) {
+    const pp_crypto_ctx ctx = mode->crypto;
+    const size_t max_prefix_len = OpenVPNPacketOpcodeLength + OpenVPNPacketPeerIdLength;
+    const size_t enc_len = pp_crypto_encryption_capacity(ctx, len);
     return max_prefix_len + enc_len;
 }
 
 static inline
-size_t dp_mode_assemble_and_encrypt_capacity(const dp_mode_t *_Nonnull mode, size_t len) {
-    return dp_mode_encrypt_capacity(mode, dp_mode_assemble_capacity(mode, len));
+size_t openvpn_dp_mode_assemble_and_encrypt_capacity(const openvpn_dp_mode *_Nonnull mode, size_t len) {
+    return openvpn_dp_mode_encrypt_capacity(mode, openvpn_dp_mode_assemble_capacity(mode, len));
 }
 
-size_t dp_mode_assemble(dp_mode_t *_Nonnull mode,
+size_t openvpn_dp_mode_assemble(openvpn_dp_mode *_Nonnull mode,
                         uint32_t packet_id,
-                        zeroing_data_t *_Nonnull dst,
+                        pp_zd *_Nonnull dst,
                         const uint8_t *_Nonnull src,
                         size_t src_len);
 
-size_t dp_mode_encrypt(dp_mode_t *_Nonnull mode,
+size_t openvpn_dp_mode_encrypt(openvpn_dp_mode *_Nonnull mode,
                        uint8_t key,
                        uint32_t packet_id,
-                       zeroing_data_t *_Nonnull dst,
+                       pp_zd *_Nonnull dst,
                        const uint8_t *_Nonnull src,
                        size_t src_len,
-                       dp_error_t *_Nullable error);
+                       openvpn_dp_error *_Nullable error);
 
 static inline
-zeroing_data_t *_Nullable dp_mode_assemble_and_encrypt(dp_mode_t *_Nonnull mode,
+pp_zd *_Nullable openvpn_dp_mode_assemble_and_encrypt(openvpn_dp_mode *_Nonnull mode,
                                                        uint8_t key,
                                                        uint32_t packet_id,
-                                                       zeroing_data_t *_Nonnull buf,
+                                                       pp_zd *_Nonnull buf,
                                                        const uint8_t *_Nonnull src,
                                                        size_t src_len,
-                                                       dp_error_t *_Nullable error) {
+                                                       openvpn_dp_error *_Nullable error) {
 
-    pp_assert(buf->length >= dp_mode_assemble_and_encrypt_capacity(mode, src_len));
-    const size_t asm_len = dp_mode_assemble(mode, packet_id, buf,
+    pp_assert(buf->length >= openvpn_dp_mode_assemble_and_encrypt_capacity(mode, src_len));
+    const size_t asm_len = openvpn_dp_mode_assemble(mode, packet_id, buf,
                                             src, src_len);
     if (!asm_len) {
         return NULL;
     }
-    zeroing_data_t *dst = zd_create(dp_mode_encrypt_capacity(mode, asm_len));
-    const size_t dst_len = dp_mode_encrypt(mode, key, packet_id, dst,
+    pp_zd *dst = pp_zd_create(openvpn_dp_mode_encrypt_capacity(mode, asm_len));
+    const size_t dst_len = openvpn_dp_mode_encrypt(mode, key, packet_id, dst,
                                            buf->bytes, asm_len, error);
     if (!dst_len) {
-        zd_free(dst);
+        pp_zd_free(dst);
         return NULL;
     }
-    zd_resize(dst, dst_len);
+    pp_zd_resize(dst, dst_len);
     return dst;
 }
 
 // MARK: - Decryption
 
-size_t dp_mode_decrypt(dp_mode_t *_Nonnull mode,
-                       zeroing_data_t *_Nonnull dst,
+size_t openvpn_dp_mode_decrypt(openvpn_dp_mode *_Nonnull mode,
+                       pp_zd *_Nonnull dst,
                        uint32_t *_Nonnull dst_packet_id,
                        const uint8_t *_Nonnull src,
                        size_t src_len,
-                       dp_error_t *_Nullable error);
+                       openvpn_dp_error *_Nullable error);
 
-size_t dp_mode_parse(dp_mode_t *_Nonnull mode,
-                     zeroing_data_t *_Nonnull dst,
+size_t openvpn_dp_mode_parse(openvpn_dp_mode *_Nonnull mode,
+                     pp_zd *_Nonnull dst,
                      uint8_t *_Nonnull dst_header,
                      uint8_t *_Nonnull src,
                      size_t src_len,
-                     dp_error_t *_Nullable error);
+                     openvpn_dp_error *_Nullable error);
 
 static inline
-zeroing_data_t *_Nullable dp_mode_decrypt_and_parse(dp_mode_t *_Nonnull mode,
-                                                    zeroing_data_t *_Nonnull buf,
+pp_zd *_Nullable openvpn_dp_mode_decrypt_and_parse(openvpn_dp_mode *_Nonnull mode,
+                                                    pp_zd *_Nonnull buf,
                                                     uint32_t *_Nonnull dst_packet_id,
                                                     uint8_t *_Nonnull dst_header,
                                                     bool *_Nonnull dst_keep_alive,
                                                     const uint8_t *_Nonnull src,
                                                     size_t src_len,
-                                                    dp_error_t *_Nullable error) {
+                                                    openvpn_dp_error *_Nullable error) {
 
     pp_assert(buf->length >= src_len);
-    const size_t dec_len = dp_mode_decrypt(mode, buf, dst_packet_id,
+    const size_t dec_len = openvpn_dp_mode_decrypt(mode, buf, dst_packet_id,
                                            src, src_len, error);
     if (!dec_len) {
         return NULL;
     }
-    zeroing_data_t *dst = zd_create(dec_len);
-    const size_t dst_len = dp_mode_parse(mode, dst, dst_header,
+    pp_zd *dst = pp_zd_create(dec_len);
+    const size_t dst_len = openvpn_dp_mode_parse(mode, dst, dst_header,
                                          buf->bytes, dec_len, error);
     if (!dst_len) {
-        zd_free(dst);
+        pp_zd_free(dst);
         return NULL;
     }
-    zd_resize(dst, dst_len);
+    pp_zd_resize(dst, dst_len);
     pp_assert(dst->length == dst_len);
-    if (packet_is_ping(dst->bytes, dst->length)) {
+    if (openvpn_packet_is_ping(dst->bytes, dst->length)) {
         *dst_keep_alive = true;
     }
     return dst;

Sources/Crypto/TLSMbed_C/tls.c
@@ -1,0 +1,58 @@
+/*
+ * SPDX-FileCopyrightText: 2025 Davide De Rosa
+ *
+ * SPDX-License-Identifier: GPL-3.0
+ */
+
+#include "portable/common.h"
+#include "crypto/tls.h"
+
+// FIXME: #108, implement with mbedTLS
+
+//static const char *const TLSBoxClientEKU = "TLS Web Client Authentication";
+static const char *const TLSBoxServerEKU = "TLS Web Server Authentication";
+
+pp_tls_ctx pp_tls_create(const pp_tls_options *opt, pp_tls_error_code *error) {
+    return NULL;
+}
+
+void pp_tls_free(pp_tls_ctx tls) {
+}
+
+bool pp_tls_start(pp_tls_ctx _Nonnull tls) {
+    return false;
+}
+
+bool pp_tls_is_connected(pp_tls_ctx _Nonnull tls) {
+    return false;
+}
+
+// MARK: - I/O
+
+pp_zd *_Nullable pp_tls_pull_cipher(pp_tls_ctx _Nonnull tls,
+                                                  pp_tls_error_code *_Nullable error) {
+    return NULL;
+}
+
+pp_zd *_Nullable pp_tls_pull_plain(pp_tls_ctx _Nonnull tls,
+                                                 pp_tls_error_code *_Nullable error) {
+    return NULL;
+}
+
+bool pp_tls_put_cipher(pp_tls_ctx _Nonnull tls,
+                            const uint8_t *_Nonnull src, size_t src_len,
+                            pp_tls_error_code *_Nullable error) {
+    return false;
+}
+
+bool pp_tls_put_plain(pp_tls_ctx _Nonnull tls,
+                           const uint8_t *_Nonnull src, size_t src_len,
+                           pp_tls_error_code *_Nullable error) {
+    return false;
+}
+
+// MARK: - MD5
+
+char *pp_tls_ca_md5(const pp_tls_ctx tls) {
+    return NULL;
+}

commit d31a623ef2df077ac7336f3bc6f1046d69f237a5

Fix errors introduced in #164
- Compile error on old macro name
- snprintf() not accounting for '\0'

Sources/Crypto/TLSOpenSSL_C/tls.c
@@ -300,7 +300,7 @@ char *pp_tls_ca_md5(const pp_tls_ctx tls) {
     char *hex = pp_alloc_crypto(2 * sizeof(md) + 1);
     char *ptr = hex;
     for (size_t i = 0; i < sizeof(md); ++i) {
-        ptr += snprintf(ptr, 2, "%02x", md[i]);
+        ptr += snprintf(ptr, 3, "%02x", md[i]);
     }
     *ptr = '\0';
     return hex;

Sources/OpenVPN/Cross_C/include/openvpn/dp_mode.h
@@ -145,7 +145,7 @@ uint32_t openvpn_dp_mode_peer_id(openvpn_dp_mode *_Nonnull mode) {
 
 static inline
 void openvpn_dp_mode_set_peer_id(openvpn_dp_mode *_Nonnull mode, uint32_t peer_id) {
-    mode->opt.peer_id = openvpn_peer_id_masked(peer_id);
+    mode->opt.peer_id = OPENVPN_PEER_ID_MASKED(peer_id);
 }
 
 static inline