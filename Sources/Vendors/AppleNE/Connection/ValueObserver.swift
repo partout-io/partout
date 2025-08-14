de9799acda8909589a0d51f7843e5021045603dd
Davide <keeshux@gmail.com>
2025-08-14 14:59:19 +0200

    [ci skip]

--- Sources/Vendors/AppleNE/Connection/ValueObserver.swift
+++ Sources/Vendors/AppleNE/Connection/ValueObserver.swift
@@ -6,7 +6,7 @@ import Foundation
 import PartoutCore
 
 /// Observes KVO updates asynchronously.
-@available(*, deprecated, message: "Required by deprecated NEUDPSocket and NETCPSocket. Use NESocket instead")
+@available(*, deprecated, message: "Required by the deprecated NEUDPObserver and NETCPObserver. Use NESocketObserver instead")
 actor ValueObserver<O> where O: NSObject {
     private weak var subject: O?
 
