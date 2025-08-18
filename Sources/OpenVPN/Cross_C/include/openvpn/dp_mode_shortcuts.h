eab9688b4f5a8d2cefdb4544e81a3c702c5056c7
Davide <keeshux@gmail.com>
2025-08-18 14:05:38 +0200

    Split dp_mode shortcuts from mock

---
+++ Sources/OpenVPN/Cross_C/include/openvpn/dp_mode_shortcuts.h
@@ -0,0 +1,34 @@
+// SPDX-FileCopyrightText: 2025 Davide De Rosa
+//
+// SPDX-License-Identifier: GPL-3.0
+
+#pragma once
+
+#include "crypto/crypto_aead.h"
+#include "crypto/crypto_cbc.h"
+#include "openvpn/dp_mode_ad.h"
+#include "openvpn/dp_mode_hmac.h"
+
+static inline
+openvpn_dp_mode *_Nullable openvpn_dp_mode_ad_create_aead(const char *_Nonnull cipher,
+                                            size_t tag_len, size_t id_len,
+                                            const pp_crypto_keys *_Nullable keys,
+                                            openvpn_compression_framing comp_f) {
+    pp_crypto_ctx crypto = pp_crypto_aead_create(cipher, tag_len, id_len, keys);
+    if (!crypto) {
+        return NULL;
+    }
+    return openvpn_dp_mode_ad_create(crypto, pp_crypto_aead_free, comp_f);
+}
+
+static inline
+openvpn_dp_mode *_Nullable openvpn_dp_mode_hmac_create_cbc(const char *_Nullable cipher,
+                                             const char *_Nonnull digest,
+                                             const pp_crypto_keys *_Nullable keys,
+                                             openvpn_compression_framing comp_f) {
+    pp_crypto_ctx crypto = pp_crypto_cbc_create(cipher, digest, keys);
+    if (!crypto) {
+        return NULL;
+    }
+    return openvpn_dp_mode_hmac_create(crypto, pp_crypto_cbc_free, comp_f);
+}
