2ee457fa3867196e049e813f9b89d752bdf00ae3
Davide <keeshux@gmail.com>
2025-08-20 16:06:50 +0200

    Build Core binary from local submodule

--- scripts/ci-generate.sh
+++ scripts/ci-generate.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-src_path="PartoutCore"
-src_url="https://${ACCESS_TOKEN}@github.com/passepartoutvpn/partout-core"
-src_sha1=`grep 'let sha1' Package.swift | sed -E "s/^let sha1 = \"([A-Fa-f0-9]{40})\"$/\\1/"`
-git clone --no-checkout --filter=blob:none --depth=1 $src_url $src_path
-( cd $src_path && git fetch origin $src_sha1 && git checkout $src_sha1 )
-$src_path/scripts/create-framework.sh
+vendors/core/scripts/create-framework.sh
