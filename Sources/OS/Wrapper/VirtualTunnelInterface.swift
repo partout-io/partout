7360629589df2a767e08a51886648baca80fcb54
Davide <keeshux@gmail.com>
2025-09-02 23:11:57 +0200

    Take ctx out of closures to prevent copy

--- Sources/OS/Wrapper/VirtualTunnelInterface.swift
+++ Sources/OS/Wrapper/VirtualTunnelInterface.swift
@@ -47,41 +47,49 @@ public final class VirtualTunnelInterface: IOInterface, @unchecked Sendable {
     }
 
     public func readPackets() async throws -> [Data] {
-        try await withCheckedThrowingContinuation { continuation in
-            readQueue.async { [weak self] in
-                guard let self else {
-                    continuation.resume(throwing: PartoutError(.releasedObject))
-                    return
+        do {
+            return try await withCheckedThrowingContinuation { continuation in
+                readQueue.async { [weak self] in
+                    guard let self else {
+                        continuation.resume(throwing: PartoutError(.releasedObject))
+                        return
+                    }
+                    let readCount = pp_tun_read(tun, &readBuf, readBuf.count)
+                    guard readCount > 0 else {
+                        continuation.resume(throwing: PartoutError(.linkFailure))
+                        return
+                    }
+                    let newPacket = Data(readBuf[0..<Int(readCount)])
+                    continuation.resume(returning: [newPacket])
                 }
-                let readCount = pp_tun_read(tun, &readBuf, readBuf.count)
-                guard readCount > 0 else {
-                    pp_log(ctx, .core, .fault, "Unable to read TUN packets")
-                    continuation.resume(throwing: PartoutError(.linkFailure))
-                    return
-                }
-                let newPacket = Data(readBuf[0..<Int(readCount)])
-                continuation.resume(returning: [newPacket])
             }
+        } catch {
+            pp_log(ctx, .core, .fault, "Unable to read TUN packets: \(error)")
+            throw error
         }
     }
 
     public func writePackets(_ packets: [Data]) async throws {
-        try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Void, Error>) in
-            writeQueue.async { [weak self] in
-                guard let self else { return }
-                for toWrite in packets {
-                    guard !toWrite.isEmpty else { continue }
-                    let writtenCount = toWrite.withUnsafeBytes {
-                        pp_tun_write(self.tun, $0.bytePointer, toWrite.count)
+        do {
+            try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Void, Error>) in
+                writeQueue.async { [weak self] in
+                    guard let self else { return }
+                    for toWrite in packets {
+                        guard !toWrite.isEmpty else { continue }
+                        let writtenCount = toWrite.withUnsafeBytes {
+                            pp_tun_write(self.tun, $0.bytePointer, toWrite.count)
+                        }
+                        guard writtenCount > 0 else {
+                            continuation.resume(throwing: PartoutError(.linkFailure))
+                            return
+                        }
                     }
-                    guard writtenCount > 0 else {
-                        pp_log(ctx, .core, .fault, "Unable to write TUN packets")
-                        continuation.resume(throwing: PartoutError(.linkFailure))
-                        return
-                    }
+                    continuation.resume()
                 }
-                continuation.resume()
             }
+        } catch {
+            pp_log(ctx, .core, .fault, "Unable to write TUN packets: \(error)")
+            throw error
         }
     }
 }
