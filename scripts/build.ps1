e07e67bd0abb7d9eeacad105fd2ac6af65ad4d1e
Davide <keeshux@gmail.com>
2025-09-03 19:13:28 +0200

    Build to dot dirs to sanitize Xcode search

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,16 +1,18 @@
 $cwd = Get-Location
+$build_dir = ".cmake"
+$bin_dir = ".bin"
 
 try {
     # Remove all .txt files in the build folder
-    Remove-Item -Path "build\*.txt" -ErrorAction SilentlyContinue
+    Remove-Item -Path "$build_dir\*.txt" -ErrorAction SilentlyContinue
 
     # Create build folder if it doesn't exist
-    if (-not (Test-Path -Path "build")) {
-        New-Item -ItemType Directory -Path "build" | Out-Null
+    if (-not (Test-Path -Path "$build_dir")) {
+        New-Item -ItemType Directory -Path "$build_dir" | Out-Null
     }
 
     # Change directory to build and remove PartoutProject* folders/files
-    Set-Location -Path "build"
+    Set-Location -Path "$build_dir"
     Get-ChildItem -Path "PartoutProject*" -Recurse -Force | Remove-Item -Recurse -Force -ErrorAction SilentlyContinue
 
     # Run CMake with ninja for Swift
