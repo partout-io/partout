partout-io/partout

commit e95e0a2d6a8c2071c7df407474d4d01c14c86b1e

NE: Clone the default route objects (#167)
NEIPv4Route and NEIPv6Route are classes and therefore .default() is a
shared object. Do not modify it because changes reflect elsewhere and
unpredictably.

The problem was made evident by the tests failing due to other tests
modifying the gateway address of the default routes.

Fixes #163

Tests/Vendors/AppleNE/ProfileNetworkSettingsTests.swift
@@ -38,12 +38,12 @@ struct ProfileNetworkSettingsTests {
         let ipV4Settings = try #require(sut.ipv4Settings)
         let ipV6Settings = try #require(sut.ipv6Settings)
         let expRoutesV4: [NEIPv4Route] = {
-            let route = NEIPv4Route.default()
+            let route = NEIPv4Route.default().cloned()
             route.gatewayAddress = "10.20.30.40"
             return [route]
         }()
         let expRoutesV6: [NEIPv6Route] = {
-            let route = NEIPv6Route.default()
+            let route = NEIPv6Route.default().cloned()
             route.gatewayAddress = "10:20::30:40"
             return [route]
         }()

Sources/Vendors/AppleNE/Extensions/NEIPv4Route+Equatable.swift
@@ -24,6 +24,22 @@ extension NEIPv4Route {
     }
 }
 
+extension NEIPv4Route {
+    public func cloned() -> NEIPv4Route {
+        let copy = NEIPv4Route(
+            destinationAddress: destinationAddress,
+            subnetMask: destinationSubnetMask
+        )
+        copy.gatewayAddress = gatewayAddress
+        return copy
+    }
+
+    public func hasSameDestination(as other: NEIPv4Route) -> Bool {
+        destinationAddress == other.destinationAddress &&
+            destinationSubnetMask == other.destinationSubnetMask
+    }
+}
+
 private extension NEIPv4Route {
     var equalitySubject: [String?] {
         [

Sources/Vendors/AppleNE/Extensions/NEIPv6Route+Equatable.swift
@@ -24,6 +24,22 @@ extension NEIPv6Route {
     }
 }
 
+extension NEIPv6Route {
+    public func cloned() -> NEIPv6Route {
+        let copy = NEIPv6Route(
+            destinationAddress: destinationAddress,
+            networkPrefixLength: destinationNetworkPrefixLength
+        )
+        copy.gatewayAddress = gatewayAddress
+        return copy
+    }
+
+    public func hasSameDestination(as other: NEIPv6Route) -> Bool {
+        destinationAddress == other.destinationAddress &&
+            destinationNetworkPrefixLength == other.destinationNetworkPrefixLength
+    }
+}
+
 private extension NEIPv6Route {
     var equalitySubject: [String?] {
         [

Sources/Vendors/AppleNE/Modules/IPModule+NE.swift
@@ -43,7 +43,7 @@ private extension Route {
     var neIPv4Route: NEIPv4Route {
         let route = destination.map {
             NEIPv4Route(destinationAddress: $0.address.rawValue, subnetMask: $0.ipv4Mask)
-        } ?? NEIPv4Route.default()
+        } ?? NEIPv4Route.default().cloned()
         route.gatewayAddress = gateway?.rawValue
         return route
     }
@@ -69,7 +69,7 @@ private extension Route {
     var neIPv6Route: NEIPv6Route {
         let route = destination.map {
             NEIPv6Route(destinationAddress: $0.address.rawValue, networkPrefixLength: $0.prefixLength as NSNumber)
-        } ?? NEIPv6Route.default()
+        } ?? NEIPv6Route.default().cloned()
         route.gatewayAddress = gateway?.rawValue
         return route
     }

Tests/Vendors/AppleNE/NESettingsApplyingTests.swift
@@ -208,7 +208,7 @@ private extension NESettingsApplyingTests {
             #expect(neRoute.destinationAddress == destination.address.rawValue)
             #expect(neRoute.destinationSubnetMask == destination.ipv4Mask)
         } else {
-            #expect(neRoute == NEIPv4Route.default())
+            #expect(neRoute.hasSameDestination(as: .default()))
         }
         #expect(neRoute.gatewayAddress == route.gateway?.rawValue)
     }
@@ -218,7 +218,7 @@ private extension NESettingsApplyingTests {
             #expect(neRoute.destinationAddress == destination.address.rawValue)
             #expect(neRoute.destinationNetworkPrefixLength.intValue == destination.prefixLength)
         } else {
-            #expect(neRoute == NEIPv6Route.default())
+            #expect(neRoute.hasSameDestination(as: .default()))
         }
         #expect(neRoute.gatewayAddress == route.gateway?.rawValue)
     }
@@ -231,7 +231,7 @@ private extension Collection where Element == Route {
                 neRoute.destinationAddress == destination.address.rawValue &&
                 neRoute.destinationSubnetMask == destination.ipv4Mask
             } else {
-                neRoute == NEIPv4Route.default()
+                neRoute.hasSameDestination(as: .default())
             }
         }
     }
@@ -242,7 +242,7 @@ private extension Collection where Element == Route {
                 neRoute.destinationAddress == destination.address.rawValue &&
                 neRoute.destinationNetworkPrefixLength.intValue == destination.prefixLength
             } else {
-                neRoute == NEIPv6Route.default()
+                neRoute.hasSameDestination(as: .default())
             }
         }
     }

commit 01eaa75a419b1f803135620b67674f23cf6da9b0

NE: Fix default gateway evaluation (#216)
Regression in #167

Sources/PartoutOS/AppleNE/Modules/Profile+NE.swift
@@ -47,8 +47,12 @@ extension Profile {
             }
         }
 
-        let isGatewayIPv4 = neSettings.ipv4Settings?.includedRoutes?.contains(.default()) ?? false
-        let isGatewayIPv6 = neSettings.ipv6Settings?.includedRoutes?.contains(.default()) ?? false
+        let isGatewayIPv4 = neSettings.ipv4Settings?.includedRoutes?.contains {
+            $0.hasSameDestination(as: .default())
+        } ?? false
+        let isGatewayIPv6 = neSettings.ipv6Settings?.includedRoutes?.contains {
+            $0.hasSameDestination(as: .default())
+        } ?? false
         let isGateway = isGatewayIPv4 || isGatewayIPv6
         pp_log(ctx, .os, .info, "\tVPN is default gateway: \(isGateway)")
 

Tests/PartoutOSTests/AppleNE/ProfileNetworkSettingsTests.swift
@@ -9,6 +9,17 @@ import PartoutCore
 import Testing
 
 struct ProfileNetworkSettingsTests {
+    @Test
+    func givenRoutes_whenDefaultGateway_thenContains() {
+        var routes: [NEIPv4Route] = []
+        let gw: NEIPv4Route = .default().cloned()
+        gw.gatewayAddress = "10.8.0.1"
+        routes.append(gw)
+        #expect(!routes.contains(.default()))
+        #expect(routes.contains {
+            $0.hasSameDestination(as: .default())
+        })
+    }
 
     // MARK: Plain
 