partout-io/partout

commit 30452d9a1609f75f83cbd24087a04123cd9629d1

Run ABI on MainActor (#211)
Resolve the errors triggered by using Swift 6.1 in the manifest. Clean
up failing/dup OpenVPN tests.

Sources/Partout/ABI.swift
@@ -22,6 +22,7 @@ import PartoutCore
 //
 
 @_cdecl("partout_init")
+@MainActor
 public func partout_init(cArgs: UnsafePointer<partout_init_args>) -> UnsafeMutableRawPointer {
     pp_log_g(.core, .debug, "Partout: Initialize")
 
@@ -90,11 +91,13 @@ public func partout_init(cArgs: UnsafePointer<partout_init_args>) -> UnsafeMutab
 }
 
 @_cdecl("partout_deinit")
+@MainActor
 public func partout_deinit(cCtx: UnsafeMutableRawPointer) {
     ABIContext.pop(cCtx)
 }
 
 @_cdecl("partout_daemon_start")
+@MainActor
 public func partout_daemon_start(
     cCtx: UnsafeMutableRawPointer,
     cArgs: UnsafePointer<partout_daemon_start_args>
@@ -131,16 +134,15 @@ public func partout_daemon_start(
     }
 
     // Throws .unknownImportedModule if missing implementation
-    //let ovpnCfg = try StandardOpenVPNParser().parsed(fromContents: str).configuration
-    //let ovpn = try OpenVPNModule.Builder(configurationBuilder: ovpnCfg.builder()).tryBuild()
-    //print(ovpn)
+    // let ovpnCfg = try StandardOpenVPNParser().parsed(fromContents: str).configuration
+    // let ovpn = try OpenVPNModule.Builder(configurationBuilder: ovpnCfg.builder()).tryBuild()
+    // print(ovpn)
 
     // This task is short-lived
     Task {
+        // try await Task.sleep(interval: 3.0)
         do {
-            // try await Task.sleep(interval: 3.0)
-            try await daemon.start()
-            ctx.daemon = daemon
+            try await ctx.startDaemon(daemon)
         } catch {
             pp_log_g(.core, .error, "Partout: Unable to start daemon: \(error)")
             exit(-1)
@@ -151,13 +153,13 @@ public func partout_daemon_start(
 }
 
 @_cdecl("partout_daemon_stop")
+@MainActor
 public func partout_daemon_stop(cCtx: UnsafeMutableRawPointer) {
     pp_log_g(.core, .debug, "Partout: Stop daemon with ctx: \(cCtx)")
     let ctx = ABIContext.peek(cCtx)
     pp_log_g(.core, .debug, "Partout: Stop daemon with ctx (ABIContext): \(ctx)")
     Task {
-        await ctx.daemon?.stop()
-        ctx.daemon = nil
+        await ctx.stopDaemon()
     }
 }
 

Sources/Partout/ABIContext.swift
@@ -6,17 +6,28 @@
 import PartoutCore
 #endif
 
-// FIXME: #188, @MainActor maybe
+@MainActor
 final class ABIContext {
     let registry: Registry
 
-    var daemon: SimpleConnectionDaemon?
+    private var daemon: SimpleConnectionDaemon?
 
     init(registry: Registry) {
         self.registry = registry
     }
+
+    func startDaemon(_ daemon: SimpleConnectionDaemon) async throws {
+        self.daemon = daemon
+        try await daemon.start()
+    }
+
+    func stopDaemon() async {
+        await daemon?.stop()
+        daemon = nil
+    }
 }
 
+@MainActor
 extension ABIContext {
     func push() -> UnsafeMutableRawPointer {
         Unmanaged.passRetained(self).toOpaque()

Tests/PartoutOpenVPNTests/KeyDecrypterTests.swift
@@ -8,7 +8,7 @@ import Testing
 
 struct KeyDecrypterTests {
     @Test
-    func givenPKCS1_whenParse_thenFails() throws {
+    func givenDecrypter_whenParsePKCS1_thenFails() throws {
         let sut = newDecrypter()
         let path = try path(withName: "tunnelbear.enc.1.key")
         #expect(throws: Error.self) {
@@ -17,7 +17,7 @@ struct KeyDecrypterTests {
     }
 
     @Test
-    func givenPKCS1_whenParseWithPassphrase_thenSucceeds() throws {
+    func givenDecrypter_whenParsePKCS1WithPassphrase_thenSucceeds() throws {
         let sut = newDecrypter()
         let expected = try String(contentsOfFile: path(withName: "tunnelbear.key"))
         let path = try path(withName: "tunnelbear.enc.1.key")
@@ -26,7 +26,7 @@ struct KeyDecrypterTests {
     }
 
     @Test
-    func givenPKCS8_whenParse_thenFails() throws {
+    func givenDecrypter_whenParsePKCS8_thenFails() throws {
         let sut = newDecrypter()
         let path = try path(withName: "tunnelbear.enc.8.key")
         #expect(throws: Error.self) {
@@ -35,7 +35,7 @@ struct KeyDecrypterTests {
     }
 
     @Test
-    func givenPKCS8_whenParseWithPassphrase_thenSucceeds() throws {
+    func givenDecrypter_whenParsePKCS8WithPassphrase_thenSucceeds() throws {
         let sut = newDecrypter()
         let expected = try String(contentsOfFile: path(withName: "tunnelbear.key"))
         let path = try path(withName: "tunnelbear.enc.8.key")

Tests/PartoutOpenVPNTests/OpenVPNParserTests.swift
@@ -7,7 +7,6 @@ import Foundation
 import PartoutCore
 import Testing
 
-@Suite
 struct OpenVPNParserTests {
     private let parser = StandardOpenVPNParser(supportsLZO: true, decrypter: nil)
 
@@ -163,11 +162,15 @@ struct OpenVPNParserTests {
         #expect(cfg.warning == nil)
         #expect(cfg4.configuration.xorMethod == .obfuscate(mask: multiMask))
     }
+}
 
-    // MARK: PKCS
+// MARK: PKCS
 
-    @Test(arguments: allParsers)
-    func givenPKCS1_whenParse_thenFails(sut: StandardOpenVPNParser) {
+// Use the .serialized workaround to run these tests in Xcode
+// https://github.com/swiftlang/swift-testing/issues/749#issuecomment-2396267228
+extension OpenVPNParserTests {
+    @Test(.serialized, arguments: allParsers())
+    func givenParser_whenParsePKCS1_thenFails(sut: StandardOpenVPNParser) {
         let cfgURL = url(withName: "tunnelbear.enc.1")
         do {
             _ = try sut.parsed(fromURL: cfgURL)
@@ -177,14 +180,14 @@ struct OpenVPNParserTests {
         }
     }
 
-    @Test(arguments: allParsers)
-    func givenPKCS1_whenParseWithPassphrase_thenSucceeds(sut: StandardOpenVPNParser) throws {
+    @Test(.serialized, arguments: allParsers())
+    func givenParser_whenParsePKCS1WithPassphrase_thenSucceeds(sut: StandardOpenVPNParser) throws {
         let cfgURL = url(withName: "tunnelbear.enc.1")
         _ = try sut.parsed(fromURL: cfgURL, passphrase: "foobar")
     }
 
-    @Test(arguments: allParsers)
-    func givenPKCS8_whenParse_thenFails(sut: StandardOpenVPNParser) {
+    @Test(.serialized, arguments: allParsers())
+    func givenParser_whenParsePKCS8_thenFails(sut: StandardOpenVPNParser) {
         let cfgURL = url(withName: "tunnelbear.enc.8")
         do {
             _ = try sut.parsed(fromURL: cfgURL)
@@ -194,8 +197,8 @@ struct OpenVPNParserTests {
         }
     }
 
-    @Test(arguments: allParsers)
-    func givenPKCS8_whenParseWithPassphrase_thenSucceeds(sut: StandardOpenVPNParser) throws {
+    @Test(.serialized, arguments: allParsers())
+    func givenParser_whenParsePKCS8WithPassphrase_thenSucceeds(sut: StandardOpenVPNParser) throws {
         let cfgURL = url(withName: "tunnelbear.enc.8")
         do {
             _ = try sut.parsed(fromURL: cfgURL)
@@ -221,7 +224,7 @@ private extension OpenVPNParserTests {
         return url
     }
 
-    static var allParsers: [StandardOpenVPNParser] {
+    static func allParsers() -> [StandardOpenVPNParser] {
 #if OPENVPN_DEPRECATED_LZO
         let supportsLZO = true
 #else

Tests/PartoutOpenVPNTests/ParserDecryptionTests.swift
@@ -1,54 +1,0 @@
-// SPDX-FileCopyrightText: 2025 Davide De Rosa
-//
-// SPDX-License-Identifier: GPL-3.0
-
-import Foundation
-@testable import PartoutOpenVPN
-import Testing
-
-struct ParserDecryptionTests {
-    @Test
-    func givenPKCS1_whenParse_thenFails() {
-        let sut = newParser()
-        let cfgURL = url(withName: "tunnelbear.enc.1")
-        #expect(throws: StandardOpenVPNParserError.self) {
-            try sut.parsed(fromURL: cfgURL)
-        }
-    }
-
-    @Test
-    func givenPKCS1_whenParseWithPassphrase_thenSucceeds() throws {
-        let sut = newParser()
-        let cfgURL = url(withName: "tunnelbear.enc.1")
-        _ = try sut.parsed(fromURL: cfgURL, passphrase: "foobar")
-    }
-
-    @Test
-    func givenPKCS8_whenParse_thenFails() {
-        let sut = newParser()
-        let cfgURL = url(withName: "tunnelbear.enc.8")
-        #expect(throws: StandardOpenVPNParserError.self) {
-            try sut.parsed(fromURL: cfgURL)
-        }
-    }
-
-    @Test
-    func givenPKCS8_whenParseWithPassphrase_thenSucceeds() throws {
-        let sut = newParser()
-        let cfgURL = url(withName: "tunnelbear.enc.8")
-        #expect(throws: StandardOpenVPNParserError.self) {
-            try sut.parsed(fromURL: cfgURL)
-        }
-        _ = try sut.parsed(fromURL: cfgURL, passphrase: "foobar")
-    }
-}
-
-private extension ParserDecryptionTests {
-    func newParser() -> StandardOpenVPNParser {
-        StandardOpenVPNParser(supportsLZO: false, decrypter: SimpleKeyDecrypter())
-    }
-
-    func url(withName name: String) -> URL {
-        Bundle.module.url(forResource: name, withExtension: "ovpn")!
-    }
-}

commit 4a61b36e9f475604498c2e1021cf4d74988e1c06

Fix ABI MainActor locked by main sleep()
Regression in #211

Sources/Partout/ABI.swift
@@ -12,6 +12,12 @@ import PartoutOS
 import PartoutCore
 #endif
 
+@globalActor
+actor ABIActor {
+    actor Shared {}
+    static let shared = Shared()
+}
+
 // FIXME: #188, ABI is still optimistic
 //
 // - should receive callback arguments for completion and error handling
@@ -22,7 +28,7 @@ import PartoutCore
 //
 
 @_cdecl("partout_init")
-@MainActor
+@ABIActor
 public func partout_init(cArgs: UnsafePointer<partout_init_args>) -> UnsafeMutableRawPointer {
     pp_log_g(.core, .debug, "Partout: Initialize")
 
@@ -91,13 +97,13 @@ public func partout_init(cArgs: UnsafePointer<partout_init_args>) -> UnsafeMutab
 }
 
 @_cdecl("partout_deinit")
-@MainActor
+@ABIActor
 public func partout_deinit(cCtx: UnsafeMutableRawPointer) {
     ABIContext.pop(cCtx)
 }
 
 @_cdecl("partout_daemon_start")
-@MainActor
+@ABIActor
 public func partout_daemon_start(
     cCtx: UnsafeMutableRawPointer,
     cArgs: UnsafePointer<partout_daemon_start_args>
@@ -153,7 +159,7 @@ public func partout_daemon_start(
 }
 
 @_cdecl("partout_daemon_stop")
-@MainActor
+@ABIActor
 public func partout_daemon_stop(cCtx: UnsafeMutableRawPointer) {
     pp_log_g(.core, .debug, "Partout: Stop daemon with ctx: \(cCtx)")
     let ctx = ABIContext.peek(cCtx)

Sources/Partout/ABIContext.swift
@@ -6,7 +6,7 @@
 import PartoutCore
 #endif
 
-@MainActor
+@ABIActor
 final class ABIContext {
     let registry: Registry
 
@@ -27,7 +27,7 @@ final class ABIContext {
     }
 }
 
-@MainActor
+@ABIActor
 extension ABIContext {
     func push() -> UnsafeMutableRawPointer {
         Unmanaged.passRetained(self).toOpaque()