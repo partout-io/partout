c981f555efefcefc03551cca4db34360bff1981c
Davide De Rosa <keeshux@gmail.com>
2025-10-07 14:24:17 +0000

    0.99.194

--- Package.swift
+++ Package.swift
@@ -551,8 +551,8 @@ enum CryptoMode {
 
 // action-release-binary-package (PartoutCore)
 let binaryFilename = "PartoutCore.xcframework.zip"
-let version = "0.99.193"
-let checksum = "3207ab3ecd6ed89194e01b000b9acd92bb4d4abf3acb9c0c3dfe8403e86ac4b4"
+let version = "0.99.194"
+let checksum = "52fe447406569a07989346a437f5a5f42bb7b760ad3af6c0f1bc220860847ec4"
 
 enum CoreDeployment: String, RawRepresentable {
     case remoteBinary
