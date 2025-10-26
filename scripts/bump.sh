6e647c156beece3028ee5bab6028368831541764
Davide <keeshux@gmail.com>
2025-10-26 18:52:46 +0100

    Tag version in bump script

--- scripts/bump.sh
+++ scripts/bump.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-version=`cat .version`
+version="$1"
+if [[ -z $1 ]]; then                                                                                                                                      
+    echo "Version required"
+    exit 1
+fi
 abi="Sources/PartoutABI_C/partout.c"
 set -e
 sed -i '' -E "s/PARTOUT_VERSION = \"(.*)\"/PARTOUT_VERSION = \"$version\"/" $abi
-git add .version $abi
 git commit -m "[ci skip] Bump"
+git tag -as "$version" -m "$version"
