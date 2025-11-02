partout-io/partout

commit 63e2f8d48252071ea9f6ae45bcbb75fc8230947b

Group core libs and third parties into PartoutFoundation (#264)
- Hide all manual Foundation imports behind a reexport (until #228)
- Reexport third parties (GenericJSON)
- Drop ObjC types from GenericJSON (NSNull, NSNumber)
- Implement a few cross-platform extensions
  - Data deallocator
  - RegularExpression
  - NotificationCenter
  - String.range(of:) → String.ranges(of:).first
  - Some default values
- Bump target to iOS 16 / macOS 13

Closes #263

Sources/PartoutFoundation/GenericJSON/Initialization.swift
@@ -2,8 +2,6 @@
 //
 // SPDX-License-Identifier: MIT
 
-import Foundation
-
 private struct InitializationError: Error {}
 
 extension JSON {
@@ -16,16 +14,8 @@ extension JSON {
     /// You can also pass `nil` or `NSNull`, both will be treated as `.null`.
     public init(_ value: Any) throws {
         switch value {
-        case _ as NSNull:
-            self = .null
         case let opt as Any? where opt == nil:
             self = .null
-        case let num as NSNumber:
-            if num.isBool {
-                self = .bool(num.boolValue)
-            } else {
-                self = .number(num.doubleValue)
-            }
         case let str as String:
             self = .string(str)
         case let bool as Bool:
@@ -102,27 +92,3 @@ extension JSON: ExpressibleByStringLiteral {
         self = .string(value)
     }
 }
-
-// MARK: - NSNumber
-
-extension NSNumber {
-
-    /// Boolean value indicating whether this `NSNumber` wraps a boolean.
-    ///
-    /// For example, when using `NSJSONSerialization` Bool values are converted into `NSNumber` instances.
-    ///
-    /// - seealso: https://stackoverflow.com/a/49641315/3589408
-    fileprivate var isBool: Bool {
-        let objCType = String(cString: self.objCType)
-        if (self.compare(trueNumber) == .orderedSame && objCType == trueObjCType) || (self.compare(falseNumber) == .orderedSame && objCType == falseObjCType) {
-            return true
-        } else {
-            return false
-        }
-    }
-}
-
-private let trueNumber = NSNumber(value: true)
-private let falseNumber = NSNumber(value: false)
-private let trueObjCType = String(cString: trueNumber.objCType)
-private let falseObjCType = String(cString: falseNumber.objCType)

commit 203f1b4534c2440c30111ff0ff777d47fbd84d2f

Restore handling of numbers in JSON
Regression in #264

Sources/PartoutFoundation/GenericJSON/Initialization.swift
@@ -24,6 +24,10 @@ extension JSON {
             self = .array(try array.map(JSON.init))
         case let dict as [String: Any]:
             self = .object(try dict.mapValues(JSON.init))
+        case let num as Double:
+            self = .number(num)
+        case let num as Int:
+            self = .number(Double(num))
         default:
             throw InitializationError()
         }