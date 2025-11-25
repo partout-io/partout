7a1ac4f4feb6213c7a2b18e991fc563c236d4cae
Davide <keeshux@gmail.com>
2025-11-25 23:40:50 +0100

    Drop redundant WIN32 condition from Wintun
    Already checked in root.

--- vendors/wintun.cmake
+++ vendors/wintun.cmake
@@ -1,14 +1,10 @@
 set(WINTUN_DIR ${PP_BUILD_OUTPUT}/wintun)
-
-if(WIN32)
-    set(WINTUN_VERSION "0.14.1")
-    set(WINTUN_URL "https://www.wintun.net/builds/wintun-${WINTUN_VERSION}.zip")
-    FetchContent_Declare(
-        wintun
-        URL ${WINTUN_URL}
-        SOURCE_DIR ${CMAKE_BINARY_DIR}/wintun-src
-    )
-    FetchContent_MakeAvailable(wintun)
-    file(COPY ${wintun_SOURCE_DIR}/include/wintun.h DESTINATION ${WINTUN_DIR})
-    file(COPY ${wintun_SOURCE_DIR}/bin/${ARCH_NAME}/wintun.dll DESTINATION ${WINTUN_DIR})
-endif()
+set(WINTUN_URL "https://www.wintun.net/builds/wintun-${WINTUN_VERSION}.zip")
+FetchContent_Declare(
+    wintun
+    URL ${WINTUN_URL}
+    SOURCE_DIR ${CMAKE_BINARY_DIR}/wintun-src
+)
+FetchContent_MakeAvailable(wintun)
+file(COPY ${wintun_SOURCE_DIR}/include/wintun.h DESTINATION ${WINTUN_DIR})
+file(COPY ${wintun_SOURCE_DIR}/bin/${ARCH_NAME}/wintun.dll DESTINATION ${WINTUN_DIR})
