8082fa45ad9af18b79d1c100a984a1902df0e8da
Davide <keeshux@gmail.com>
2025-12-07 11:31:08 +0100

    Revert to usedEncoding: in ModuleImporter

--- Sources/PartoutCore/Profile/ModuleImporter.swift
+++ Sources/PartoutCore/Profile/ModuleImporter.swift
@@ -13,12 +13,14 @@ extension ModuleImporter {
     }
 
     public func module(fromURL url: URL, object: Any? = nil) throws -> Module {
-        let contents = try String(contentsOf: url, encoding: .utf8)
+        var encoding: String.Encoding = .utf8
+        let contents = try String(contentsOf: url, usedEncoding: &encoding)
         return try module(fromContents: contents, object: object)
     }
 
     public func module(fromPath path: String, object: Any? = nil) throws -> Module {
-        let contents = try String(contentsOfFile: path, encoding: .utf8)
+        var encoding: String.Encoding = .utf8
+        let contents = try String(contentsOfFile: path, usedEncoding: &encoding)
         return try module(fromContents: contents, object: object)
     }
 }
