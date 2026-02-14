ceb2eab979165f55373d4b32949149617f88fb32
Davide <keeshux@gmail.com>
2026-02-14 18:20:57 +0100

    Import SWON as ExternalProject to build on host machine

---
+++ vendors/swon.cmake
@@ -0,0 +1,16 @@
+ExternalProject_Add(
+    SWONProject
+    SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/vendors/swon
+    INSTALL_COMMAND ""
+)
+
+add_library(SWONInterface INTERFACE)
+add_dependencies(SWONInterface SWONProject)
+target_include_directories(SWONInterface INTERFACE
+    ${CMAKE_CURRENT_BINARY_DIR}
+    ${CMAKE_CURRENT_SOURCE_DIR}/vendors/swon/Sources/SWON_C/include
+)
+target_compile_options(SWONInterface INTERFACE
+    -load-plugin-executable "${CMAKE_CURRENT_BINARY_DIR}/SWONMacros#SWONMacros"
+)
+target_link_libraries(SWONInterface INTERFACE SWON)
