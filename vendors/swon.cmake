e399034ebd464fbe35a94aaf09e6de508eed64a6
Davide <keeshux@gmail.com>
2026-02-15 11:05:30 +0100

    Make SWON binary output predictable

--- vendors/swon.cmake
+++ vendors/swon.cmake
@@ -1,16 +1,17 @@
 ExternalProject_Add(
     SWONProject
     SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/vendors/swon
+    BINARY_DIR ${CMAKE_CURRENT_BINARY_DIR}/vendors/swon
     INSTALL_COMMAND ""
 )
 
 add_library(SWONInterface INTERFACE)
 add_dependencies(SWONInterface SWONProject)
 target_include_directories(SWONInterface INTERFACE
-    ${CMAKE_CURRENT_BINARY_DIR}
+    ${CMAKE_CURRENT_BINARY_DIR}/vendors/swon
     ${CMAKE_CURRENT_SOURCE_DIR}/vendors/swon/Sources/SWON_C/include
 )
 target_compile_options(SWONInterface INTERFACE
-    -load-plugin-executable "${CMAKE_CURRENT_BINARY_DIR}/SWONMacros#SWONMacros"
+    -load-plugin-executable "${CMAKE_CURRENT_BINARY_DIR}/vendors/swon/SWONMacros#SWONMacros"
 )
 target_link_libraries(SWONInterface INTERFACE SWON)
