1591460395ec1c63ef9c08929610f3af5e04ef1b
Davide <keeshux@gmail.com>
2026-02-24 11:33:12 +0100

    Test file URL contents (Compat only)

--- Tests/MiniFoundationTests/StringTests.swift
+++ Tests/MiniFoundationTests/StringTests.swift
@@ -2,6 +2,7 @@
 //
 // SPDX-License-Identifier: MIT
 
+import Foundation
 import MiniFoundation
 import Testing
 
@@ -16,4 +17,33 @@ struct StringTests {
     func hexFormat(format: String, arg: Int, string: String) {
         #expect(String(format: format, arg) == string)
     }
+
+    @Test(arguments: [
+        ("foo.txt", "foo\nbar\nbaz\n")
+    ])
+    func fileContents(filename: String, expectedContents: String) throws {
+        let url = try #require(Bundle.module.url(forResource: filename, withExtension: nil))
+        let foundContents = try String(contentsOf: url, encoding: .utf8)
+        print(foundContents)
+        #expect(foundContents == expectedContents)
+    }
+
+#if canImport(MiniFoundationCompat)
+    @Test(arguments: [
+        ("file://nonexisting.txt", true),
+        ("https://nonexisting/path", false)
+    ])
+    func urlContents(string: String, isFile: Bool) throws {
+        let url = try #require(Compat.URL(string: string))
+        do {
+            _ = try String(contentsOf: url, encoding: .utf8)
+            #expect(isFile)
+        } catch let mfError as MiniFoundationError {
+            switch mfError {
+            case .notFileURL: #expect(!isFile) // Non-file URL stops at this error
+            default: #expect(isFile) // File URL stops at I/O (non-existing file)
+            }
+        }
+    }
+#endif
 }
