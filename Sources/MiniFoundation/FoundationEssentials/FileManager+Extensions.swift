b2aad7b20f11a7384bf85eab07a281d2a533fa09
Davide <keeshux@gmail.com>
2026-02-27 21:05:30 +0100

    Split FileManager Compat/Native extensions

--- Sources/MiniFoundation/FoundationEssentials/FileManager+Extensions.swift
+++ Sources/MiniFoundation/FoundationEssentials/FileManager+Extensions.swift
@@ -6,11 +6,4 @@ extension FileManager {
     public func makeTemporaryURL(filename: String) -> URL {
         temporaryDirectory.appending(component: filename)
     }
-
-    public func contentsOfDirectory(at url: URL) throws -> [URL] {
-        let items = try contentsOfDirectory(atPath: url.filePath())
-        return items.map {
-            URL(filePath: $0, relativeTo: url)
-        }
-    }
 }
