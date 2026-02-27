001f8fa9151120c21345c1413852721490096dd1
Davide <keeshux@gmail.com>
2026-02-27 12:54:36 +0100

    Enforce autolinked libs in Linux/Android toolchains
    Prepend FoundationEssentials just in case

--- toolchains/linux.toolchain.cmake
+++ toolchains/linux.toolchain.cmake
@@ -1,4 +1,4 @@
 set(CMAKE_C_COMPILER "clang")
 set(CMAKE_CXX_COMPILER "clang")
 set(CMAKE_C_FLAGS "-fPIC")
-set(CMAKE_Swift_FLAGS "-resource-dir $ENV{SWIFT_RESOURCE_DIR} -ldispatch -lstdc++ -lm")
+set(CMAKE_Swift_FLAGS "-resource-dir $ENV{SWIFT_RESOURCE_DIR} -ldispatch -lstdc++ -lm -lFoundationEssentials -l_FoundationCollections -l_FoundationCShims -lswiftSynchronization")
