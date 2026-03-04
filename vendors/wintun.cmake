b1386f5d15dd9ac0e1785d1b979bc14dbc44de4a
Davide <keeshux@gmail.com>
2026-03-04 21:15:27 +0100

    Do not override Wintun CMake folder

--- vendors/wintun.cmake
+++ vendors/wintun.cmake
@@ -2,7 +2,6 @@ set(WINTUN_DIR ${PP_BUILD_OUTPUT}/wintun)
 set(WINTUN_URL "https://www.wintun.net/builds/wintun-${WINTUN_VERSION}.zip")
 FetchContent_Declare(wintun
     URL ${WINTUN_URL}
-    SOURCE_DIR ${CMAKE_BINARY_DIR}/wintun-src
 )
 FetchContent_MakeAvailable(wintun)
 file(COPY ${wintun_SOURCE_DIR}/include/wintun.h DESTINATION ${WINTUN_DIR})
