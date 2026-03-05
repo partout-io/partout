a060d5733580dbef8650b9d6397a0256f5e94720
Davide <keeshux@gmail.com>
2026-03-05 16:41:53 +0100

    Drop isFileURL assert to allow relative paths

--- Sources/MiniFoundation/FoundationEssentials/URL+Extensions.swift
+++ Sources/MiniFoundation/FoundationEssentials/URL+Extensions.swift
@@ -6,7 +6,6 @@ extension URL {
     public func filePath() -> String {
         // XXX: This should be equal to .path(percentEncoded: false)
         // i.e. false means "not percent encoded" = "after decoding percents"
-        assert(isFileURL)
-        return path
+        path
     }
 }
