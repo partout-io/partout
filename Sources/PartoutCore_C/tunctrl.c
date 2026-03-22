78260a72f92cd09f40e8d45fe2b31a028413a653
Davide <keeshux@gmail.com>
2026-03-22 01:47:28 +0100

    Handle failure/cleanup in JNI tunctrl

--- Sources/PartoutCore_C/tunctrl.c
+++ Sources/PartoutCore_C/tunctrl.c
@@ -38,49 +38,149 @@ void *pp_tun_ctrl_set_tunnel(void *jni_ref, const char *info_json) {
     pp_clog(PPLogCategoryCore, PPLogLevelInfo, "set_tunnel()");
 
     JNIEnv *env;
-    (*jvm)->AttachCurrentThread(jvm, &env, NULL);
+    jint attach_status = (*jvm)->AttachCurrentThread(jvm, &env, NULL);
+    if (attach_status != JNI_OK) {
+        pp_clog_v(PPLogCategoryCore, PPLogLevelError, "set_tunnel(): AttachCurrentThread failed (%d)", attach_status);
+        return NULL;
+    }
 
-    // Call VpnWrapper.build(), returns optional fd (Int?)
-    jclass cls = (*env)->GetObjectClass(env, jni_ref);
-    jstring infoJson = info_json ? (*env)->NewStringUTF(env, info_json) : NULL;
+    void *result = NULL;
+    jclass cls = NULL;
+    jstring infoJson = NULL;
+    jobject fdObj = NULL;
+    jclass integerClass = NULL;
+    vpn_impl *impl = NULL;
+
+    cls = (*env)->GetObjectClass(env, jni_ref);
+    if (cls == NULL) {
+        pp_clog(PPLogCategoryCore, PPLogLevelError, "set_tunnel(): GetObjectClass returned NULL");
+        goto failure;
+    }
+
+    infoJson = info_json ? (*env)->NewStringUTF(env, info_json) : NULL;
     jmethodID buildMethod = (*env)->GetMethodID(env, cls, "build", "(Ljava/lang/String;)Ljava/lang/Integer;");
-    jobject fdObj = (*env)->CallObjectMethod(env, jni_ref, buildMethod, infoJson);
+    if (buildMethod == NULL) {
+        pp_clog(PPLogCategoryCore, PPLogLevelError, "set_tunnel(): build() method not found");
+        goto failure;
+    }
+
+    fdObj = (*env)->CallObjectMethod(env, jni_ref, buildMethod, infoJson);
+    if (fdObj == NULL) {
+        goto failure;
+    }
+
+    integerClass = (*env)->FindClass(env, "java/lang/Integer");
+    if (integerClass == NULL) {
+        pp_clog(PPLogCategoryCore, PPLogLevelError, "set_tunnel(): java/lang/Integer class not found");
+        goto failure;
+    }
+
+    jmethodID intValueMethod = (*env)->GetMethodID(env, integerClass, "intValue", "()I");
+    if (intValueMethod == NULL) {
+        pp_clog(PPLogCategoryCore, PPLogLevelError, "set_tunnel(): Integer.intValue() not found");
+        goto failure;
+    }
+
+    const jint fd = (*env)->CallIntMethod(env, fdObj, intValueMethod);
+
+    impl = malloc(sizeof(*impl));
+    if (impl == NULL) {
+        pp_clog(PPLogCategoryCore, PPLogLevelError, "set_tunnel(): malloc failed");
+        goto failure;
+    }
+    impl->fd = fd;
+    result = impl;
+
+failure:
+    if (fdObj != NULL) {
+        (*env)->DeleteLocalRef(env, fdObj);
+    }
+    if (integerClass != NULL) {
+        (*env)->DeleteLocalRef(env, integerClass);
+    }
     if (infoJson != NULL) {
         (*env)->DeleteLocalRef(env, infoJson);
     }
-    if (fdObj == NULL) {
-        return NULL;
+    if (cls != NULL) {
+        (*env)->DeleteLocalRef(env, cls);
     }
-    jclass integerClass = (*env)->FindClass(env, "java/lang/Integer");
-    jmethodID intValueMethod = (*env)->GetMethodID(env, integerClass, "intValue", "()I");
-    const jint fd = (*env)->CallIntMethod(env, fdObj, intValueMethod);
-    (*env)->DeleteLocalRef(env, integerClass);
-    (*env)->DeleteLocalRef(env, fdObj);
 
-    // Return the tun_impl for VirtualTunnelInterface
-    vpn_impl *impl = malloc(sizeof(*impl));
-    impl->fd = fd;
-    return impl;
+    return result;
 }
 
 void pp_tun_ctrl_configure_sockets(void *jni_ref, const int *fds, const size_t fds_len) {
     pp_clog(PPLogCategoryCore, PPLogLevelInfo, "configure_sockets()");
     JNIEnv *env;
-    (*jvm)->AttachCurrentThread(jvm, &env, NULL);
+    jint attach_status = (*jvm)->AttachCurrentThread(jvm, &env, NULL);
+    if (attach_status != JNI_OK) {
+        pp_clog_v(PPLogCategoryCore, PPLogLevelError, "configure_sockets(): AttachCurrentThread failed (%d)", attach_status);
+        return;
+    }
 
-    jclass integerCls = (*env)->FindClass(env, "java/lang/Integer");
-    jmethodID integerCtor = (*env)->GetMethodID(env, integerCls, "<init>", "(I)V");
-    jobjectArray fdsObj = (*env)->NewObjectArray(env, fds_len, integerCls, NULL);
+    jclass integerCls = NULL;
+    jmethodID integerCtor = NULL;
+    jobjectArray fdsObj = NULL;
+    jobject elem = NULL;
+    jclass cls = NULL;
+    jmethodID cfgMethod = NULL;
+
+    integerCls = (*env)->FindClass(env, "java/lang/Integer");
+    if (integerCls == NULL) {
+        pp_clog(PPLogCategoryCore, PPLogLevelError, "configure_sockets(): java/lang/Integer class not found");
+        goto cleanup;
+    }
+
+    integerCtor = (*env)->GetMethodID(env, integerCls, "<init>", "(I)V");
+    if (integerCtor == NULL) {
+        pp_clog(PPLogCategoryCore, PPLogLevelError, "configure_sockets(): Integer constructor not found");
+        goto cleanup;
+    }
+
+    fdsObj = (*env)->NewObjectArray(env, fds_len, integerCls, NULL);
+    if (fdsObj == NULL) {
+        pp_clog(PPLogCategoryCore, PPLogLevelError, "configure_sockets(): failed to allocate Integer[]");
+        goto cleanup;
+    }
+
     for (jsize i = 0; i < fds_len; i++) {
-        jobject elem = (*env)->NewObject(env, integerCls, integerCtor, (jint)(fds[i]));
+        elem = (*env)->NewObject(env, integerCls, integerCtor, (jint)(fds[i]));
+        if (elem == NULL) {
+            pp_clog_v(PPLogCategoryCore, PPLogLevelError, "configure_sockets(): failed to wrap fd %d", i);
+            goto cleanup;
+        }
         (*env)->SetObjectArrayElement(env, fdsObj, i, elem);
         (*env)->DeleteLocalRef(env, elem);
+        elem = NULL;
     }
 
     // Call VpnWrapper.configureSockets()
-    jclass cls = (*env)->GetObjectClass(env, jni_ref);
-    jmethodID cfgMethod = (*env)->GetMethodID(env, cls, "configureSockets", "([Ljava/lang/Integer;)V");
+    cls = (*env)->GetObjectClass(env, jni_ref);
+    if (cls == NULL) {
+        pp_clog(PPLogCategoryCore, PPLogLevelError, "configure_sockets(): GetObjectClass returned NULL");
+        goto cleanup;
+    }
+
+    cfgMethod = (*env)->GetMethodID(env, cls, "configureSockets", "([Ljava/lang/Integer;)V");
+    if (cfgMethod == NULL) {
+        pp_clog(PPLogCategoryCore, PPLogLevelError, "configure_sockets(): configureSockets method not found");
+        goto cleanup;
+    }
+
     (*env)->CallVoidMethod(env, jni_ref, cfgMethod, fdsObj);
+
+cleanup:
+    if (elem != NULL) {
+        (*env)->DeleteLocalRef(env, elem);
+    }
+    if (fdsObj != NULL) {
+        (*env)->DeleteLocalRef(env, fdsObj);
+    }
+    if (integerCls != NULL) {
+        (*env)->DeleteLocalRef(env, integerCls);
+    }
+    if (cls != NULL) {
+        (*env)->DeleteLocalRef(env, cls);
+    }
 }
 
 void pp_tun_ctrl_clear_tunnel(void *jni_ref, void *tun_impl) {
