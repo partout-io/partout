8c727f1c6644ebbf6a3639d60cf5db5bed607137
Davide <keeshux@gmail.com>
2026-03-24 12:38:38 +0100

    Inject schema aliases from the command line

--- scripts/gen-api.sh
+++ scripts/gen-api.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-swift run partout-codegen --manifest scripts/manifest.yaml >scripts/openapi.yaml
+swift run partout-codegen \
+    --manifest scripts/manifest.yaml \
+    --aliases SecureData:string,UniqueID:string \
+    >scripts/openapi.yaml
