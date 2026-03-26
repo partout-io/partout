partout-io/partout

commit 46705a364730b4cc330fa18568c4c07775525ec3

Always include OpenVPN/WireGuard core targets (#320)
Only bind VPN connection targets to defines.

Package.swift
@@ -22,7 +22,6 @@ let wgGoVersion: Version = "0.0.2025063103"
 let cmakeOutput = envCMakeOutput ?? "bin/windows-arm64"
 let useFoundationCompatibility: FoundationCompatibility = .off
 // let useFoundationCompatibility: FoundationCompatibility = OS.current != .apple ? .on : .off
-let forPreviews = false
 
 // MARK: - Package
 
@@ -71,32 +70,21 @@ let package = Package(
                 if cryptoMode != nil {
                     list.append("_PartoutCryptoImpl_C")
                     if areas.contains(.openVPN) {
-                        if forPreviews {
-                            list.append("PartoutOpenVPN")
-                        } else {
-                            list.append("PartoutOpenVPNConnection")
-                        }
+                        list.append("PartoutOpenVPNConnection")
+                    } else {
+                        list.append("PartoutOpenVPN")
                     }
                 }
                 if areas.contains(.wireGuard) {
-                    if forPreviews {
-                        list.append("PartoutWireGuard")
-                    } else {
-                        list.append("PartoutWireGuardConnection")
-                    }
+                    list.append("PartoutWireGuardConnection")
+                } else {
+                    list.append("PartoutWireGuard")
                 }
                 return list
             }(),
-            swiftSettings: {
-                var list: [SwiftSetting] = areas.compactMap(\.define).map {
-                    .define($0)
-                }
-                if forPreviews {
-                    list.append(.define("PARTOUT_FOR_PREVIEWS"))
-                }
-                list.append(contentsOf: useFoundationCompatibility.swiftSettings)
-                return list
-            }()
+            swiftSettings: areas.compactMap(\.define).map {
+                .define($0)
+            } + useFoundationCompatibility.swiftSettings
         ),
         .testTarget(
             name: "PartoutTests",
@@ -196,48 +184,67 @@ package.targets.append(contentsOf: [
 
 // MARK: OpenVPN
 
+package.products.append(
+    .library(
+        name: "PartoutOpenVPN",
+        targets: ["PartoutOpenVPN"]
+    )
+)
+package.targets.append(
+    .target(
+        name: "PartoutOpenVPN",
+        dependencies: ["PartoutOS"]
+    )
+)
+
 // OpenVPN requires Crypto/TLS wrappers
-if areas.contains(.openVPN) {
-    package.products.append(
-        .library(
-            name: "PartoutOpenVPN",
-            targets: ["PartoutOpenVPN"]
-        )
-    )
-    package.targets.append(
+if areas.contains(.openVPN), cryptoMode != nil {
+    package.targets.append(contentsOf: [
         .target(
-            name: "PartoutOpenVPN",
-            dependencies: ["PartoutOS"]
+            name: "PartoutOpenVPNConnection_C",
+            dependencies: ["_PartoutCryptoImpl_C"],
+            cSettings: globalCSettings
+        ),
+        .target(
+            name: "PartoutOpenVPNConnection",
+            dependencies: [
+                "PartoutOpenVPN",
+                "PartoutOpenVPNConnection_C"
+            ]
+        ),
+        .testTarget(
+            name: "PartoutOpenVPNTests",
+            dependencies: ["PartoutOpenVPNConnection"],
+            exclude: useFoundationCompatibility.openVPNTestsExclude + ["DataPathPerformanceTests.swift"],
+            resources: [
+                .process("Resources")
+            ],
+            swiftSettings: useFoundationCompatibility.swiftSettings
         )
-    )
-    if cryptoMode != nil {
-        package.targets.append(contentsOf: [
-            .target(
-                name: "PartoutOpenVPN_C",
-                dependencies: ["_PartoutCryptoImpl_C"],
-                cSettings: globalCSettings
-            ),
-            .target(
-                name: "PartoutOpenVPNConnection",
-                dependencies: [
-                    "PartoutOpenVPN",
-                    "PartoutOpenVPN_C"
-                ]
-            ),
-            .testTarget(
-                name: "PartoutOpenVPNTests",
-                dependencies: ["PartoutOpenVPNConnection"],
-                exclude: useFoundationCompatibility.openVPNTestsExclude + ["DataPathPerformanceTests.swift"],
-                resources: [
-                    .process("Resources")
-                ],
-                swiftSettings: useFoundationCompatibility.swiftSettings
-            )
-        ])
-    }
+    ])
 }
 
 // MARK: WireGuard
+
+package.products.append(
+    .library(
+        name: "PartoutWireGuard",
+        targets: ["PartoutWireGuard"]
+    )
+)
+package.targets.append(contentsOf: [
+    .target(
+        name: "PartoutWireGuard",
+        dependencies: [
+            "PartoutOS",
+            "PartoutWireGuard_C"
+        ]
+    ),
+    .target(
+        name: "PartoutWireGuard_C",
+        dependencies: ["PartoutCore_C"]
+    )
+])
 
 if areas.contains(.wireGuard) {
     switch OS.current {
@@ -271,24 +278,7 @@ if areas.contains(.wireGuard) {
             )
         )
     }
-    package.products.append(
-        .library(
-            name: "PartoutWireGuard",
-            targets: ["PartoutWireGuard"]
-        )
-    )
     package.targets.append(contentsOf: [
-        .target(
-            name: "PartoutWireGuard",
-            dependencies: [
-                "PartoutOS",
-                "PartoutWireGuard_C"
-            ]
-        ),
-        .target(
-            name: "PartoutWireGuard_C",
-            dependencies: ["PartoutCore_C"]
-        ),
         .target(
             name: "PartoutWireGuardConnection",
             dependencies: [
@@ -427,12 +417,7 @@ enum Area: CaseIterable {
     case openVPN
     case wireGuard
 
-    var define: String? {
-        switch self {
-        case .openVPN: "PARTOUT_OPENVPN"
-        case .wireGuard: "PARTOUT_WIREGUARD"
-        }
-    }
+    var define: String? { nil }
 }
 
 enum OS: String, CaseIterable {

.github/workflows/test.yml
@@ -17,10 +17,6 @@ jobs:
       - uses: partout-io/action-prepare-xcode-build@master
         with:
           access-token: ${{ secrets.GITHUB_TOKEN }}
-      - name: Ensure previews are off
-        run: |
-          expected="let forPreviews = false"
-          grep "^$expected" Package.swift
       - name: Ensure Foundation compatibility is off
         run: |
           expected="let useFoundationCompatibility: FoundationCompatibility = .off"

Sources/CMakeLists.txt
@@ -14,7 +14,7 @@ target_compile_options(partout_c PRIVATE
 set(PARTOUT_C_INCLUDE_DIRS
     ${CMAKE_CURRENT_SOURCE_DIR}/MiniFoundation_C/include
     ${CMAKE_CURRENT_SOURCE_DIR}/PartoutCore_C/include
-    ${CMAKE_CURRENT_SOURCE_DIR}/PartoutOpenVPN_C/include
+    ${CMAKE_CURRENT_SOURCE_DIR}/PartoutOpenVPNConnection_C/include
     ${CMAKE_CURRENT_SOURCE_DIR}/PartoutWireGuard_C/include
     ${CMAKE_CURRENT_SOURCE_DIR}/PartoutWireGuardConnection_C/include
 )
@@ -72,8 +72,6 @@ add_library(partout STATIC "")
 # Define symbols to skip Swift imports and legacy code
 target_compile_options(partout PRIVATE
     -DUSE_CMAKE
-    -DPARTOUT_OPENVPN
-    -DPARTOUT_WIREGUARD
     -enable-library-evolution
 )
 

Sources/Partout/PartoutExports.swift
@@ -9,18 +9,14 @@
 
 // MARK: - Optional
 
-#if PARTOUT_OPENVPN
 @_exported import PartoutOpenVPN
-#if !PARTOUT_FOR_PREVIEWS
+#if PARTOUT_OPENVPN
 @_exported import PartoutOpenVPNConnection
 #endif
-#endif
 
-#if PARTOUT_WIREGUARD
 @_exported import PartoutWireGuard
-#if !PARTOUT_FOR_PREVIEWS
+#if PARTOUT_WIREGUARD
 @_exported import PartoutWireGuardConnection
-#endif
 #endif
 
 #endif

Sources/Partout/Registry+Support.swift
@@ -3,21 +3,14 @@
 // SPDX-License-Identifier: GPL-3.0
 
 extension Registry {
-    private static let knownHandlers: [ModuleHandler] = {
-        var handlers: [ModuleHandler] = [
-            DNSModule.moduleHandler,
-            HTTPProxyModule.moduleHandler,
-            IPModule.moduleHandler,
-            OnDemandModule.moduleHandler
-        ]
-#if PARTOUT_OPENVPN
-        handlers.append(OpenVPNModule.moduleHandler)
-#endif
-#if PARTOUT_WIREGUARD
-        handlers.append(WireGuardModule.moduleHandler)
-#endif
-        return handlers
-    }()
+    private static let knownHandlers: [ModuleHandler] = [
+        DNSModule.moduleHandler,
+        HTTPProxyModule.moduleHandler,
+        IPModule.moduleHandler,
+        OnDemandModule.moduleHandler,
+        OpenVPNModule.moduleHandler,
+        WireGuardModule.moduleHandler
+    ]
 
     /// Returns a ``Registry`` with the known module handlers and resolvers and an empty device ID.
     public convenience init() {

Sources/PartoutOpenVPNConnection/Internal/AuthSerializer.swift
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: GPL-3.0
 
-internal import _PartoutOpenVPN_C
+internal import _PartoutOpenVPNConnection_C
 
 extension ControlChannel {
     final class AuthSerializer: ControlChannelSerializer {

Sources/PartoutOpenVPNConnection/Internal/CControlPacket+Serialization.swift
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: GPL-3.0
 
-internal import _PartoutOpenVPN_C
+internal import _PartoutOpenVPNConnection_C
 
 extension CControlPacket {
     typealias SerializationFunction = (

Sources/PartoutOpenVPNConnection/Internal/CControlPacket.swift
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: GPL-3.0
 
-internal import _PartoutOpenVPN_C
+internal import _PartoutOpenVPNConnection_C
 
 final class CControlPacket {
     let pkt: UnsafeMutablePointer<openvpn_ctrl>

commit 62264711200a658566da9708bc039c839aaaec8f

Restore OpenVPN/WireGuard defines
For exported imports.

Regression in #320

Package.swift
@@ -417,7 +417,12 @@ enum Area: CaseIterable {
     case openVPN
     case wireGuard
 
-    var define: String? { nil }
+    var define: String? {
+        switch self {
+        case .openVPN: "PARTOUT_OPENVPN"
+        case .wireGuard: "PARTOUT_WIREGUARD"
+        }
+    }
 }
 
 enum OS: String, CaseIterable {