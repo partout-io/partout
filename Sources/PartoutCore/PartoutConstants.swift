f3943c066f8f0c5a30bd8dba82715e891e8fc0cd
Davide <keeshux@gmail.com>
2026-03-30 14:08:53 +0200

    Bump version

--- Sources/PartoutCore/PartoutConstants.swift
+++ Sources/PartoutCore/PartoutConstants.swift
@@ -7,7 +7,7 @@ public enum PartoutConstants {
     public static let identifier = "io.partout"
 
     /// The library version.
-    public static let version = "0.121.1"
+    public static let version = "0.121.2"
 
     /// The computed version identifier.
     public static let versionIdentifier: String = "\(identifier) \(version)"
