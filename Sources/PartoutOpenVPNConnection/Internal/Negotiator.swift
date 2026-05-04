partout-io/partout

commit c22c009b9c863f238174469d3bcbf17daddb7b5b

Normalize version of VPN connections (#365)
- OpenVPNConnection -> V1
- OpenVPNConnectionV2 -> V2
- LegacyWireGuardConnection -> V1
- WireGuardConnection -> V2

Expose default impl as typealias (V1/V1 for both).

Sources/PartoutOpenVPNConnection/Internal/Negotiator.swift
@@ -11,7 +11,7 @@ final class Negotiator {
 
         let withLocalOptions: Bool
 
-        let sessionOptions: OpenVPNConnection.Options
+        let sessionOptions: _OpenVPNConnectionV1.Options
 
         let onConnected: (UInt8, DataChannel, PushReply) async -> Void
 

Sources/PartoutOpenVPNConnection/Internal/OpenVPNSession.swift
@@ -27,7 +27,7 @@ final class OpenVPNSession {
 
     let cachesURL: URL
 
-    let options: OpenVPNConnection.Options
+    let options: _OpenVPNConnectionV1.Options
 
     private let tlsFactory: TLSFactory
 
@@ -100,7 +100,7 @@ final class OpenVPNSession {
         credentials: OpenVPN.Credentials?,
         prng: PRNGProtocol,
         cachesURL: URL,
-        options: OpenVPNConnection.Options = .init(),
+        options: _OpenVPNConnectionV1.Options = .init(),
         tlsFactory: @escaping TLSFactory,
         dpFactory: @escaping DataPathFactory
     ) throws {

Sources/PartoutOpenVPNConnection/OpenVPNConnection+Alias.swift
@@ -1,0 +1,5 @@
+// SPDX-FileCopyrightText: 2026 Davide De Rosa
+//
+// SPDX-License-Identifier: GPL-3.0
+
+public typealias OpenVPNConnection = _OpenVPNConnectionV1

Sources/PartoutOpenVPNConnection/V2/_OpenVPNConnectionV2+Default.swift
@@ -2,18 +2,23 @@
 //
 // SPDX-License-Identifier: GPL-3.0
 
-extension OpenVPNConnection {
+extension _OpenVPNConnectionV2 {
     public init(
         _ ctx: PartoutLoggerContext,
         parameters: ConnectionParameters,
         module: OpenVPNModule,
         cachesURL: URL,
-        options: Options = .init()
+        options baseOptions: _OpenVPNConnectionV1.Options = .init()
     ) throws {
         guard let configuration = module.configuration else {
             fatalError("Creating session without OpenVPN configuration?")
         }
-        pp_log(ctx, .openvpn, .notice, "OpenVPN: Using cross-platform connection")
+        pp_log(ctx, .openvpn, .notice, "OpenVPN: Using cross-platform connection V2")
+
+        // Override options with feature flags
+        var options = baseOptions
+        options.withLoopsV2 = true
+        options.withFlushDataBeforeControl = true
 
         // Hardcode portable implementations
         let prng = PlatformPRNG()

Sources/PartoutWireGuardConnection/WireGuardConnection+Alias.swift
@@ -8,6 +8,8 @@
 //  SPDX-License-Identifier: MIT
 //  Copyright © 2018-2024 WireGuard LLC. All Rights Reserved.
 
+public typealias WireGuardConnection = _WireGuardConnectionV1
+
 enum WireGuardConnectionError: Error {
     case dnsResolutionFailure
 

Sources/PartoutCore/Connection/CyclingConnection.swift
@@ -53,7 +53,7 @@ extension CyclingConnection {
 /// Automates cycling through a list of endpoints until a connection is successfully established. The connection creation implementation is provided by a ``NetworkInterfaceFactory``, while internal behavior is defined with the use of a ``CyclingConnection/Hooks`` object.
 ///
 /// Make sure to only access this entity within the same actor.
-@available(*, deprecated, message: "Merge into OpenVPNConnection")
+@available(*, deprecated, message: "Merge into _OpenVPNConnectionV1")
 public actor CyclingConnection {
 
     // MARK: Initialization

Sources/PartoutOpenVPNConnection/V1/_OpenVPNConnectionV1+Default.swift
@@ -2,23 +2,18 @@
 //
 // SPDX-License-Identifier: GPL-3.0
 
-extension OpenVPNConnectionV2 {
+extension _OpenVPNConnectionV1 {
     public init(
         _ ctx: PartoutLoggerContext,
         parameters: ConnectionParameters,
         module: OpenVPNModule,
         cachesURL: URL,
-        options baseOptions: OpenVPNConnection.Options = .init()
+        options: Options = .init()
     ) throws {
         guard let configuration = module.configuration else {
             fatalError("Creating session without OpenVPN configuration?")
         }
-        pp_log(ctx, .openvpn, .notice, "OpenVPN: Using cross-platform connection V2")
-
-        // Override options with feature flags
-        var options = baseOptions
-        options.withLoopsV2 = true
-        options.withFlushDataBeforeControl = true
+        pp_log(ctx, .openvpn, .notice, "OpenVPN: Using cross-platform connection")
 
         // Hardcode portable implementations
         let prng = PlatformPRNG()

Sources/PartoutOpenVPNConnection/V1/_OpenVPNConnectionV1+Options.swift
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: GPL-3.0
 
-extension OpenVPNConnection {
+extension _OpenVPNConnectionV1 {
     /// Intervals are expressed in seconds.
     public struct Options: Sendable {
         public var maxPackets: Int = 100

commit 169d0fbd24c79bc671e9e15dbbad10f5f6a5e59a

Fix CMake build issues with connection aliases
Broken after #365

Sources/CMakeLists.txt
@@ -91,8 +91,9 @@ if(USE_COMPAT)
     set(EXCLUDED_PATTERNS
         partout-codegen.*
         PartoutCodegen.*
+        PartoutOpenVPNConnection\/V1
         PartoutOS\/Apple.*
-        PartoutWireGuardConnection\/Legacy.*
+        PartoutWireGuardConnection\/V1
     )
 endif()
 

Sources/PartoutOpenVPNConnection/Internal/Negotiator.swift
@@ -11,7 +11,7 @@ final class Negotiator {
 
         let withLocalOptions: Bool
 
-        let sessionOptions: _OpenVPNConnectionV1.Options
+        let sessionOptions: OpenVPNConnection.Options
 
         let onConnected: (UInt8, DataChannel, PushReply) async -> Void
 

Sources/PartoutOpenVPNConnection/Internal/OpenVPNSession.swift
@@ -27,7 +27,7 @@ final class OpenVPNSession {
 
     let cachesURL: URL
 
-    let options: _OpenVPNConnectionV1.Options
+    let options: OpenVPNConnection.Options
 
     private let tlsFactory: TLSFactory
 
@@ -100,7 +100,7 @@ final class OpenVPNSession {
         credentials: OpenVPN.Credentials?,
         prng: PRNGProtocol,
         cachesURL: URL,
-        options: _OpenVPNConnectionV1.Options = .init(),
+        options: OpenVPNConnection.Options = .init(),
         tlsFactory: @escaping TLSFactory,
         dpFactory: @escaping DataPathFactory
     ) throws {

Sources/PartoutOpenVPNConnection/OpenVPNConnection+Alias.swift
@@ -2,4 +2,8 @@
 //
 // SPDX-License-Identifier: GPL-3.0
 
+#if !USE_CMAKE
 public typealias OpenVPNConnection = _OpenVPNConnectionV1
+#else
+public typealias OpenVPNConnection = _OpenVPNConnectionV2
+#endif

Sources/PartoutOpenVPNConnection/OpenVPNConnection+Options.swift
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: GPL-3.0
 
-extension _OpenVPNConnectionV1 {
+extension OpenVPNConnection {
     /// Intervals are expressed in seconds.
     public struct Options: Sendable {
         public var maxPackets: Int = 100

Sources/PartoutOpenVPNConnection/V2/_OpenVPNConnectionV2+Default.swift
@@ -8,7 +8,7 @@ extension _OpenVPNConnectionV2 {
         parameters: ConnectionParameters,
         module: OpenVPNModule,
         cachesURL: URL,
-        options baseOptions: _OpenVPNConnectionV1.Options = .init()
+        options baseOptions: OpenVPNConnection.Options = .init()
     ) throws {
         guard let configuration = module.configuration else {
             fatalError("Creating session without OpenVPN configuration?")

Sources/PartoutWireGuardConnection/WireGuardConnection+Alias.swift
@@ -8,7 +8,11 @@
 //  SPDX-License-Identifier: MIT
 //  Copyright © 2018-2024 WireGuard LLC. All Rights Reserved.
 
+#if !USE_CMAKE
 public typealias WireGuardConnection = _WireGuardConnectionV1
+#else
+public typealias WireGuardConnection = _WireGuardConnectionV2
+#endif
 
 enum WireGuardConnectionError: Error {
     case dnsResolutionFailure