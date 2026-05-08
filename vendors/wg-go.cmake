b7b18cec4c796004e15383373ada3cc1ef9a3bad
Davide <keeshux@gmail.com>
2026-05-08 22:11:44 +0200

    Fix wg-go rpath in INSTALL_COMMAND vs POST_BUILD
    Add BUILD_BYPRODUCTS, too.

--- vendors/wg-go.cmake
+++ vendors/wg-go.cmake
@@ -1,10 +1,15 @@
 set(WGGO_DIR ${PP_BUILD_OUTPUT}/wg-go)
 
 if(WIN32)
+    set(WGGO_BYPRODUCTS
+        ${WGGO_DIR}/lib/wg-go.dll
+        ${WGGO_DIR}/lib/wg-go.lib
+    )
     set(WGGO_CMD
         make-windows.bat ${WGGO_DIR}
     )
 else()
+    set(WGGO_BYPRODUCTS ${WGGO_DIR}/lib/libwg-go${LIBEXT})
     set(WGGO_CMD
         make -C ${CMAKE_CURRENT_SOURCE_DIR}/vendors/wg-go
         DESTDIR=${WGGO_DIR}
@@ -15,28 +20,27 @@ else()
     endif()
 endif()
 
+if(APPLE)
+    set(WGGO_INSTALL_COMMAND
+        install_name_tool -id "@rpath/libwg-go.dylib" "${WGGO_DIR}/lib/libwg-go.dylib"
+    )
+elseif(WIN32)
+    set(WGGO_INSTALL_COMMAND
+        gendef "${WGGO_DIR}/lib/wg-go.dll"
+        COMMAND dlltool -d wg-go.def -l "${WGGO_DIR}/lib/wg-go.lib"
+    )
+else()
+    set(WGGO_INSTALL_COMMAND "")
+endif()
+
 ExternalProject_Add(WireGuardGoProject
     SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/vendors/wg-go
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ${VENDOR_ENV} ${WGGO_CMD}
-    INSTALL_COMMAND ""
+    INSTALL_COMMAND ${WGGO_INSTALL_COMMAND}
     BUILD_IN_SOURCE 1
+    BUILD_BYPRODUCTS ${WGGO_BYPRODUCTS}
 )
-
-if(APPLE)
-    add_custom_command(
-        TARGET WireGuardGoProject
-        POST_BUILD
-        COMMAND install_name_tool -id "@rpath/libwg-go.dylib" "${WGGO_DIR}/lib/libwg-go.dylib"
-    )
-elseif(WIN32)
-    add_custom_command(
-        TARGET WireGuardGoProject
-        POST_BUILD
-        COMMAND gendef "${WGGO_DIR}/lib/wg-go.dll"
-        COMMAND dlltool -d wg-go.def -l "${WGGO_DIR}/lib/wg-go.lib"
-    )
-endif()
 
 add_library(WireGuardGoInterface INTERFACE)
 add_dependencies(WireGuardGoInterface WireGuardGoProject)
