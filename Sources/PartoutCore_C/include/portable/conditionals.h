432a4d4ed7faf004a7fc83414d47caac5bde9e64
Davide <keeshux@gmail.com>
2026-05-10 15:08:36 +0200

    Add lib-specific conditionals

---
+++ Sources/PartoutCore_C/include/portable/conditionals.h
@@ -0,0 +1,40 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Davide De Rosa
+ *
+ * SPDX-License-Identifier: GPL-3.0
+ */
+
+#if defined(__APPLE__)
+#include <TargetConditionals.h>
+#define PARTOUT_APPLE       1
+#if TARGET_OS_OSX
+#define PARTOUT_MACOS       1
+#else
+#define PARTOUT_MACOS       0
+#endif
+#else
+#define PARTOUT_APPLE       0
+#endif
+
+#if defined(__ANDROID__)
+#define PARTOUT_ANDROID     1
+#define PARTOUT_LINUX       0
+#elif defined(__linux__)
+#define PARTOUT_ANDROID     0
+#define PARTOUT_LINUX       1
+#else
+#define PARTOUT_ANDROID     0
+#define PARTOUT_LINUX       0
+#endif
+
+#if defined(_WIN32)
+#define PARTOUT_WINDOWS     1
+#else
+#define PARTOUT_WINDOWS     0
+#endif
+
+#if defined(PARTOUT_MACOS) || defined(PARTOUT_LINUX) || defined(PARTOUT_WINDOWS)
+#define PARTOUT_ABI         1
+#else
+#define PARTOUT_ABI         0
+#endif
