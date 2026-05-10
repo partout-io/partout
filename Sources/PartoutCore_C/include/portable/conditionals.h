partout-io/partout

commit 7eac671b8d60c4f9b7b4c96e097b9a79c9861927

Wrap OS conditionals in lib-specific macros (#384)
There was a lot of boilerplate for OS checks and similar conditions.
Wrap them in a single `conditionals.h` header for better readability and
fault tolerance.

Sources/PartoutCore_C/include/portable/conditionals.h
@@ -1,0 +1,42 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Davide De Rosa
+ *
+ * SPDX-License-Identifier: GPL-3.0
+ */
+
+#pragma once
+
+#if defined(__APPLE__)
+#include <TargetConditionals.h>
+#define PARTOUT_APPLE       1
+#if TARGET_OS_OSX
+#define PARTOUT_MACOS       1
+#else
+#define PARTOUT_MACOS       0
+#endif
+#else
+#define PARTOUT_APPLE       0
+#endif
+
+#if defined(__ANDROID__)
+#define PARTOUT_ANDROID     1
+#define PARTOUT_LINUX       0
+#elif defined(__linux__)
+#define PARTOUT_ANDROID     0
+#define PARTOUT_LINUX       1
+#else
+#define PARTOUT_ANDROID     0
+#define PARTOUT_LINUX       0
+#endif
+
+#if defined(_WIN32)
+#define PARTOUT_WINDOWS     1
+#else
+#define PARTOUT_WINDOWS     0
+#endif
+
+#if PARTOUT_MACOS || PARTOUT_LINUX || PARTOUT_WINDOWS
+#define PARTOUT_ABI         1
+#else
+#define PARTOUT_ABI         0
+#endif

Package.swift
@@ -127,12 +127,13 @@ package.targets.append(contentsOf: [
     .target(
         name: "PartoutCore_C",
         cSettings: globalCSettings + {
+            var list: [CSetting] = []
             if OS.current == .windows {
-                return [
-                    .unsafeFlags(["-I\(cmakeOutput)/wintun"])
-                ]
+                list.append(.unsafeFlags([
+                    "-I\(cmakeOutput)/wintun"
+                ]))
             }
-            return []
+            return list
         }()
     ),
     .target(

Sources/PartoutCore_C/common.c
@@ -23,7 +23,7 @@ void pp_clog_v(pp_log_category category,
     pp_free(msg);
 }
 
-#ifdef __ANDROID__
+#if PARTOUT_ANDROID
 #include <android/log.h>
 
 JNIEnv *pp_jni_attach_thread(bool *did_attach) {

Sources/PartoutCore_C/include/portable/common.h
@@ -5,6 +5,7 @@
  */
 
 #pragma once
+#include "portable/conditionals.h"
 
 #include <assert.h>
 #include <stdbool.h>
@@ -12,7 +13,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Logging counterpart of Swift pp_log
+/* Logging counterpart of Swift pp_log. */
 
 typedef enum {
     PPLogLevelFault,
@@ -37,7 +38,7 @@ void pp_log_simple_append(const char *_Nullable tag,
                           pp_log_level level,
                           const char *_Nonnull message);
 
-// Use inline rather than #define to make available to Swift
+/* Use inline rather than #define to make available to Swift. */
 
 static inline
 void pp_assert(bool condition) {
@@ -71,7 +72,7 @@ void pp_zero(void *_Nonnull ptr, size_t count) {
 
 static inline
 char *_Nonnull pp_dup(const char *_Nonnull str) {
-#ifdef _WIN32
+#if PARTOUT_WINDOWS
     char *ptr = _strdup(str);
 #else
     char *ptr = strdup(str);
@@ -83,7 +84,7 @@ char *_Nonnull pp_dup(const char *_Nonnull str) {
     return ptr;
 }
 
-#ifdef _WIN32
+#if PARTOUT_WINDOWS
 static inline
 FILE *_Nullable pp_fopen(const char *_Nonnull filename, const char *_Nonnull mode) {
     FILE *file_ret = NULL;
@@ -99,7 +100,7 @@ FILE *_Nullable pp_fopen(const char *_Nonnull filename, const char *_Nonnull mod
 #define pp_sscanf sscanf
 #endif
 
-#ifdef __ANDROID__
+#if PARTOUT_ANDROID
 #include <jni.h>
 #include <stdbool.h>
 extern _Nullable JavaVM *_Nullable jvm;

Sources/PartoutCore_C/include/portable/endian.h
@@ -5,10 +5,11 @@
  */
 
 #pragma once
+#include "portable/conditionals.h"
 
 #include <stdint.h>
 
-#ifdef __APPLE__
+#if PARTOUT_APPLE
 
 #include <CoreFoundation/CoreFoundation.h>
 
@@ -34,7 +35,7 @@ uint32_t pp_endian_htonl(uint32_t num) {
 
 #else
 
-#ifdef _WIN32
+#if PARTOUT_WINDOWS
 #include <WinSock2.h>
 #else
 #include <arpa/inet.h>

Sources/PartoutCore_C/include/portable/network.h
@@ -5,18 +5,15 @@
  */
 
 #pragma once
+#include "portable/conditionals.h"
 
 #include <stdint.h>
 #include <stdlib.h>
 
-#ifdef _WIN32
+#if PARTOUT_WINDOWS
 #include <WinSock2.h>
 #else
 #include <arpa/inet.h>
-#include <net/if.h>
-#include <netdb.h>
-#include <sys/ioctl.h>
-#include <sys/socket.h>
 #endif
 
 typedef enum {

Sources/PartoutCore_C/include/portable/prng.h
@@ -4,6 +4,9 @@
  * SPDX-License-Identifier: GPL-3.0
  */
 
+#pragma once
+#include "portable/conditionals.h"
+
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>

Sources/PartoutCore_C/include/portable/socket.h
@@ -5,6 +5,7 @@
  */
 
 #pragma once
+#include "portable/conditionals.h"
 
 #include <stdbool.h>
 #include <stdint.h>

commit b9024c7ecf345fe82247568e4ea8d95c6315447d

Fix ABI condition not including Android
Regression in #384

Sources/PartoutCore_C/include/portable/conditionals.h
@@ -35,7 +35,7 @@
 #define PARTOUT_WINDOWS     0
 #endif
 
-#if PARTOUT_MACOS || PARTOUT_LINUX || PARTOUT_WINDOWS
+#if PARTOUT_MACOS || PARTOUT_LINUX || PARTOUT_WINDOWS || PARTOUT_ANDROID
 #define PARTOUT_ABI         1
 #else
 #define PARTOUT_ABI         0