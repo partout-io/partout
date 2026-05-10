partout-io/partout

commit 67cb6538ae0bbf7cbee74c65eef7652b2f735fb8

Use Swift 6 conditionally in CMake (#377)
Leave Swift 5 level in SwiftPM for access to NE sockets, but enforce
Swift 6 in CMake. Be conservative about conditionals.

Mitigates #171

Sources/PartoutOpenVPNConnection/Internal/Negotiator.swift
@@ -652,10 +652,15 @@ private extension Negotiator {
     func completeConnection(pushReply: PushReply) throws {
         pp_log(ctx, .openvpn, .info, "Complete connection of key \(key)")
         let history = NegotiationHistory(pushReply: pushReply)
+#if swift(<6.0)
         let dataChannel = try newDataChannel(with: history)
+#endif
         self.history = history
         authenticator?.reset()
         Task {
+#if swift(>=6.0)
+            nonisolated(unsafe) let dataChannel = try newDataChannel(with: history)
+#endif
             await options.onConnected(key, dataChannel, pushReply)
         }
     }

Sources/CMakeLists.txt
@@ -108,7 +108,7 @@ add_dependencies(partout partout_c)
 target_sources(partout PRIVATE ${PARTOUT_SOURCES})
 target_link_libraries(partout PRIVATE partout_c)
 target_compile_options(partout PRIVATE
-    -language-mode 5
+    -language-mode 6
 )
 
 ################################################

Sources/Partout/VirtualTunnelController.swift
@@ -56,9 +56,6 @@ public final class VirtualTunnelController: TunnelController {
             tunImpl = infoJSON.withCString {
                 pp_tun_ctrl_set_tunnel(impl, $0)
             }
-            guard let tunImpl else {
-                throw PartoutError(.linkNotActive)
-            }
         } else {
             tunImpl = nil
         }

Sources/PartoutCore/Tunnel/Tunnel.swift
@@ -28,7 +28,9 @@ public actor Tunnel {
         self.environmentFactory = environmentFactory
         snapshotsSubject = CurrentValueStream([:])
         environments = [:]
+#if swift(<6.0)
         observeObjects()
+#endif
     }
 }
 
@@ -36,6 +38,9 @@ public actor Tunnel {
 
 extension Tunnel: TunnelStrategy {
     public func prepare(purge: Bool) async throws {
+#if swift(>=6.0)
+        observeObjects()
+#endif
         pp_log(ctx, .core, .info, "Prepare tunnel (purge: \(purge))...")
         try await strategy.prepare(purge: purge)
     }
@@ -144,6 +149,10 @@ extension Tunnel {
 
 private extension Tunnel {
     func observeObjects() {
+#if swift(>=6.0)
+        // Subscribe once
+        guard strategySubscription == nil else { return }
+#endif
         strategySubscription?.cancel()
         strategySubscription = Task { [weak self] in
             guard let stream = self?.strategy.didUpdateActiveProfiles else { return }

Sources/PartoutOpenVPNConnection/Internal/NegotiationHistory.swift
@@ -2,6 +2,6 @@
 //
 // SPDX-License-Identifier: GPL-3.0
 
-struct NegotiationHistory {
+struct NegotiationHistory: Sendable {
     let pushReply: PushReply
 }

Sources/PartoutOpenVPNConnection/Internal/PushReply.swift
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: GPL-3.0
 
-struct PushReply {
+struct PushReply: Sendable {
     private let original: String
 
     let options: OpenVPN.Configuration

commit 8eab378c33fe07884bf82d17fe913467b0ad72c3

OpenVPN: Fix regression in data channel
Regression in #377

Sources/PartoutOpenVPNConnection/Internal/Negotiator.swift
@@ -652,15 +652,14 @@ private extension Negotiator {
     func completeConnection(pushReply: PushReply) throws {
         pp_log(ctx, .openvpn, .info, "Complete connection of key \(key)")
         let history = NegotiationHistory(pushReply: pushReply)
-#if swift(<6.0)
+#if swift(>=6.0)
+        nonisolated(unsafe) let dataChannel = try newDataChannel(with: history)
+#else
         let dataChannel = try newDataChannel(with: history)
 #endif
         self.history = history
         authenticator?.reset()
         Task {
-#if swift(>=6.0)
-            nonisolated(unsafe) let dataChannel = try newDataChannel(with: history)
-#endif
             await options.onConnected(key, dataChannel, pushReply)
         }
     }