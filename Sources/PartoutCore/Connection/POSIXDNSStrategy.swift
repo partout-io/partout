d457adcdbd77299d820f8e329a576608506ff4b7
Davide <keeshux@gmail.com>
2026-05-13 20:37:29 +0200

    Core: Restrict AI_ALL to Apple platforms
    Behavior elsewhere is broken (Android) or undefined.

--- Sources/PartoutCore/Connection/POSIXDNSStrategy.swift
+++ Sources/PartoutCore/Connection/POSIXDNSStrategy.swift
@@ -45,8 +45,11 @@ private extension POSIXDNSStrategy {
     static func resolveAndBlock(hostname: String) throws -> [DNSRecord]? {
         let addr = hostname.cString(using: .utf8)
         var hints = addrinfo()
+#if canImport(Darwin)
         // We set this to ALL so that we get v4 addresses even on DNS64 networks
+        // However, DNS breaks on Android when AI_ALL + AF_UNSPEC is set
         hints.ai_flags = AI_ALL
+#endif
         hints.ai_family = AF_UNSPEC // IPv4/IPv6
         var infoPointer: UnsafeMutablePointer<addrinfo>?
         let result = getaddrinfo(addr, nil, &hints, &infoPointer)
