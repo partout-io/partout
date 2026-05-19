partout-io/partout

commit 951f284693635f248f21416593d21d53db2e9df0

Core: Fix daemon status reporting (#405)
Rather than giving the connection direct access to the
TunnelEnvironment, report connection updates through a
ConnectionReporter wrapper. This way:

- It's easier to centralize status/error reporting
- A new snapshot may be emitted immediately

Other fixes:

- OpenVPN: Fix bad error codes, `.recoverable` in particular was not
properly mapped through the inner error.
- OpenVPN: Report recoverable error codes, too.
- WireGuard: Report errors because it wasn't being done.

Lastly, on Android, fix the service not being stopped on unrecoverable
connection failure, leaving the daemon in a hanging state because of the
finished connection status stream. Implement `cancelTunnelConnection()`
via JNI to call `PartoutVpnServiceRuntime.disconnect()` when that
happens, and stop the service.

Sources/PartoutCore/Connection/SimpleConnectionDaemon.swift
@@ -44,6 +44,8 @@ public actor SimpleConnectionDaemon: ConnectionDaemon {
 
     private let statusSubject: CurrentValueStream<ConnectionStatus>
 
+    private let reporter: ConnectionReporter
+
     private var isEvaluatingConnection: Bool
 
     private var onHold: Bool
@@ -81,6 +83,7 @@ public actor SimpleConnectionDaemon: ConnectionDaemon {
 
         state = .initial
         statusSubject = CurrentValueStream(.disconnected)
+        reporter = params.connectionParameters.reporter
         isEvaluatingConnection = false
         onHold = false
 
@@ -152,7 +155,7 @@ public actor SimpleConnectionDaemon: ConnectionDaemon {
             pp_log_id(profile.id, .core, .notice, "Daemon started successfully")
         } catch {
             pp_log_id(profile.id, .core, .fault, "Unable to start daemon: \(error)")
-            environment.setEnvironmentValue(PartoutError(error).code, forKey: TunnelEnvironmentKeys.lastErrorCode)
+            reportLastError(error)
             controller.setReasserting(false)
             controller.cancelTunnelConnection(with: error)
         }
@@ -372,7 +375,7 @@ extension SimpleConnectionDaemon {
             didStart = try await connection.start()
         } catch {
             pp_log_id(profile.id, .core, .error, "Unable to start connection: \(error)")
-            environment.setEnvironmentValue(PartoutError(error).code, forKey: TunnelEnvironmentKeys.lastErrorCode)
+            reportLastError(error)
             return
         }
 
@@ -426,8 +429,14 @@ extension SimpleConnectionDaemon {
     }
 
     func onConnectionError(_ error: Error) {
-        environment.setEnvironmentValue(PartoutError(error).code, forKey: TunnelEnvironmentKeys.lastErrorCode)
+        reportLastError(error)
         controller.setReasserting(false)
+        controller.cancelTunnelConnection(with: error)
+    }
+
+    func reportLastError(_ error: Error) {
+        reporter.reportLastError(error)
+        publishSnapshot()
     }
 
     func reportStatus(_ status: ConnectionStatus) {

Sources/PartoutOS/AppleNE/AppExtension/NEPTPForwarder.swift
@@ -35,7 +35,6 @@ public actor NEPTPForwarder {
         }
         let factory = NEInterfaceFactory(ctx, provider: provider, options: factoryOptions)
         let reachability = NEObservablePath(ctx)
-
         let connectionParameters = ConnectionParameters(
             profile: profile,
             controller: controller,

Sources/PartoutOpenVPNConnection/Internal/OpenVPNSessionError+Mapping.swift
@@ -15,7 +15,7 @@ extension OpenVPNSessionError: PartoutErrorMappable {
         case .negotiationTimeout, .pingTimeout, .writeTimeout:
             return .timeout
 
-        case .badCredentials:
+        case .badCredentials, .badCredentialsWithLocalOptions:
             return .authentication
 
         case .serverCompression:
@@ -47,6 +47,11 @@ extension OpenVPNSessionError: PartoutErrorMappable {
 
             default:
                 break
+            }
+
+        case .recoverable(let error):
+            if let error {
+                return error.partoutErrorCode
             }
 
         default:

Tests/PartoutCoreTests/SimpleConnectionDaemonTests.swift
@@ -63,7 +63,7 @@ struct SimpleConnectionDaemonTests {
     }
 
     @Test
-    func givenConnectionFailingToStart_whenStartDaemon_thenStarts() async throws {
+    func givenConnectionFailingToStart_whenStartDaemon_thenStartsAndPublishesLastErrorCode() async throws {
         var connectionModule = MockConnectionModule()
         connectionModule.options.startError = PartoutError(.dnsFailure)
         let profile = try Profile.Builder(
@@ -72,9 +72,19 @@ struct SimpleConnectionDaemonTests {
         ).build()
         #expect(profile.activeConnectionModule != nil)
 
-        let sut = try await newDaemon(with: profile)
+        let expLastError = Expectation()
+        let sut = try await newDaemon(
+            with: profile,
+            onSnapshot: { snapshot in
+                guard snapshot.environment?.lastErrorCode == .dnsFailure else { return }
+                Task {
+                    await expLastError.fulfill()
+                }
+            }
+        )
         do {
             try await sut.start()
+            try await expLastError.fulfillment(timeout: 500)
         } catch {
             #expect(Bool(false), error.localizedComment)
         }
@@ -113,6 +123,74 @@ struct SimpleConnectionDaemonTests {
         #expect(await stream.nextElement() == .disconnected)
         #expect(await stream.nextElement() == .connecting)
         #expect(await stream.nextElement() == .connected)
+    }
+
+    @Test
+    func givenStartedConnectionFailingAsynchronously_whenConnectionFails_thenPublishesLastErrorCode() async throws {
+        var connectionModule = MockConnectionModule()
+        connectionModule.options.failure = (50, PartoutError(.authentication))
+        let profile = try Profile.Builder(
+            modules: [connectionModule],
+            activeModulesIds: [connectionModule.id]
+        ).build()
+        #expect(profile.activeConnectionModule != nil)
+
+        let expLastError = Expectation()
+        let expCancel = Expectation()
+        let sut = try await newDaemon(
+            with: profile,
+            onCancel: { error in
+                guard error?.partoutErrorCode == .authentication else { return }
+                Task {
+                    await expCancel.fulfill()
+                }
+            },
+            reconnectionDelay: 5000,
+            onSnapshot: { snapshot in
+                guard snapshot.environment?.lastErrorCode == .authentication else { return }
+                Task {
+                    await expLastError.fulfill()
+                }
+            }
+        )
+        let stream = sut.statusStream
+
+        try await sut.start()
+        #expect(await stream.nextElement() == .disconnected)
+        #expect(await stream.nextElement() == .connecting)
+        #expect(await stream.nextElement() == .connected)
+        try await expLastError.fulfillment(timeout: 500)
+        try await expCancel.fulfillment(timeout: 500)
+    }
+
+    @Test
+    func givenStartedConnectionReportingRecoverableError_whenConnectionDisconnects_thenPublishesLastErrorCode() async throws {
+        var connectionModule = MockConnectionModule()
+        connectionModule.options.reportedError = (50, PartoutError(.timeout))
+        let profile = try Profile.Builder(
+            modules: [connectionModule],
+            activeModulesIds: [connectionModule.id]
+        ).build()
+        #expect(profile.activeConnectionModule != nil)
+
+        let expLastError = Expectation()
+        let sut = try await newDaemon(
+            with: profile,
+            reconnectionDelay: 5000,
+            onSnapshot: { snapshot in
+                guard snapshot.environment?.lastErrorCode == .timeout else { return }
+                Task {
+                    await expLastError.fulfill()
+                }
+            }
+        )
+        let stream = sut.statusStream
+
+        try await sut.start()
+        #expect(await stream.nextElement() == .disconnected)
+        #expect(await stream.nextElement() == .connecting)
+        #expect(await stream.nextElement() == .connected)
+        try await expLastError.fulfillment(timeout: 500)
     }
 
     @Test
@@ -181,7 +259,8 @@ private extension SimpleConnectionDaemonTests {
         environment: TunnelEnvironment? = nil,
         onCancel: @escaping (Error?) -> Void = { _ in },
         stopDelay: Int = 1000,
-        reconnectionDelay: Int = 1000
+        reconnectionDelay: Int = 1000,
+        onSnapshot: OnTunnelSnapshotCallback? = nil
     ) async throws -> SimpleConnectionDaemon {
         let controller = MockTunnelController()
         controller.onCancelTunnelConnection = onCancel
@@ -200,7 +279,8 @@ private extension SimpleConnectionDaemonTests {
             messageHandler: DefaultMessageHandler(.global, environment: environment),
             startsImmediately: false,
             stopDelay: stopDelay,
-            reconnectionDelay: reconnectionDelay
+            reconnectionDelay: reconnectionDelay,
+            onSnapshot: onSnapshot
         ))
     }
 }
@@ -241,7 +321,7 @@ private struct MockConnectionModule: ConnectionModule {
         if let creationError {
             throw creationError
         }
-        return MockConnection(options: options)
+        return MockConnection(options: options, reporter: parameters.reporter)
     }
 }
 
@@ -251,10 +331,14 @@ private final class MockConnection: Connection {
 
         var failure: (interval: Int, error: Error)?
 
+        var reportedError: (interval: Int, error: Error)?
+
         var shouldTimeout = false
     }
 
     let options: Options
+
+    let reporter: ConnectionReporter
 
     private let statusSubject = CurrentValueStream<ConnectionStatus>(.disconnected)
 
@@ -269,8 +353,9 @@ private final class MockConnection: Connection {
     nonisolated(unsafe)
     private var sleepTask: Task<Void, Never>?
 
-    init(options: Options) {
+    init(options: Options, reporter: ConnectionReporter) {
         self.options = options
+        self.reporter = reporter
     }
 
     func start() async throws -> Bool {
@@ -285,6 +370,13 @@ private final class MockConnection: Connection {
             Task {
                 try? await Task.sleep(milliseconds: failure.interval)
                 statusSubject.send(completion: .failure(failure.error))
+            }
+        }
+        if let reportedError = options.reportedError {
+            Task {
+                try? await Task.sleep(milliseconds: reportedError.interval)
+                reporter.reportLastError(reportedError.error)
+                statusSubject.send(.disconnected)
             }
         }
         return true

Sources/Partout/NativeTunnelController.swift
@@ -97,7 +97,13 @@ public final class NativeTunnelController: TunnelController {
     }
 
     public func cancelTunnelConnection(with error: Error?) {
-        // Do nothing
+        guard let error else {
+            pp_tun_ctrl_cancel_tunnel(ref, nil)
+            return
+        }
+        String(describing: error).withCString {
+            pp_tun_ctrl_cancel_tunnel(ref, $0)
+        }
     }
 }
 

Sources/PartoutCore/Connection/ConnectionParameters.swift
@@ -17,11 +17,14 @@ public final class ConnectionParameters: Sendable {
     /// The ``ReachabilityObserver`` to observe network events.
     public let reachability: ReachabilityObserver
 
-    /// The ``TunnelEnvironment`` where to store shared values.
+    /// The ``TunnelEnvironment`` backing the connection state.
     public let environment: TunnelEnvironment
 
     /// The ``ConnectionParameters/Options-swift.struct`` to fine-tune (re)connection behavior.
     public let options: Options
+
+    /// The ``ConnectionReporter`` where connections can report to.
+    public let reporter: ConnectionReporter
 
     public init(
         profile: Profile,
@@ -37,6 +40,46 @@ public final class ConnectionParameters: Sendable {
         self.reachability = reachability
         self.environment = environment
         self.options = options
+        reporter = ConnectionReporter(.init(profile.id), environment: environment)
+    }
+}
+
+/// Reports connection-originated values without exposing the whole tunnel environment to connections.
+public final class ConnectionReporter: Sendable {
+    private let ctx: PartoutLoggerContext
+    private let environment: TunnelEnvironment
+
+    public init(_ ctx: PartoutLoggerContext, environment: TunnelEnvironment) {
+        self.ctx = ctx
+        self.environment = environment
+    }
+
+    deinit {
+        pp_log(ctx, .core, .debug, "Deinit ConnectionReporter")
+    }
+
+    public func reportDataCount(_ dataCount: DataCount) {
+        reportEnvironmentValue(dataCount, forKey: TunnelEnvironmentKeys.dataCount)
+    }
+
+    public func clearDataCount() {
+        clearEnvironmentValue(forKey: TunnelEnvironmentKeys.dataCount)
+    }
+
+    public func reportEnvironmentValue<T>(_ value: T, forKey key: TunnelEnvironmentKey<T>) where T: Encodable {
+        environment.setEnvironmentValue(value, forKey: key)
+    }
+
+    public func clearEnvironmentValue<T>(forKey key: TunnelEnvironmentKey<T>) {
+        environment.removeEnvironmentValue(forKey: key)
+    }
+
+    public func reportLastError(_ error: Error) {
+        reportLastErrorCode(error.partoutErrorCode)
+    }
+
+    public func reportLastErrorCode(_ code: PartoutError.Code) {
+        reportEnvironmentValue(code, forKey: TunnelEnvironmentKeys.lastErrorCode)
     }
 }
 

Sources/PartoutCore/PartoutError.swift
@@ -68,6 +68,19 @@ extension PartoutError: Equatable {
     }
 }
 
+extension Error {
+    public var partoutErrorCode: PartoutError.Code {
+        switch self {
+        case let pe as PartoutError:
+            return pe.code
+        case let me as PartoutErrorMappable:
+            return me.asPartoutError.code
+        default:
+            return .unhandled
+        }
+    }
+}
+
 // MARK: - Description
 
 extension PartoutError: CustomDebugStringConvertible {

Sources/PartoutCore_C/include/portable/tun.h
@@ -36,3 +36,5 @@ void pp_tun_ctrl_report_snapshots(void *_Nullable ref,
                                   const char *_Nonnull snapshots_json);
 void pp_tun_ctrl_clear_tunnel(void *_Nullable ref,
                               pp_tun _Nullable tun_impl);
+void pp_tun_ctrl_cancel_tunnel(void *_Nullable ref,
+                               const char *_Nullable error_message);

commit 6086e1c5a7d18d97010251c9bd540d423a002fe5

Core: Regressions in error reporting (#411)
- Temporary OpenVPN `badCredentialsWithLocalOptions` auth failure was
mapped to an unrecoverable error (would not retry)
- Make cancel on failure an option in SimpleConnectionDaemon, because on
Apple systems, on-demand may cause an infinite reconnection cycle

Regressions in #405

Sources/PartoutCore/Connection/SimpleConnectionDaemon.swift
@@ -26,6 +26,8 @@ public actor SimpleConnectionDaemon: ConnectionDaemon {
 
     private let startsImmediately: Bool
 
+    private let cancelsUnrecoverable: Bool
+
     private let stopDelay: Int
 
     private let reconnectionDelay: Int
@@ -80,6 +82,7 @@ public actor SimpleConnectionDaemon: ConnectionDaemon {
         reachability = params.connectionParameters.reachability
         messageHandler = params.messageHandler
         startsImmediately = params.startsImmediately
+        cancelsUnrecoverable = params.cancelsUnrecoverable
         stopDelay = params.stopDelay
         reconnectionDelay = params.reconnectionDelay
         snapshotInterval = params.snapshotInterval
@@ -162,7 +165,9 @@ public actor SimpleConnectionDaemon: ConnectionDaemon {
             pp_log_id(profile.id, .core, .fault, "Unable to start daemon: \(error)")
             reportLastError(error)
             controller.setReasserting(false)
-            controller.cancelTunnelConnection(with: error)
+            if cancelsUnrecoverable {
+                controller.cancelTunnelConnection(with: error)
+            }
         }
         if !startsImmediately {
             networkObserver?.setEnabled(true)
@@ -436,7 +441,9 @@ extension SimpleConnectionDaemon {
     func onConnectionError(_ error: Error) {
         reportLastError(error)
         controller.setReasserting(false)
-        controller.cancelTunnelConnection(with: error)
+        if cancelsUnrecoverable {
+            controller.cancelTunnelConnection(with: error)
+        }
     }
 
     func reportLastError(_ error: Error) {
@@ -519,6 +526,8 @@ extension SimpleConnectionDaemon {
 
         let startsImmediately: Bool
 
+        let cancelsUnrecoverable: Bool
+
         let stopDelay: Int
 
         let reconnectionDelay: Int
@@ -534,6 +543,7 @@ extension SimpleConnectionDaemon {
             connectionParameters: ConnectionParameters,
             messageHandler: MessageHandler,
             startsImmediately: Bool,
+            cancelsUnrecoverable: Bool,
             stopDelay: Int? = nil,
             reconnectionDelay: Int? = nil,
             snapshotInterval: Int? = nil,
@@ -544,6 +554,7 @@ extension SimpleConnectionDaemon {
             self.connectionParameters = connectionParameters
             self.messageHandler = messageHandler
             self.startsImmediately = startsImmediately
+            self.cancelsUnrecoverable = cancelsUnrecoverable
             self.stopDelay = stopDelay ?? 2000
             self.reconnectionDelay = reconnectionDelay ?? 2000
             self.snapshotInterval = snapshotInterval ?? 1000

Sources/PartoutOS/AppleNE/AppExtension/NEPTPForwarder.swift
@@ -26,6 +26,7 @@ public actor NEPTPForwarder {
         environment: TunnelEnvironment,
         factoryOptions: NEInterfaceFactory.Options = .init(),
         connectionOptions: ConnectionParameters.Options = .init(),
+        cancelsUnrecoverable: Bool,
         stopDelay: Int? = nil,
         reconnectionDelay: Int? = nil,
         minDataCountDelta: UInt64? = nil
@@ -51,6 +52,7 @@ public actor NEPTPForwarder {
             connectionParameters: connectionParameters,
             messageHandler: messageHandler,
             startsImmediately: true,
+            cancelsUnrecoverable: cancelsUnrecoverable,
             stopDelay: stopDelay,
             reconnectionDelay: reconnectionDelay,
             minDataCountDelta: minDataCountDelta

Sources/PartoutOpenVPN/Partout+OpenVPN.swift
@@ -33,6 +33,8 @@ extension PartoutError.Code {
 
         public static let passphraseRequired = PartoutError.Code("OpenVPN.passphraseRequired")
 
+        public static let recoverableAuthentication = PartoutError.Code("OpenVPN.recoverableAuthentication")
+
         public static let serverShutdown = PartoutError.Code("OpenVPN.serverShutdown")
 
         public static let tlsFailure = PartoutError.Code("OpenVPN.tlsFailure")

Sources/PartoutOpenVPNConnection/Internal/Error+Recoverable.swift
@@ -20,5 +20,6 @@ private let ppRecoverableCodes: [PartoutError.Code] = [
     .linkFailure,
     .networkChanged,
     .OpenVPN.connectionFailure,
+    .OpenVPN.recoverableAuthentication,
     .OpenVPN.serverShutdown
 ]

Sources/PartoutOpenVPNConnection/Internal/OpenVPNSessionError+Mapping.swift
@@ -15,8 +15,11 @@ extension OpenVPNSessionError: PartoutErrorMappable {
         case .negotiationTimeout, .pingTimeout, .writeTimeout:
             return .timeout
 
-        case .badCredentials, .badCredentialsWithLocalOptions:
+        case .badCredentials:
             return .authentication
+
+        case .badCredentialsWithLocalOptions:
+            return .OpenVPN.recoverableAuthentication
 
         case .serverCompression:
             return .OpenVPN.compressionMismatch

Tests/PartoutCoreTests/SimpleConnectionDaemonTests.swift
@@ -15,7 +15,7 @@ struct SimpleConnectionDaemonTests {
         let profile = try Profile.Builder()
             .build()
 
-        let sut = try await newDaemon(with: profile)
+        let sut = try newDaemon(with: profile)
         do {
             try await sut.start()
         } catch {
@@ -33,7 +33,7 @@ struct SimpleConnectionDaemonTests {
         #expect(profile.activeConnectionModule != nil)
 
         let reachability = MockReachabilityObserver()
-        let sut = try await newDaemon(with: profile, reachability: reachability)
+        let sut = try newDaemon(with: profile, reachability: reachability)
         let stream = sut.statusStream
 
         try await sut.start()
@@ -54,7 +54,7 @@ struct SimpleConnectionDaemonTests {
 
         let environment = SharedTunnelEnvironment(profileId: profile.id)
         do {
-            _ = try await newDaemon(with: profile, environment: environment)
+            _ = try newDaemon(with: profile, environment: environment)
         } catch let error as PartoutError {
             #expect(error.code == .authentication)
         } catch {
@@ -73,7 +73,7 @@ struct SimpleConnectionDaemonTests {
         #expect(profile.activeConnectionModule != nil)
 
         let expLastError = Expectation()
-        let sut = try await newDaemon(
+        let sut = try newDaemon(
             with: profile,
             onSnapshot: { snapshot in
                 guard snapshot.environment?.lastErrorCode == .dnsFailure else { return }
@@ -101,7 +101,7 @@ struct SimpleConnectionDaemonTests {
 
         let reachability = MockReachabilityObserver()
         reachability.isReachable = false
-        let sut = try await newDaemon(
+        let sut = try newDaemon(
             with: profile,
             reachability: reachability,
             reconnectionDelay: 100
@@ -137,7 +137,7 @@ struct SimpleConnectionDaemonTests {
 
         let expLastError = Expectation()
         let expCancel = Expectation()
-        let sut = try await newDaemon(
+        let sut = try newDaemon(
             with: profile,
             onCancel: { error in
                 guard error?.partoutErrorCode == .authentication else { return }
@@ -174,7 +174,7 @@ struct SimpleConnectionDaemonTests {
         #expect(profile.activeConnectionModule != nil)
 
         let expLastError = Expectation()
-        let sut = try await newDaemon(
+        let sut = try newDaemon(
             with: profile,
             reconnectionDelay: 5000,
             onSnapshot: { snapshot in
@@ -209,7 +209,7 @@ struct SimpleConnectionDaemonTests {
 
         let recorder = SnapshotRecorder()
         let expDataCount = Expectation()
-        let sut = try await newDaemon(
+        let sut = try newDaemon(
             with: profile,
             reconnectionDelay: 5000,
             snapshotInterval: 30,
@@ -246,7 +246,7 @@ struct SimpleConnectionDaemonTests {
         #expect(profile.activeConnectionModule != nil)
 
         let reachability = MockReachabilityObserver()
-        let sut = try await newDaemon(
+        let sut = try newDaemon(
             with: profile,
             reachability: reachability,
             stopDelay: 100,
@@ -275,7 +275,7 @@ struct SimpleConnectionDaemonTests {
         #expect(profile.activeConnectionModule != nil)
 
         let reachability = MockReachabilityObserver()
-        let sut = try await newDaemon(
+        let sut = try newDaemon(
             with: profile,
             reachability: reachability,
             stopDelay: 200
@@ -306,7 +306,7 @@ private extension SimpleConnectionDaemonTests {
         snapshotInterval: Int = 1000,
         minDataCountDelta: UInt64 = 0,
         onSnapshot: OnTunnelSnapshotCallback? = nil
-    ) async throws -> SimpleConnectionDaemon {
+    ) throws -> SimpleConnectionDaemon {
         let controller = MockTunnelController()
         controller.onCancelTunnelConnection = onCancel
         let options = ConnectionParameters.Options()
@@ -323,6 +323,7 @@ private extension SimpleConnectionDaemonTests {
             ),
             messageHandler: DefaultMessageHandler(.global, environment: environment),
             startsImmediately: false,
+            cancelsUnrecoverable: true,
             stopDelay: stopDelay,
             reconnectionDelay: reconnectionDelay,
             snapshotInterval: snapshotInterval,