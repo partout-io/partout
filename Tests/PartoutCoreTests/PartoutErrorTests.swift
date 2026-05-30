f3b7392601518450266cf259cd5e756669c463e6
Davide <keeshux@gmail.com>
2026-05-30 21:41:22 +0200

    Fix error tests

--- Tests/PartoutCoreTests/PartoutErrorTests.swift
+++ Tests/PartoutCoreTests/PartoutErrorTests.swift
@@ -29,7 +29,7 @@ struct PartoutErrorTests {
     @Test(arguments: [
         (PartoutError(.authentication), "{PartoutError.authentication}"),
         (PartoutError(.authentication, "userInfo"), "{PartoutError.authentication, userInfo=userInfo}"),
-        (PartoutError(.authentication, "userInfo", SomeDescriptiveError()), "{PartoutError.authentication, userInfo=userInfo, reason=SomeDescriptiveError()}")
+        (PartoutError(.authentication, "userInfo", SomeDescriptiveError()), "{PartoutError.authentication, userInfo=userInfo, reason=SomeDescriptiveError() (errorDescription)}")
     ])
     func givenError_whenDescribe_thenReturnsDescription(error: PartoutError, expectedDescription: String) {
         #expect(error.debugDescription == expectedDescription)
