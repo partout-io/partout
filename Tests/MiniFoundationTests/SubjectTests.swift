78172be8648331cacfbdd42a13b1911bd6299a10
Davide <keeshux@gmail.com>
2026-06-04 10:28:26 +0200

    Clean up Combine stream extensions

--- Tests/MiniFoundationTests/SubjectTests.swift
+++ Tests/MiniFoundationTests/SubjectTests.swift
@@ -3,7 +3,7 @@
 // SPDX-License-Identifier: MIT
 
 #if canImport(Combine)
-@preconcurrency import Combine
+import Combine
 import MiniFoundation
 import Testing
 
@@ -71,55 +71,6 @@ struct SubjectTests {
         }
         sut.send(completion: .finished)
         try await exp.fulfillment(timeout: 500)
-    }
-
-    @Test
-    func givenPassthrough_whenStream_thenMatches() async throws {
-        let sut = PassthroughSubject<Int, Error>()
-        let expected = [5, 7, 67]
-        let stream = sut.stream()
-        Task {
-            for num in expected {
-                sut.send(num)
-                try await Task.sleep(for: .milliseconds(100))
-            }
-            sut.send(completion: .finished)
-        }
-        var i = 0
-        for try await num in stream {
-            print("Number: \(num)")
-            guard i < expected.count else {
-                #expect(Bool(false), "Emitted more values than sequence length")
-                return
-            }
-            #expect(num == expected[i])
-            i += 1
-        }
-    }
-
-    @Test
-    func givenCurrentValue_whenStream_thenMatches() async throws {
-        let sut = CurrentValueSubject<Int, Error>(100)
-        let sequence = [5, 7, 67]
-        let expected = [100] + sequence
-        let stream = sut.stream()
-        Task {
-            for num in sequence {
-                sut.send(num)
-                try await Task.sleep(for: .milliseconds(100))
-            }
-            sut.send(completion: .finished)
-        }
-        var i = 0
-        for try await num in stream {
-            print("Number: \(num)")
-            guard i < expected.count else {
-                #expect(Bool(false), "Emitted more values than sequence length")
-                return
-            }
-            #expect(num == expected[i])
-            i += 1
-        }
     }
 }
 #endif
