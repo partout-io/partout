80749df1c0ea39268e622ed8641282affd586034
Davide <keeshux@gmail.com>
2026-06-09 00:04:22 +0200

    Delete old classes

--- Sources/PartoutCore/Connection/SocketIOInterface.swift
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: 2026 Davide De Rosa
-//
-// SPDX-License-Identifier: GPL-3.0
-
-/// An ``IOInterface`` suitable for representing a socket.
-public protocol SocketIOInterface: IOInterface {
-    func close() async
-}
