e337bf3fdc484cce4b87231877ff8dda70ca3d5a
Davide <keeshux@gmail.com>
2026-06-08 23:49:57 +0200

    [ci skip]

--- Sources/PartoutCore/Misc/FileDescriptor.swift
+++ Sources/PartoutCore/Misc/FileDescriptor.swift
@@ -3,7 +3,9 @@
 // SPDX-License-Identifier: GPL-3.0
 
 #if os(Windows)
+/// A native handle for a file descriptor.
 public typealias FileDescriptor = UInt64
 #else
+/// A native handle for a file descriptor.
 public typealias FileDescriptor = Int32
 #endif
