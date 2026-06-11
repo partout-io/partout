994f08b002f7986b6836a96e27de2ecfaf0eecb4
Davide <keeshux@gmail.com>
2026-06-11 11:18:46 +0200

    Update docs

--- scripts/gen-docs.sh
+++ scripts/gen-docs.sh
@@ -10,8 +10,6 @@ PP_BUILD_DOCS="1" swift package generate-documentation \
     --target PartoutCore \
     --target PartoutOS \
     --target PartoutOpenVPN \
-    --target PartoutOpenVPN \
-    --target PartoutWireGuard \
     --target PartoutWireGuard
 
 if [[ -z $1 ]]; then
