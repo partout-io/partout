d109641d686707321f30cdb717b88bb26e62644b
Davide <keeshux@gmail.com>
2026-06-12 18:05:54 +0200

    Android: Reorg and document controller interactions

--- cross/android/io/partout/vpn/JNITunnelController.kt
+++ cross/android/io/partout/vpn/JNITunnelController.kt
@@ -23,25 +23,43 @@ import kotlinx.coroutines.launch
 import kotlinx.serialization.SerializationException
 import kotlinx.serialization.json.Json
 
-// Must match signatures in tun_android.c
-interface TunnelController {
-    // Runtime
-    fun startObserving()
-    fun stopObserving()
+/*
+Three layers are involved in this:
+- PartoutVpnServiceRuntime.kt
+- JNITunnelController.kt
+- NativeTunnelController.swift (+ tun_android.c)
 
-    // JNI -> Jotlin
+Where:
+- PartoutVpnServiceRuntime owns JNITunnelController and forwards its JNI ref to Engine.start()
+- The engine sets up the Swift/C NativeTunnelController with the JNI ref
+- On setup, NativeTunnelController sets itself (via C) as the JNITunnelController delegate
+- When needed, NativeTunnelController calls JNITunnelController methods via JNI
+- When needed, JNITunnelController calls NativeTunnelController methods via the JNI delegate
+ */
+
+// Swift/C -> Kotlin: ProGuard rules MUST MATCH!
+interface NativeTunnelControllerJNI {
     fun setDelegate(delegate: Long): Long
     fun setTunnel(infoJSON: String): Int
     fun configureSockets(fds: IntArray)
     fun onSnapshot(snapshotJSON: String)
     fun clearTunnel(killSwitch: Boolean)
     fun cancelTunnel(errorCode: String?)
+}
 
-    // Kotlin -> JNI
+// Kotlin -> JNI -> C
+interface NativeTunnelController {
     fun onReachabilityUpdate(info: NetworkInfo)
     fun getEnvironmentValue(key: String): String?
 }
 
+// Called by the runtime
+interface TunnelController: NativeTunnelController, NativeTunnelControllerJNI {
+    fun startObserving()
+    fun stopObserving()
+}
+
+// Delegated to the runtime
 interface TunnelControllerDelegate {
     fun sendSnapshot(snapshot: TunnelSnapshot)
     fun disconnect(controller: JNITunnelController)
@@ -280,6 +298,7 @@ class JNITunnelController(
         return getNativeEnvironmentValue(nativeDelegate, key)
     }
 
+    // Signatures in tun_android.c MUST MATCH!
     private external fun onNativeReachabilityUpdate(delegate: Long, networkHandle: Long)
     private external fun onNativeBetterPathUpdate(delegate: Long)
     private external fun getNativeEnvironmentValue(delegate: Long, key: String): String?
