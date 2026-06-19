partout-io/partout

commit 551f3cee917e412f95271c6ac6c07c420bca2161

OpenVPN: Fix OTPMethod breaking profile encoding

Sources/PartoutCore/OpenAPI/OpenVPN/Credentials.swift
@@ -6,7 +6,7 @@ extension OpenVPN {
 
     /// A set of credentials for authentication.
     public struct Credentials: Hashable, Sendable {
-        public enum OTPMethod: Hashable, Codable, Sendable {
+        public enum OTPMethod: String, Hashable, Codable, Sendable {
             case none
 
             case append

commit b6bb5281ba9a9919401588cf3c522f19bb8e4d2d

OpenVPN: Fix regression in OTPMethod encoding
Broken in 551f3cee917e412f95271c6ac6c07c420bca2161

Sources/PartoutCore/OpenAPI/OpenVPN/Credentials.swift
@@ -137,3 +137,48 @@ private extension OpenVPN.Credentials.OTPMethod {
         }
     }
 }
+
+// MARK: - Custom Codable
+
+extension OpenVPN.Credentials.OTPMethod {
+    private enum LegacyCodingKeys: String, CodingKey {
+        case none
+        case append
+        case encode
+    }
+
+    public init(from decoder: any Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if let rawValue = try? container.decode(String.self) {
+            guard let method = Self(rawValue: rawValue) else {
+                throw DecodingError.dataCorruptedError(
+                    in: container,
+                    debugDescription: "Unknown OTP method '\(rawValue)'"
+                )
+            }
+            self = method
+            return
+        }
+
+        let legacyContainer = try decoder.container(keyedBy: LegacyCodingKeys.self)
+        if legacyContainer.contains(.none) {
+            self = .none
+        } else if legacyContainer.contains(.append) {
+            self = .append
+        } else if legacyContainer.contains(.encode) {
+            self = .encode
+        } else {
+            throw DecodingError.dataCorrupted(
+                .init(
+                    codingPath: decoder.codingPath,
+                    debugDescription: "Unknown legacy OTP method"
+                )
+            )
+        }
+    }
+
+    public func encode(to encoder: any Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encode(rawValue)
+    }
+}

Tests/PartoutCoreTests/CodingRegistryTests.swift
@@ -37,6 +37,40 @@ struct CodingRegistryTests {
         #expect(profile == decoded)
     }
 
+    @Test
+    func givenCoder_whenDecodeV3ProfileWithLegacyOTPMethod_thenIsDecoded() throws {
+        let registry = Registry(withKnown: true)
+        let sut = registry.withLegacyEncoding(false)
+
+        var ovpnBuilder = OpenVPN.Configuration.Builder()
+        ovpnBuilder.ca = OpenVPN.CryptoContainer(pem: "ca is required")
+        ovpnBuilder.cipher = .aes128cbc
+        ovpnBuilder.remotes = [
+            try ExtendedEndpoint("host.name", EndpointProtocol(.tcp, 80))
+        ]
+        let credentials = OpenVPN.Credentials.Builder(
+            username: "user",
+            password: "password",
+            otpMethod: .append,
+            otp: "123456"
+        ).build()
+        let module = try OpenVPNModule.Builder(
+            configurationBuilder: ovpnBuilder,
+            credentials: credentials
+        ).build()
+        let profile = try Profile.Builder(modules: [module]).build()
+
+        let encoded = try sut.string(fromProfile: profile)
+        let legacyEncoded = encoded.replacingOccurrences(
+            of: "\"otpMethod\":\"append\"",
+            with: "\"otpMethod\":{\"append\":{}}"
+        )
+        #expect(legacyEncoded != encoded)
+
+        let decoded = try sut.profile(fromString: legacyEncoded)
+        #expect(decoded == profile)
+    }
+
     @Test(arguments: [true, false])
     func givenCoder_whenEncodeProfileWithRegisteredModule_thenIsDecoded(legacy: Bool) throws {
         let registry = Registry(allHandlers: [