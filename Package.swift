partout-io/partout

commit 537d7c3796f9d94f55c0b882ced32b585de2f1ae

Crypto: Wrap up MbedTLS build (#506)
- Simplify the SwiftPM manifest with .systemLibrary targets around
OpenSSL and MbedTLS
- Update CMake build and handle Android
- Delete stale shims and modulemaps

Package.swift
@@ -18,8 +18,10 @@ let envDocs = env["PP_BUILD_DOCS"] == "1"
 let areas = Area.allCases
 let cryptoMode: CryptoMode? = .openSSL
 let openSSLVersion: Version = "3.6.300" // 3.6.2
+// let cryptoMode: CryptoMode? = .native
 let wgGoVersion: Version = "0.0.20260530"
-let cmakeOutput = envCMakeOutput ?? "bin/windows-arm64"
+// Local CMake output is only required for generated wg-go and wintun artifacts.
+let cmakeOutput = envCMakeOutput ?? "bin/darwin-arm64"
 let useFoundationCompatibility: FoundationCompatibility = .off
 // let useFoundationCompatibility: FoundationCompatibility = OS.current != .apple ? .on : .off
 
@@ -307,45 +309,48 @@ if areas.contains(.wireGuard) {
 switch cryptoMode {
 case .openSSL:
     // OpenSSL-based crypto/TLS implementations
-    switch OS.current {
-    case .apple:
-        package.dependencies.append(
-            .package(url: "https://github.com/partout-io/openssl-apple", from: openSSLVersion)
+    package.targets.append(
+        .systemLibrary(
+            name: "COpenSSL",
+            path: "Sources/SystemLibraries/COpenSSL",
+            pkgConfig: "openssl",
+            providers: [
+                .brew(["openssl@3"]),
+                .apt(["libssl-dev"])
+            ]
         )
-        package.targets.append(contentsOf: [
-            .target(
-                name: "_PartoutCryptoImpl_C",
-                dependencies: [
-                    "openssl-apple",
-                    "PartoutCore_C"
-                ],
-                path: "Sources/PartoutCrypto/OpenSSL_C"
-            )
-        ])
-    default:
-        package.targets.append(
-            .target(
-                name: "_PartoutCryptoImpl_C",
-                dependencies: ["PartoutCore_C"],
-                path: "Sources/PartoutCrypto/OpenSSL_C",
-                cSettings: globalCSettings + [
-                    .unsafeFlags(["-I\(cmakeOutput)/openssl/include"])
-                ],
-                linkerSettings: [
-                    .unsafeFlags(["-L\(cmakeOutput)/openssl/lib"]),
-                    // WARNING: order matters, ssl then crypto
-                    .linkedLibrary("\(staticLibPrefix)ssl"),
-                    .linkedLibrary("\(staticLibPrefix)crypto")
-                ]
-            )
-        )
-    }
-case .native:
-    // Crypto with OS routines, TLS with MbedTLS
+    )
     package.targets.append(
         .target(
             name: "_PartoutCryptoImpl_C",
-            dependencies: ["PartoutCore_C"],
+            dependencies: [
+                "PartoutCore_C",
+                "COpenSSL"
+            ],
+            path: "Sources/PartoutCrypto/OpenSSL_C",
+            cSettings: globalCSettings
+        )
+    )
+case .native:
+    // Crypto with OS routines, TLS with MbedTLS
+    package.targets.append(
+        .systemLibrary(
+            name: "CMbedTLS",
+            path: "Sources/SystemLibraries/CMbedTLS",
+            pkgConfig: "mbedtls",
+            providers: [
+                .brew(["mbedtls"]),
+                .apt(["libmbedtls-dev"])
+            ]
+        )
+    )
+    package.targets.append(
+        .target(
+            name: "_PartoutCryptoImpl_C",
+            dependencies: [
+                "PartoutCore_C",
+                "CMbedTLS"
+            ],
             path: "Sources/PartoutCrypto/Native_C",
             exclude: {
                 // Pick current OS by removing it from exclusions
@@ -353,16 +358,7 @@ case .native:
                 list.remove(.current)
                 return list.map { "src/\($0.rawValue)" }
             }(),
-            cSettings: globalCSettings + [
-                .unsafeFlags(["-I\(cmakeOutput)/mbedtls/include"])
-            ],
-            linkerSettings: [
-                .unsafeFlags(["-L\(cmakeOutput)/mbedtls/lib"]),
-                 // WARNING: order matters
-                .linkedLibrary("mbedtls"),
-                .linkedLibrary("mbedx509"),
-                .linkedLibrary("mbedcrypto")
-            ]
+            cSettings: globalCSettings
         )
     )
 default:
@@ -435,7 +431,7 @@ enum Area: CaseIterable {
 
 enum OS: String, CaseIterable {
     case android
-    case apple
+    case apple = "darwin"
     case linux
     case windows
 

Sources/CMakeLists.txt
@@ -28,7 +28,7 @@ elseif(PP_BUILD_USE_MBEDTLS)
     list(APPEND PARTOUT_C_INCLUDE_DIRS ${CMAKE_CURRENT_SOURCE_DIR}/PartoutCrypto/Native_C/include)
     list(APPEND EXCLUDED_PATTERNS PartoutCrypto\/OpenSSL_C\/)
     if(NOT APPLE)
-        list(APPEND EXCLUDED_PATTERNS PartoutCrypto\/Native_C\/src/apple)
+        list(APPEND EXCLUDED_PATTERNS PartoutCrypto\/Native_C\/src/darwin)
     endif()
     if(NOT LINUX)
         list(APPEND EXCLUDED_PATTERNS PartoutCrypto\/Native_C\/src/linux)

Sources/PartoutCrypto/Native_C/src/crypto_stubs.c
@@ -14,37 +14,58 @@
 pp_crypto_ctx pp_crypto_aead_create(const char *cipher_name,
                                     size_t tag_len, size_t id_len,
                                     const pp_crypto_keys *keys) {
+    (void)cipher_name;
+    (void)tag_len;
+    (void)id_len;
+    (void)keys;
     return NULL;
 }
 
-void pp_crypto_aead_free(pp_crypto_ctx vctx) {}
+void pp_crypto_aead_free(pp_crypto_ctx vctx) {
+    (void)vctx;
+}
 
 pp_crypto_ctx pp_crypto_cbc_create(const char *cipher_name,
                                    const char *digest_name,
                                    const pp_crypto_keys *keys) {
+    (void)cipher_name;
+    (void)digest_name;
+    (void)keys;
     return NULL;
 }
 
-void pp_crypto_cbc_free(pp_crypto_ctx vctx) {}
+void pp_crypto_cbc_free(pp_crypto_ctx vctx) {
+    (void)vctx;
+}
 
 pp_crypto_ctx pp_crypto_ctr_create(const char *cipher_name,
                                    const char *digest_name,
                                    size_t tag_len, size_t payload_len,
                                    const pp_crypto_keys *keys) {
+    (void)cipher_name;
+    (void)digest_name;
+    (void)tag_len;
+    (void)payload_len;
+    (void)keys;
     return NULL;
 }
 
-void pp_crypto_ctr_free(pp_crypto_ctx vctx) {}
+void pp_crypto_ctr_free(pp_crypto_ctx vctx) {
+    (void)vctx;
+}
 
 bool pp_crypto_init_seed(const uint8_t *src, const size_t len) {
+    (void)src;
+    (void)len;
     return false;
 }
 
 pp_zd *pp_hmac_create(void) {
-    return NULL;
+    return 0xffffffff;
 }
 
 size_t pp_hmac_do(pp_hmac_ctx *ctx) {
+    (void)ctx;
     return 0;
 }
 

Sources/PartoutCrypto/Native_C/src/mbed/keys.c
@@ -10,10 +10,14 @@
 
 char *pp_key_decrypted_from_path(const char *path, const char *passphrase) {
     // FIXME: #108, Implement with mbedTLS
+    (void)path;
+    (void)passphrase;
     return NULL;
 }
 
 char *pp_key_decrypted_from_pem(const char *pem, const char *passphrase) {
     // FIXME: #108, Implement with mbedTLS
+    (void)pem;
+    (void)passphrase;
     return NULL;
 }

Sources/PartoutCrypto/Native_C/src/mbed/tls.c
@@ -10,47 +10,65 @@
 // FIXME: #108, Implement with mbedTLS
 
 //static const char *const TLSBoxClientEKU = "TLS Web Client Authentication";
-static const char *const TLSBoxServerEKU = "TLS Web Server Authentication";
+//static const char *const TLSBoxServerEKU = "TLS Web Server Authentication";
 
 pp_tls pp_tls_create(const pp_tls_options *opt, pp_tls_error_code *error) {
+    (void)opt;
+    (void)error;
     return NULL;
 }
 
 void pp_tls_free(pp_tls tls) {
+    (void)tls;
 }
 
 bool pp_tls_start(pp_tls tls) {
+    (void)tls;
     return false;
 }
 
 bool pp_tls_is_connected(pp_tls tls) {
+    (void)tls;
     return false;
 }
 
 // MARK: - I/O
 
 pp_zd *_Nullable pp_tls_pull_cipher(pp_tls tls, pp_tls_error_code *_Nullable error) {
+    (void)tls;
+    (void)error;
     return NULL;
 }
 
 pp_zd *_Nullable pp_tls_pull_plain(pp_tls tls, pp_tls_error_code *_Nullable error) {
+    (void)tls;
+    (void)error;
     return NULL;
 }
 
 bool pp_tls_put_cipher(pp_tls tls,
                        const uint8_t *src, size_t src_len,
                        pp_tls_error_code *_Nullable error) {
+    (void)tls;
+    (void)src;
+    (void)src_len;
+    (void)error;
     return false;
 }
 
 bool pp_tls_put_plain(pp_tls tls,
                       const uint8_t *src, size_t src_len,
                       pp_tls_error_code *_Nullable error) {
+    (void)tls;
+    (void)src;
+    (void)src_len;
+    (void)error;
     return false;
 }
 
 // MARK: - MD5
 
 char *pp_tls_ca_md5(const pp_tls tls) {
+    (void)tls;
     return NULL;
 }

Sources/SystemLibraries/CMbedTLS/module.modulemap
@@ -1,4 +1,4 @@
-module mbedtls [system] {
+module CMbedTLS [system] {
     header "shim.h"
     link "mbedtls"
     link "mbedx509"

Sources/SystemLibraries/CMbedTLS/shim.h
@@ -1,0 +1,12 @@
+/*
+ * SPDX-FileCopyrightText: 2026 Davide De Rosa
+ *
+ * SPDX-License-Identifier: GPL-3.0
+ */
+
+#pragma once
+
+#include <mbedtls/build_info.h>
+#include <mbedtls/pem.h>
+#include <mbedtls/ssl.h>
+#include <mbedtls/x509.h>

Sources/SystemLibraries/COpenSSL/module.modulemap
@@ -1,4 +1,4 @@
-module openssl [system] {
+module COpenSSL [system] {
     header "shim.h"
     link "ssl"
     link "crypto"

commit 6d436a747f1a0ad62ed455e2cdf2c6579a46b227

Crypto: Fix OpenSSL build regression on Apple
Using system library on Apple rather than XCFramework.

Regression in #506

Package.swift
@@ -309,28 +309,46 @@ if areas.contains(.wireGuard) {
 switch cryptoMode {
 case .openSSL:
     // OpenSSL-based crypto/TLS implementations
-    package.targets.append(
-        .systemLibrary(
-            name: "COpenSSL",
-            path: "Sources/SystemLibraries/COpenSSL",
-            pkgConfig: "openssl",
-            providers: [
-                .brew(["openssl@3"]),
-                .apt(["libssl-dev"])
-            ]
+    switch OS.current {
+    case .apple:
+        package.dependencies.append(
+            .package(url: "https://github.com/partout-io/openssl-apple", from: openSSLVersion)
         )
-    )
-    package.targets.append(
-        .target(
-            name: "_PartoutCryptoImpl_C",
-            dependencies: [
-                "PartoutCore_C",
-                "COpenSSL"
-            ],
-            path: "Sources/PartoutCrypto/OpenSSL_C",
-            cSettings: globalCSettings
+        package.targets.append(
+            .target(
+                name: "_PartoutCryptoImpl_C",
+                dependencies: [
+                    "PartoutCore_C",
+                    "openssl-apple"
+                ],
+                path: "Sources/PartoutCrypto/OpenSSL_C",
+                cSettings: globalCSettings
+            )
         )
-    )
+    default:
+        package.targets.append(
+            .systemLibrary(
+                name: "COpenSSL",
+                path: "Sources/SystemLibraries/COpenSSL",
+                pkgConfig: "openssl",
+                providers: [
+                    .brew(["openssl@3"]),
+                    .apt(["libssl-dev"])
+                ]
+            )
+        )
+        package.targets.append(
+            .target(
+                name: "_PartoutCryptoImpl_C",
+                dependencies: [
+                    "PartoutCore_C",
+                    "COpenSSL"
+                ],
+                path: "Sources/PartoutCrypto/OpenSSL_C",
+                cSettings: globalCSettings
+            )
+        )
+    }
 case .native:
     // Crypto with OS routines, TLS with MbedTLS
     package.targets.append(