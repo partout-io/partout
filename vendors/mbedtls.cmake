65553412fb1ae2859e35881df3e6f359941ac278
Davide <keeshux@gmail.com>
2026-08-06 09:32:47 +0200

    Clean up vendors

--- vendors/mbedtls.cmake
+++ vendors/mbedtls.cmake
@@ -1,144 +1,93 @@
-set(PP_MBEDTLS_IS_SYSTEM OFF)
+set(PARTOUT_MBEDTLS_DEPENDENCY "")
+
 if(PP_USE_SYSTEM_VENDORS)
-    find_package(MbedTLS CONFIG QUIET)
-    if(MbedTLS_FOUND AND TARGET MbedTLS::mbedtls AND TARGET MbedTLS::mbedx509 AND TARGET MbedTLS::mbedcrypto)
-        set(PP_MBEDTLS_IS_SYSTEM ON)
-        get_target_property(PARTOUT_MBEDTLS_INCLUDE_DIRS MbedTLS::mbedtls INTERFACE_INCLUDE_DIRECTORIES)
-        list(GET PARTOUT_MBEDTLS_INCLUDE_DIRS 0 PARTOUT_MBEDTLS_INCLUDE_DIR)
-        get_target_property(PARTOUT_MBEDTLS_LIBRARY MbedTLS::mbedtls IMPORTED_LOCATION)
-        if(NOT PARTOUT_MBEDTLS_LIBRARY)
-            get_target_property(PARTOUT_MBEDTLS_CONFIGS MbedTLS::mbedtls IMPORTED_CONFIGURATIONS)
-            if(PARTOUT_MBEDTLS_CONFIGS)
-                list(GET PARTOUT_MBEDTLS_CONFIGS 0 PARTOUT_MBEDTLS_CONFIG)
-                string(TOUPPER "${PARTOUT_MBEDTLS_CONFIG}" PARTOUT_MBEDTLS_CONFIG)
-                get_target_property(PARTOUT_MBEDTLS_LIBRARY MbedTLS::mbedtls "IMPORTED_LOCATION_${PARTOUT_MBEDTLS_CONFIG}")
-            endif()
-        endif()
-        if(NOT PARTOUT_MBEDTLS_LIBRARY)
-            message(FATAL_ERROR "Unable to locate the system MbedTLS library")
-        endif()
-        get_filename_component(PARTOUT_MBEDTLS_LIBRARY_DIR "${PARTOUT_MBEDTLS_LIBRARY}" DIRECTORY)
-        add_library(MbedTLSInterface INTERFACE)
-        target_link_libraries(MbedTLSInterface INTERFACE
-            MbedTLS::mbedtls
-            MbedTLS::mbedx509
-            MbedTLS::mbedcrypto
-        )
-        message(STATUS "Using system MbedTLS")
-        return()
+    partout_use_homebrew_formula(mbedtls)
+    find_path(PARTOUT_MBEDTLS_INCLUDE_DIR mbedtls/ssl.h)
+    find_library(PARTOUT_MBEDTLS_TLS_LIBRARY mbedtls)
+    find_library(PARTOUT_MBEDTLS_X509_LIBRARY mbedx509)
+    find_library(PARTOUT_MBEDTLS_CRYPTO_LIBRARY mbedcrypto)
+    if(NOT PARTOUT_MBEDTLS_INCLUDE_DIR OR
+       NOT PARTOUT_MBEDTLS_TLS_LIBRARY OR
+       NOT PARTOUT_MBEDTLS_X509_LIBRARY OR
+       NOT PARTOUT_MBEDTLS_CRYPTO_LIBRARY)
+        message(FATAL_ERROR "System MbedTLS not found")
     endif()
 
-    find_package(PkgConfig QUIET)
-    if(PkgConfig_FOUND)
-        pkg_check_modules(MBEDTLS_PKG QUIET IMPORTED_TARGET mbedtls mbedx509 mbedcrypto)
-        if(MBEDTLS_PKG_FOUND AND TARGET PkgConfig::MBEDTLS_PKG)
-            set(PP_MBEDTLS_IS_SYSTEM ON)
-            list(GET MBEDTLS_PKG_INCLUDE_DIRS 0 PARTOUT_MBEDTLS_INCLUDE_DIR)
-            list(GET MBEDTLS_PKG_LIBRARY_DIRS 0 PARTOUT_MBEDTLS_LIBRARY_DIR)
-            add_library(MbedTLSInterface INTERFACE)
-            target_link_libraries(MbedTLSInterface INTERFACE PkgConfig::MBEDTLS_PKG)
-            message(STATUS "Using system MbedTLS")
-            return()
+    get_filename_component(PARTOUT_MBEDTLS_LIBRARY_DIR "${PARTOUT_MBEDTLS_TLS_LIBRARY}" DIRECTORY)
+    foreach(library IN ITEMS PARTOUT_MBEDTLS_X509_LIBRARY PARTOUT_MBEDTLS_CRYPTO_LIBRARY)
+        get_filename_component(library_dir "${${library}}" DIRECTORY)
+        if(NOT library_dir STREQUAL PARTOUT_MBEDTLS_LIBRARY_DIR)
+            message(FATAL_ERROR "System MbedTLS libraries must be in the same directory")
         endif()
-    endif()
+    endforeach()
 
-    message(FATAL_ERROR "System MbedTLS not found")
-endif()
-
-set(MBEDTLS_DIR ${PP_BUILD_OUTPUT}/mbedtls)
-set(PARTOUT_MBEDTLS_INCLUDE_DIR "${MBEDTLS_DIR}/include")
-set(PARTOUT_MBEDTLS_LIBRARY_DIR "${MBEDTLS_DIR}/lib")
-
-# Output
-set(MBEDTLS_TLS_LIBRARY "lib/${CMAKE_STATIC_LIBRARY_PREFIX}mbedtls${CMAKE_STATIC_LIBRARY_SUFFIX}")
-set(MBEDTLS_X509_LIBRARY "lib/${CMAKE_STATIC_LIBRARY_PREFIX}mbedx509${CMAKE_STATIC_LIBRARY_SUFFIX}")
-set(MBEDTLS_CRYPTO_LIBRARY "lib/${CMAKE_STATIC_LIBRARY_PREFIX}mbedcrypto${CMAKE_STATIC_LIBRARY_SUFFIX}")
-
-if(PP_USE_PREBUILT_VENDORS)
-    partout_use_prebuilt_vendor(mbedtls MBEDTLS_DIR)
-    add_library(MbedTLS::TLS STATIC IMPORTED GLOBAL)
-    add_library(MbedTLS::X509 STATIC IMPORTED GLOBAL)
-    add_library(MbedTLS::Crypto STATIC IMPORTED GLOBAL)
-    set_target_properties(MbedTLS::TLS PROPERTIES
-        IMPORTED_LOCATION ${MBEDTLS_DIR}/${MBEDTLS_TLS_LIBRARY}
-    )
-    set_target_properties(MbedTLS::X509 PROPERTIES
-        IMPORTED_LOCATION ${MBEDTLS_DIR}/${MBEDTLS_X509_LIBRARY}
-    )
-    set_target_properties(MbedTLS::Crypto PROPERTIES
-        IMPORTED_LOCATION ${MBEDTLS_DIR}/${MBEDTLS_CRYPTO_LIBRARY}
-    )
-
-    add_library(MbedTLSInterface INTERFACE)
-    target_include_directories(MbedTLSInterface INTERFACE ${MBEDTLS_DIR}/include)
-    target_link_libraries(MbedTLSInterface INTERFACE
-        MbedTLS::TLS
-        MbedTLS::X509
-        MbedTLS::Crypto
-    )
+    message(STATUS "Using system MbedTLS")
     return()
 endif()
 
-set(MBEDTLS_PYTHON_VENV ${PP_BUILD_OUTPUT}/mbedtls-python)
-set(MBEDTLS_PYTHON_REQUIREMENTS ${CMAKE_CURRENT_SOURCE_DIR}/vendors/mbedtls/scripts/basic.requirements.txt)
-set(MBEDTLS_TFPSA_PYTHON_REQUIREMENTS ${CMAKE_CURRENT_SOURCE_DIR}/vendors/mbedtls/tf-psa-crypto/scripts/basic.requirements.txt)
-set(MBEDTLS_PYTHON_DRIVER_REQUIREMENTS ${CMAKE_CURRENT_SOURCE_DIR}/vendors/mbedtls/scripts/driver.requirements.txt)
-set(MBEDTLS_TFPSA_PYTHON_DRIVER_REQUIREMENTS ${CMAKE_CURRENT_SOURCE_DIR}/vendors/mbedtls/tf-psa-crypto/scripts/driver.requirements.txt)
-set(MBEDTLS_PYTHON_STAMP ${MBEDTLS_PYTHON_VENV}/.requirements.stamp)
-set(MBEDTLS_GENERATED_STAMP ${CMAKE_CURRENT_BINARY_DIR}/vendors/mbedtls-generated/.stamp)
-if(WIN32)
-    set(MBEDTLS_PYTHON_EXECUTABLE ${MBEDTLS_PYTHON_VENV}/Scripts/python.exe)
-else()
-    set(MBEDTLS_PYTHON_EXECUTABLE ${MBEDTLS_PYTHON_VENV}/bin/python)
+set(MBEDTLS_DIR "${PP_BUILD_OUTPUT}/mbedtls")
+set(PARTOUT_MBEDTLS_INCLUDE_DIR "${MBEDTLS_DIR}/include")
+set(PARTOUT_MBEDTLS_LIBRARY_DIR "${MBEDTLS_DIR}/lib")
+
+if(PP_USE_PREBUILT_VENDORS)
+    partout_use_prebuilt_vendor(mbedtls MBEDTLS_DIR)
+    return()
 endif()
+
+set(MBEDTLS_SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/vendors/mbedtls")
+set(MBEDTLS_PYTHON_VENV "${PP_BUILD_OUTPUT}/mbedtls-python")
+if(WIN32)
+    set(MBEDTLS_PYTHON "${MBEDTLS_PYTHON_VENV}/Scripts/python.exe")
+else()
+    set(MBEDTLS_PYTHON "${MBEDTLS_PYTHON_VENV}/bin/python")
+endif()
+
+set(MBEDTLS_REQUIREMENTS
+    "${MBEDTLS_SOURCE_DIR}/scripts/basic.requirements.txt"
+    "${MBEDTLS_SOURCE_DIR}/tf-psa-crypto/scripts/basic.requirements.txt"
+)
+set(MBEDTLS_REQUIREMENTS_DEPENDS
+    ${MBEDTLS_REQUIREMENTS}
+    "${MBEDTLS_SOURCE_DIR}/scripts/driver.requirements.txt"
+    "${MBEDTLS_SOURCE_DIR}/tf-psa-crypto/scripts/driver.requirements.txt"
+)
+set(MBEDTLS_PYTHON_STAMP "${MBEDTLS_PYTHON_VENV}/.requirements.stamp")
 
 find_package(Python3 REQUIRED COMPONENTS Interpreter)
 add_custom_command(
-    OUTPUT ${MBEDTLS_PYTHON_STAMP}
-    COMMAND ${CMAKE_COMMAND} -E make_directory ${PP_BUILD_OUTPUT}
-    COMMAND ${Python3_EXECUTABLE} -m venv ${MBEDTLS_PYTHON_VENV}
-    COMMAND ${MBEDTLS_PYTHON_EXECUTABLE} -m pip install --disable-pip-version-check -r ${MBEDTLS_PYTHON_REQUIREMENTS}
-    COMMAND ${MBEDTLS_PYTHON_EXECUTABLE} -m pip install --disable-pip-version-check -r ${MBEDTLS_TFPSA_PYTHON_REQUIREMENTS}
-    COMMAND ${CMAKE_COMMAND} -E touch ${MBEDTLS_PYTHON_STAMP}
-    DEPENDS
-        ${MBEDTLS_PYTHON_REQUIREMENTS}
-        ${MBEDTLS_TFPSA_PYTHON_REQUIREMENTS}
-        ${MBEDTLS_PYTHON_DRIVER_REQUIREMENTS}
-        ${MBEDTLS_TFPSA_PYTHON_DRIVER_REQUIREMENTS}
+    OUTPUT "${MBEDTLS_PYTHON_STAMP}"
+    COMMAND "${CMAKE_COMMAND}" -E make_directory "${PP_BUILD_OUTPUT}"
+    COMMAND "${Python3_EXECUTABLE}" -m venv "${MBEDTLS_PYTHON_VENV}"
+    COMMAND "${MBEDTLS_PYTHON}" -m pip install --disable-pip-version-check
+        -r "${MBEDTLS_SOURCE_DIR}/scripts/basic.requirements.txt"
+        -r "${MBEDTLS_SOURCE_DIR}/tf-psa-crypto/scripts/basic.requirements.txt"
+    COMMAND "${CMAKE_COMMAND}" -E touch "${MBEDTLS_PYTHON_STAMP}"
+    DEPENDS ${MBEDTLS_REQUIREMENTS_DEPENDS}
     VERBATIM
 )
-add_custom_target(MbedTLSPythonEnv DEPENDS ${MBEDTLS_PYTHON_STAMP})
-add_custom_command(
-    OUTPUT ${MBEDTLS_GENERATED_STAMP}
-    COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_CURRENT_BINARY_DIR}/vendors/mbedtls-generated
-    COMMAND ${CMAKE_COMMAND} -E chdir ${CMAKE_CURRENT_SOURCE_DIR}/vendors/mbedtls/tf-psa-crypto ${MBEDTLS_PYTHON_EXECUTABLE} framework/scripts/make_generated_files.py
-    COMMAND ${CMAKE_COMMAND} -E chdir ${CMAKE_CURRENT_SOURCE_DIR}/vendors/mbedtls ${MBEDTLS_PYTHON_EXECUTABLE} scripts/make_generated_files.py
-    COMMAND ${CMAKE_COMMAND} -E touch ${MBEDTLS_GENERATED_STAMP}
-    DEPENDS
-        ${MBEDTLS_PYTHON_STAMP}
-    VERBATIM
-)
-add_custom_target(MbedTLSGeneratedFiles DEPENDS ${MBEDTLS_GENERATED_STAMP})
+add_custom_target(MbedTLSPython DEPENDS "${MBEDTLS_PYTHON_STAMP}")
 
 set(MBEDTLS_CMAKE_ARGS
-    -DCMAKE_INSTALL_PREFIX=${MBEDTLS_DIR}
-    -DPython3_EXECUTABLE=${MBEDTLS_PYTHON_EXECUTABLE}
+    "-DCMAKE_INSTALL_PREFIX=${MBEDTLS_DIR}"
+    "-DPython3_EXECUTABLE=${MBEDTLS_PYTHON}"
+    -DGEN_FILES=ON
     -DENABLE_PROGRAMS=OFF
     -DENABLE_TESTING=OFF
 )
 if(CMAKE_BUILD_TYPE)
-    list(APPEND MBEDTLS_CMAKE_ARGS
-        -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}
-    )
+    list(APPEND MBEDTLS_CMAKE_ARGS "-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}")
 endif()
+
 if(WIN32)
     list(APPEND MBEDTLS_CMAKE_ARGS
         -DCMAKE_POLICY_DEFAULT_CMP0091=NEW
-        -DCMAKE_MSVC_RUNTIME_LIBRARY=${CMAKE_MSVC_RUNTIME_LIBRARY}
+        "-DCMAKE_MSVC_RUNTIME_LIBRARY=${CMAKE_MSVC_RUNTIME_LIBRARY}"
         -DUSE_SHARED_MBEDTLS_LIBRARY=OFF
         -DUSE_STATIC_MBEDTLS_LIBRARY=ON
     )
-    set(MBEDTLS_NORMALIZE_CRYPTO_LIBRARY_SCRIPT ${CMAKE_CURRENT_BINARY_DIR}/vendors/mbedtls-normalize-crypto.cmake)
+
+    set(MBEDTLS_NORMALIZE_CRYPTO_LIBRARY_SCRIPT
+        "${CMAKE_CURRENT_BINARY_DIR}/vendors/mbedtls-normalize-crypto.cmake")
     file(WRITE "${MBEDTLS_NORMALIZE_CRYPTO_LIBRARY_SCRIPT}" [=[
 set(mbedcrypto_lib "${MBEDTLS_DIR}/lib/mbedcrypto.lib")
 set(mbedcrypto_archive "${MBEDTLS_DIR}/lib/libmbedcrypto.a")
@@ -147,57 +96,24 @@ if(NOT EXISTS "${mbedcrypto_lib}" AND EXISTS "${mbedcrypto_archive}")
 endif()
 ]=])
     set(MBEDTLS_INSTALL_COMMAND
-        INSTALL_COMMAND
-            ${CMAKE_COMMAND} --build <BINARY_DIR> --target install ${PP_BUILD_CONFIG_ARGS}
-            COMMAND ${CMAKE_COMMAND}
-                "-DMBEDTLS_DIR=<INSTALL_DIR>"
-                -P "${MBEDTLS_NORMALIZE_CRYPTO_LIBRARY_SCRIPT}"
+        INSTALL_COMMAND "${CMAKE_COMMAND}" --build <BINARY_DIR> --target install ${PP_BUILD_CONFIG_ARGS}
+        COMMAND "${CMAKE_COMMAND}" "-DMBEDTLS_DIR=${MBEDTLS_DIR}"
+            -P "${MBEDTLS_NORMALIZE_CRYPTO_LIBRARY_SCRIPT}"
     )
-endif()
-if(ANDROID)
+elseif(ANDROID)
     list(APPEND MBEDTLS_CMAKE_ARGS
-        -DCMAKE_TOOLCHAIN_FILE=${CMAKE_ANDROID_NDK}/build/cmake/android.toolchain.cmake
-        -DCMAKE_ANDROID_NDK=${CMAKE_ANDROID_NDK}
-        -DANDROID_ABI=${ANDROID_ABI}
-        -DANDROID_PLATFORM=${ANDROID_PLATFORM}
-        -DANDROID_STL=${ANDROID_STL}
+        "-DCMAKE_TOOLCHAIN_FILE=${CMAKE_ANDROID_NDK}/build/cmake/android.toolchain.cmake"
+        "-DCMAKE_ANDROID_NDK=${CMAKE_ANDROID_NDK}"
+        "-DANDROID_ABI=${ANDROID_ABI}"
+        "-DANDROID_PLATFORM=${ANDROID_PLATFORM}"
+        "-DANDROID_STL=${ANDROID_STL}"
     )
 endif()
 
 ExternalProject_Add(MbedTLSProject
-    SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/vendors/mbedtls
-    DEPENDS MbedTLSGeneratedFiles
+    SOURCE_DIR "${MBEDTLS_SOURCE_DIR}"
+    DEPENDS MbedTLSPython
     CMAKE_ARGS ${MBEDTLS_CMAKE_ARGS}
     ${MBEDTLS_INSTALL_COMMAND}
-    INSTALL_DIR ${MBEDTLS_DIR}
-    BUILD_BYPRODUCTS
-        <INSTALL_DIR>/${MBEDTLS_TLS_LIBRARY}
-        <INSTALL_DIR>/${MBEDTLS_X509_LIBRARY}
-        <INSTALL_DIR>/${MBEDTLS_CRYPTO_LIBRARY}
 )
-
-ExternalProject_Get_Property(MbedTLSProject install_dir)
-add_library(MbedTLS::TLS STATIC IMPORTED GLOBAL)
-add_library(MbedTLS::X509 STATIC IMPORTED GLOBAL)
-add_library(MbedTLS::Crypto STATIC IMPORTED GLOBAL)
-set_target_properties(MbedTLS::TLS PROPERTIES
-    IMPORTED_LOCATION ${install_dir}/${MBEDTLS_TLS_LIBRARY}
-)
-set_target_properties(MbedTLS::X509 PROPERTIES
-    IMPORTED_LOCATION ${install_dir}/${MBEDTLS_X509_LIBRARY}
-)
-set_target_properties(MbedTLS::Crypto PROPERTIES
-    IMPORTED_LOCATION ${install_dir}/${MBEDTLS_CRYPTO_LIBRARY}
-)
-add_dependencies(MbedTLS::TLS MbedTLSProject)
-add_dependencies(MbedTLS::X509 MbedTLSProject)
-add_dependencies(MbedTLS::Crypto MbedTLSProject)
-
-add_library(MbedTLSInterface INTERFACE)
-add_dependencies(MbedTLSInterface MbedTLSProject)
-target_include_directories(MbedTLSInterface INTERFACE ${MBEDTLS_DIR}/include)
-target_link_libraries(MbedTLSInterface INTERFACE
-    MbedTLS::TLS
-    MbedTLS::X509
-    MbedTLS::Crypto
-)
+set(PARTOUT_MBEDTLS_DEPENDENCY MbedTLSProject)
--- vendors/openssl.cmake
+++ vendors/openssl.cmake
@@ -1,241 +1,89 @@
-set(PP_OPENSSL_IS_SYSTEM OFF)
+set(PARTOUT_OPENSSL_DEPENDENCY "")
+
 if(PP_USE_SYSTEM_VENDORS)
     partout_use_homebrew_formula(openssl@3.5)
-    find_package(OpenSSL 3 QUIET COMPONENTS SSL Crypto)
-    if(OpenSSL_FOUND AND TARGET OpenSSL::SSL AND TARGET OpenSSL::Crypto)
-        set(PP_OPENSSL_IS_SYSTEM ON)
-        set(PARTOUT_OPENSSL_INCLUDE_DIR "${OPENSSL_INCLUDE_DIR}")
-        get_filename_component(PARTOUT_OPENSSL_LIBRARY_DIR "${OPENSSL_SSL_LIBRARY}" DIRECTORY)
-        if(CMAKE_LIBRARY_ARCHITECTURE AND
-           EXISTS "/usr/include/${CMAKE_LIBRARY_ARCHITECTURE}/openssl/opensslconf.h")
-            set(PARTOUT_OPENSSL_CONFIG_INCLUDE_DIR "/usr/include/${CMAKE_LIBRARY_ARCHITECTURE}")
-        endif()
-        add_library(OpenSSLInterface INTERFACE)
-        target_link_libraries(OpenSSLInterface INTERFACE OpenSSL::SSL OpenSSL::Crypto)
-        message(STATUS "Using system OpenSSL")
-        return()
+    find_package(OpenSSL 3 REQUIRED COMPONENTS SSL Crypto)
+
+    set(PARTOUT_OPENSSL_INCLUDE_DIR "${OPENSSL_INCLUDE_DIR}")
+    get_filename_component(PARTOUT_OPENSSL_LIBRARY_DIR "${OPENSSL_SSL_LIBRARY}" DIRECTORY)
+    if(CMAKE_LIBRARY_ARCHITECTURE AND
+       EXISTS "/usr/include/${CMAKE_LIBRARY_ARCHITECTURE}/openssl/opensslconf.h")
+        set(PARTOUT_OPENSSL_CONFIG_INCLUDE_DIR "/usr/include/${CMAKE_LIBRARY_ARCHITECTURE}")
     endif()
 
-    find_package(PkgConfig QUIET)
-    if(PkgConfig_FOUND)
-        pkg_check_modules(OPENSSL_PKG QUIET IMPORTED_TARGET "openssl>=3")
-        if(OPENSSL_PKG_FOUND AND TARGET PkgConfig::OPENSSL_PKG)
-            set(PP_OPENSSL_IS_SYSTEM ON)
-            list(GET OPENSSL_PKG_INCLUDE_DIRS 0 PARTOUT_OPENSSL_INCLUDE_DIR)
-            list(GET OPENSSL_PKG_LIBRARY_DIRS 0 PARTOUT_OPENSSL_LIBRARY_DIR)
-            if(CMAKE_LIBRARY_ARCHITECTURE AND
-               EXISTS "/usr/include/${CMAKE_LIBRARY_ARCHITECTURE}/openssl/opensslconf.h")
-                set(PARTOUT_OPENSSL_CONFIG_INCLUDE_DIR "/usr/include/${CMAKE_LIBRARY_ARCHITECTURE}")
-            endif()
-            add_library(OpenSSLInterface INTERFACE)
-            target_link_libraries(OpenSSLInterface INTERFACE PkgConfig::OPENSSL_PKG)
-            message(STATUS "Using system OpenSSL")
-            return()
-        endif()
-    endif()
-
-    message(FATAL_ERROR "System OpenSSL 3 not found")
-endif()
-
-set(OPENSSL_DIR ${PP_BUILD_OUTPUT}/openssl)
-set(OPENSSL_LIBDIR "lib")
-set(OPENSSL_SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/vendors/openssl)
-set(OPENSSL_BUILD_SOURCE_DIR ${CMAKE_CURRENT_BINARY_DIR}/vendors/openssl-src)
-set(PARTOUT_OPENSSL_INCLUDE_DIR "${OPENSSL_DIR}/include")
-set(PARTOUT_OPENSSL_LIBRARY_DIR "${OPENSSL_DIR}/${OPENSSL_LIBDIR}")
-
-# Output
-set(OPENSSL_SSL_RUNTIME_LIBRARY "${OPENSSL_LIBDIR}/libssl${CMAKE_SHARED_LIBRARY_SUFFIX}")
-set(OPENSSL_CRYPTO_RUNTIME_LIBRARY "${OPENSSL_LIBDIR}/libcrypto${CMAKE_SHARED_LIBRARY_SUFFIX}")
-if(NOT PP_USE_PREBUILT_VENDORS)
-    if(WIN32)
-        if(ARCH_NAME MATCHES "^(arm64|aarch64)$")
-            set(OPENSSL_ARCH arm64)
-        else()
-            set(OPENSSL_ARCH x64)
-        endif()
-        set(OPENSSL_SSL_RUNTIME_LIBRARY "bin/libssl-3-${OPENSSL_ARCH}${CMAKE_SHARED_LIBRARY_SUFFIX}")
-        set(OPENSSL_CRYPTO_RUNTIME_LIBRARY "bin/libcrypto-3-${OPENSSL_ARCH}${CMAKE_SHARED_LIBRARY_SUFFIX}")
-    elseif(APPLE)
-        set(OPENSSL_SSL_RUNTIME_LIBRARY "${OPENSSL_LIBDIR}/libssl.3${CMAKE_SHARED_LIBRARY_SUFFIX}")
-        set(OPENSSL_CRYPTO_RUNTIME_LIBRARY "${OPENSSL_LIBDIR}/libcrypto.3${CMAKE_SHARED_LIBRARY_SUFFIX}")
-    elseif(ANDROID)
-        set(OPENSSL_SSL_RUNTIME_LIBRARY "${OPENSSL_LIBDIR}/libssl${CMAKE_SHARED_LIBRARY_SUFFIX}")
-        set(OPENSSL_CRYPTO_RUNTIME_LIBRARY "${OPENSSL_LIBDIR}/libcrypto${CMAKE_SHARED_LIBRARY_SUFFIX}")
-    else()
-        set(OPENSSL_SSL_RUNTIME_LIBRARY "${OPENSSL_LIBDIR}/libssl${CMAKE_SHARED_LIBRARY_SUFFIX}.3")
-        set(OPENSSL_CRYPTO_RUNTIME_LIBRARY "${OPENSSL_LIBDIR}/libcrypto${CMAKE_SHARED_LIBRARY_SUFFIX}.3")
-    endif()
-endif()
-set(OPENSSL_BYPRODUCTS
-    <INSTALL_DIR>/${OPENSSL_SSL_RUNTIME_LIBRARY}
-    <INSTALL_DIR>/${OPENSSL_CRYPTO_RUNTIME_LIBRARY}
-)
-if(WIN32)
-    set(OPENSSL_SSL_IMPORT_LIBRARY "${OPENSSL_LIBDIR}/libssl${CMAKE_IMPORT_LIBRARY_SUFFIX}")
-    set(OPENSSL_CRYPTO_IMPORT_LIBRARY "${OPENSSL_LIBDIR}/libcrypto${CMAKE_IMPORT_LIBRARY_SUFFIX}")
-    list(APPEND OPENSSL_BYPRODUCTS
-        <INSTALL_DIR>/${OPENSSL_SSL_IMPORT_LIBRARY}
-        <INSTALL_DIR>/${OPENSSL_CRYPTO_IMPORT_LIBRARY}
-    )
-endif()
-
-function(partout_import_openssl_targets ssl_runtime crypto_runtime ssl_import crypto_import)
-    add_library(OpenSSL::SSL SHARED IMPORTED GLOBAL)
-    add_library(OpenSSL::Crypto SHARED IMPORTED GLOBAL)
-    set_target_properties(OpenSSL::SSL PROPERTIES
-        IMPORTED_LOCATION "${ssl_runtime}"
-    )
-    set_target_properties(OpenSSL::Crypto PROPERTIES
-        IMPORTED_LOCATION "${crypto_runtime}"
-    )
-    if(ssl_import AND crypto_import)
-        set_target_properties(OpenSSL::SSL PROPERTIES
-            IMPORTED_IMPLIB "${ssl_import}"
-        )
-        set_target_properties(OpenSSL::Crypto PROPERTIES
-            IMPORTED_IMPLIB "${crypto_import}"
-        )
-    endif()
-endfunction()
-
-function(partout_find_openssl_runtime output_var library_dir library_name)
-    file(GLOB runtime_candidates "${library_dir}/${library_name}*${CMAKE_SHARED_LIBRARY_SUFFIX}*")
-    set(runtime_library "")
-    foreach(candidate IN LISTS runtime_candidates)
-        if(NOT IS_SYMLINK "${candidate}")
-            set(runtime_library "${candidate}")
-            break()
-        endif()
-    endforeach()
-    if(NOT runtime_library AND runtime_candidates)
-        list(GET runtime_candidates 0 runtime_library)
-    endif()
-    if(NOT runtime_library)
-        message(FATAL_ERROR "Unable to locate prebuilt OpenSSL runtime library '${library_name}' in ${library_dir}")
-    endif()
-    set(${output_var} "${runtime_library}" PARENT_SCOPE)
-endfunction()
-
-if(PP_USE_PREBUILT_VENDORS)
-    partout_use_prebuilt_vendor(openssl OPENSSL_DIR)
-    if(WIN32)
-        file(GLOB OPENSSL_SSL_RUNTIME_CANDIDATES "${OPENSSL_DIR}/bin/libssl-3*.dll")
-        file(GLOB OPENSSL_CRYPTO_RUNTIME_CANDIDATES "${OPENSSL_DIR}/bin/libcrypto-3*.dll")
-        list(LENGTH OPENSSL_SSL_RUNTIME_CANDIDATES OPENSSL_SSL_RUNTIME_CANDIDATE_COUNT)
-        list(LENGTH OPENSSL_CRYPTO_RUNTIME_CANDIDATES OPENSSL_CRYPTO_RUNTIME_CANDIDATE_COUNT)
-        if(NOT OPENSSL_SSL_RUNTIME_CANDIDATE_COUNT EQUAL 1 OR NOT OPENSSL_CRYPTO_RUNTIME_CANDIDATE_COUNT EQUAL 1)
-            message(FATAL_ERROR "Unable to locate prebuilt OpenSSL runtime libraries in ${OPENSSL_DIR}/bin")
-        endif()
-        list(GET OPENSSL_SSL_RUNTIME_CANDIDATES 0 OPENSSL_SSL_RUNTIME_LOCATION)
-        list(GET OPENSSL_CRYPTO_RUNTIME_CANDIDATES 0 OPENSSL_CRYPTO_RUNTIME_LOCATION)
-        partout_import_openssl_targets(
-            "${OPENSSL_SSL_RUNTIME_LOCATION}"
-            "${OPENSSL_CRYPTO_RUNTIME_LOCATION}"
-            "${OPENSSL_DIR}/${OPENSSL_SSL_IMPORT_LIBRARY}"
-            "${OPENSSL_DIR}/${OPENSSL_CRYPTO_IMPORT_LIBRARY}"
-        )
-    else()
-        partout_find_openssl_runtime(OPENSSL_SSL_RUNTIME_LOCATION "${OPENSSL_DIR}/${OPENSSL_LIBDIR}" libssl)
-        partout_find_openssl_runtime(OPENSSL_CRYPTO_RUNTIME_LOCATION "${OPENSSL_DIR}/${OPENSSL_LIBDIR}" libcrypto)
-        partout_import_openssl_targets(
-            "${OPENSSL_SSL_RUNTIME_LOCATION}"
-            "${OPENSSL_CRYPTO_RUNTIME_LOCATION}"
-            ""
-            ""
-        )
-    endif()
-
-    add_library(OpenSSLInterface INTERFACE)
-    target_include_directories(OpenSSLInterface INTERFACE ${OPENSSL_DIR}/include)
-    target_link_libraries(OpenSSLInterface INTERFACE
-        OpenSSL::SSL
-        OpenSSL::Crypto
-    )
+    message(STATUS "Using system OpenSSL")
     return()
 endif()
 
-# Configure flags
-set(OPENSSL_CFG_FLAGS no-apps no-docs no-dsa no-engine no-gost no-legacy shared no-ssl no-tests no-zlib)
+set(OPENSSL_DIR "${PP_BUILD_OUTPUT}/openssl")
+set(PARTOUT_OPENSSL_INCLUDE_DIR "${OPENSSL_DIR}/include")
+set(PARTOUT_OPENSSL_LIBRARY_DIR "${OPENSSL_DIR}/lib")
 
-# Add some flags if -DANDROID
-if(WIN32)
-    if(ARCH_NAME MATCHES "^(arm64|aarch64)$")
-        set(OPENSSL_TARGET "VC-WIN64-ARM")
-    else()
-        set(OPENSSL_TARGET "VC-WIN64A")
-    endif()
-elseif(ANDROID)
-    set(OPENSSL_TARGET "android-arm64")
-    list(APPEND VENDOR_ENV ANDROID_NDK_ROOT=${CMAKE_ANDROID_NDK})
-else()
-    set(OPENSSL_TARGET "")
+if(PP_USE_PREBUILT_VENDORS)
+    partout_use_prebuilt_vendor(openssl OPENSSL_DIR)
+    return()
 endif()
 
-set(CFG_ARGS
+set(OPENSSL_TARGET "")
+if(WIN32)
+    if(ARCH_NAME MATCHES "^(arm64|aarch64)$")
+        set(OPENSSL_TARGET VC-WIN64-ARM)
+    else()
+        set(OPENSSL_TARGET VC-WIN64A)
+    endif()
+elseif(ANDROID)
+    set(OPENSSL_TARGET android-arm64)
+    list(APPEND VENDOR_ENV "ANDROID_NDK_ROOT=${CMAKE_ANDROID_NDK}")
+endif()
+
+set(OPENSSL_CONFIGURE_ARGS
     ${OPENSSL_TARGET}
-    --prefix=${OPENSSL_DIR}
-    --openssldir=${OPENSSL_DIR}
-    --libdir=${OPENSSL_LIBDIR}
-    ${OPENSSL_SYMBOLS}
-    ${OPENSSL_CFG_FLAGS}
+    "--prefix=${OPENSSL_DIR}"
+    "--openssldir=${OPENSSL_DIR}"
+    --libdir=lib
+    no-apps
+    no-docs
+    no-dsa
+    no-engine
+    no-gost
+    no-legacy
+    shared
+    no-ssl
+    no-tests
+    no-zlib
 )
+
 set(OPENSSL_BUILD_COMMAND ${VENDOR_ENV} ${MAKE_CMD})
 if(NOT WIN32)
     include(ProcessorCount)
     ProcessorCount(OPENSSL_BUILD_JOBS)
-    if(NOT OPENSSL_BUILD_JOBS EQUAL 0)
+    if(OPENSSL_BUILD_JOBS)
         list(APPEND OPENSSL_BUILD_COMMAND "-j${OPENSSL_BUILD_JOBS}")
     endif()
 endif()
+
 set(OPENSSL_INSTALL_COMMAND ${VENDOR_ENV} ${MAKE_CMD} install_sw)
 if(APPLE)
     list(APPEND OPENSSL_INSTALL_COMMAND
-        COMMAND install_name_tool -id "@rpath/libcrypto.3.dylib" "${OPENSSL_DIR}/${OPENSSL_LIBDIR}/libcrypto.3.dylib"
-        COMMAND install_name_tool -id "@rpath/libssl.3.dylib" "${OPENSSL_DIR}/${OPENSSL_LIBDIR}/libssl.3.dylib"
+        COMMAND install_name_tool -id @rpath/libcrypto.3.dylib "${OPENSSL_DIR}/lib/libcrypto.3.dylib"
+        COMMAND install_name_tool -id @rpath/libssl.3.dylib "${OPENSSL_DIR}/lib/libssl.3.dylib"
         COMMAND install_name_tool -change
-            "${OPENSSL_DIR}/${OPENSSL_LIBDIR}/libcrypto.3.dylib"
-            "@rpath/libcrypto.3.dylib"
-            "${OPENSSL_DIR}/${OPENSSL_LIBDIR}/libssl.3.dylib"
+            "${OPENSSL_DIR}/lib/libcrypto.3.dylib"
+            @rpath/libcrypto.3.dylib
+            "${OPENSSL_DIR}/lib/libssl.3.dylib"
     )
 endif()
+
+set(OPENSSL_SOURCE_DIR "${CMAKE_CURRENT_SOURCE_DIR}/vendors/openssl")
+set(OPENSSL_BUILD_SOURCE_DIR "${CMAKE_CURRENT_BINARY_DIR}/vendors/openssl-src")
 ExternalProject_Add(OpenSSLProject
-    SOURCE_DIR ${OPENSSL_BUILD_SOURCE_DIR}
+    SOURCE_DIR "${OPENSSL_BUILD_SOURCE_DIR}"
     DOWNLOAD_COMMAND
-        ${CMAKE_COMMAND} -E rm -rf ${OPENSSL_BUILD_SOURCE_DIR}
-        COMMAND ${CMAKE_COMMAND} -E copy_directory ${OPENSSL_SOURCE_DIR} ${OPENSSL_BUILD_SOURCE_DIR}
-    CONFIGURE_COMMAND ${VENDOR_ENV} perl ${OPENSSL_BUILD_SOURCE_DIR}/Configure ${CFG_ARGS}
+        "${CMAKE_COMMAND}" -E rm -rf "${OPENSSL_BUILD_SOURCE_DIR}"
+        COMMAND "${CMAKE_COMMAND}" -E copy_directory "${OPENSSL_SOURCE_DIR}" "${OPENSSL_BUILD_SOURCE_DIR}"
+    CONFIGURE_COMMAND ${VENDOR_ENV} perl "${OPENSSL_BUILD_SOURCE_DIR}/Configure" ${OPENSSL_CONFIGURE_ARGS}
     BUILD_COMMAND ${OPENSSL_BUILD_COMMAND}
     INSTALL_COMMAND ${OPENSSL_INSTALL_COMMAND}
-    INSTALL_DIR ${OPENSSL_DIR}
     BUILD_IN_SOURCE 1
-    BUILD_BYPRODUCTS ${OPENSSL_BYPRODUCTS}
 )
-
-# XXX: Use absolute paths to fix linking clash with system OpenSSL/BoringSSL
-ExternalProject_Get_Property(OpenSSLProject install_dir)
-if(WIN32)
-    partout_import_openssl_targets(
-        "${install_dir}/${OPENSSL_SSL_RUNTIME_LIBRARY}"
-        "${install_dir}/${OPENSSL_CRYPTO_RUNTIME_LIBRARY}"
-        "${install_dir}/${OPENSSL_SSL_IMPORT_LIBRARY}"
-        "${install_dir}/${OPENSSL_CRYPTO_IMPORT_LIBRARY}"
-    )
-else()
-    partout_import_openssl_targets(
-        "${install_dir}/${OPENSSL_SSL_RUNTIME_LIBRARY}"
-        "${install_dir}/${OPENSSL_CRYPTO_RUNTIME_LIBRARY}"
-        ""
-        ""
-    )
-endif()
-add_dependencies(OpenSSL::SSL OpenSSLProject)
-add_dependencies(OpenSSL::Crypto OpenSSLProject)
-
-add_library(OpenSSLInterface INTERFACE)
-add_dependencies(OpenSSLInterface OpenSSLProject)
-target_include_directories(OpenSSLInterface INTERFACE ${install_dir}/include)
-target_link_libraries(OpenSSLInterface INTERFACE
-    OpenSSL::SSL
-    OpenSSL::Crypto
-)
+set(PARTOUT_OPENSSL_DEPENDENCY OpenSSLProject)
