8cf8f1963346559c4a39fdc3c3a3edd221929779
Davide <keeshux@gmail.com>
2026-08-14 10:14:26 +0200

    Move partout_log to legacy
    Already defined in Zig.

--- cross/apple/Sources/PartoutCore/Logging/PPLog.swift
+++ cross/apple/Sources/PartoutCore/Logging/PPLog.swift
@@ -40,17 +40,3 @@ public func pp_log(
     }
     logger.appendLog(level, message: ctxMessage)
 }
-
-@c(partout_log)
-public func partout_log(cLevel: Int, cMessage: UnsafePointer<CChar>?) {
-    guard let cMessage else { return }
-#if !os(Windows)
-    let savedErrno = errno
-#endif
-    let level = DebugLog.Level(rawValue: cLevel) ?? .info
-    let message = String(cString: cMessage)
-    pp_log_g(.abi, level, message)
-#if !os(Windows)
-    errno = savedErrno
-#endif
-}
