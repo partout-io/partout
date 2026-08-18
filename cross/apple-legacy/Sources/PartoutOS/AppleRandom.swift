86aabaec56d1cf3be15e38a4e2ae3dec81aea095
Davide <keeshux@gmail.com>
2026-08-18 11:11:53 +0200

    Fix partially moved files

---
+++ cross/apple-legacy/Sources/PartoutOS/AppleRandom.swift
@@ -0,0 +1,25 @@
+// SPDX-FileCopyrightText: 2026 Davide De Rosa
+//
+// SPDX-License-Identifier: GPL-3.0
+
+import Security.SecRandom
+
+/// A PRNG based on the Security framework.
+public final class AppleRandom: PRNGProtocol {
+    public init() {
+    }
+
+    public func data(length: Int) -> Data {
+        precondition(length > 0)
+        var randomData = Data(count: length)
+
+        randomData.withUnsafeMutableBytes {
+            let randomBytes = $0.bytePointer
+            guard SecRandomCopyBytes(kSecRandomDefault, length, randomBytes) == errSecSuccess else {
+                fatalError("SecRandomCopyBytes failed")
+            }
+        }
+
+        return randomData
+    }
+}
