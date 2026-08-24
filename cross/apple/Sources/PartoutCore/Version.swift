ab51effc4fa1b407909b1b185170b8dd27eae3dc
Davide <keeshux@gmail.com>
2026-08-24 15:53:23 +0200

    Revert "Bump version"
    This reverts commit d7a167cf4da85abf52a6aba0e946eaea825b66f1.

--- cross/apple/Sources/PartoutCore/Version.swift
+++ cross/apple/Sources/PartoutCore/Version.swift
@@ -4,5 +4,5 @@
 
 extension PartoutCore {
     /// The version of the Swift package. It may differ from the native runtime version.
-    public static let version = "0.161.5"
+    public static let version = "0.161.4"
 }
--- src/version.zig
+++ src/version.zig
@@ -1,2 +1,2 @@
 pub const identifier = "io.partout";
-pub const number = "0.161.5";
+pub const number = "0.161.4";
