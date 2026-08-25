2437d4db57b190c8725f96fa4d64278d286ee1ac
Davide <keeshux@gmail.com>
2026-08-26 00:17:40 +0200

    Strip PartoutException

--- cross/android/io/partout/abi/PartoutException.kt
+++ cross/android/io/partout/abi/PartoutException.kt
@@ -4,27 +4,11 @@
 
 package io.partout.abi
 
-import io.partout.models.ParseErrorInfo
 import io.partout.models.PartoutErrorCode
-import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonElement
-import kotlinx.serialization.json.decodeFromJsonElement
 
 class PartoutException(
     val result: Int? = null,
     val code: PartoutErrorCode,
     val payload: JsonElement?
-) : RuntimeException("ABI call failed (result=$result, code=$code): $payload") {
-    val arguments: List<String>
-
-    init {
-        // Best effort (should only succeed with import errors though)
-//        if (code == PartoutErrorCode.parsing)
-        arguments = payload?.let {
-            runCatching {
-                val info = Json.decodeFromJsonElement<ParseErrorInfo>(it)
-                info.arguments
-            }.getOrNull()
-        } ?: emptyList()
-    }
-}
+) : RuntimeException("ABI call failed (result=$result, code=$code): $payload")
