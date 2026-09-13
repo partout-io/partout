e06cc2f278b54ad514b839c7f70297fad708b242
Davide <keeshux@gmail.com>
2026-09-14 01:06:49 +0200

    Core: Delete SerializableModule

--- cross/apple/Sources/PartoutCore/Profile/Module.swift
+++ cross/apple/Sources/PartoutCore/Profile/Module.swift
@@ -32,14 +32,3 @@ extension Module {
         Self.moduleType
     }
 }
-
-/// A module that can be serialized as a textual profile.
-public protocol SerializableModule: Module {
-    /// Preferred file extension for serialized output.
-    var preferredExtension: String { get }
-
-    func serialized() throws -> String
-}
-
-extension SerializableModule {
-}
